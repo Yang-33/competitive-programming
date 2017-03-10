46d9756ba1d68cd41f877c30c325f05a8d59bd4e
Yang-33 <kasai.yuta0810@gmail.com>
2017-03-10 17:28:17 +0900

    make template

---
+++ topcoder/SRM/my_template.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <algorithm>
+#include <set>
+#include <sstream>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <list>
+#include <fstream>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll,ll> pll;
+
+#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
+#define FORR(i,s,e) for(ll (i)=(s);(i)>(e);(i)--)
+#define debug(x) cout<<#x<<": "<<x<<endl
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+const ll MOD = 1000000007;
+const int INF = 1e9;
+const ll LINF = 1e16;
+const double PI = acos(-1.0);
+int dx[8] = { 0,0,1,-1,1,1,-1,-1 };
+int dy[8] = { 1,-1,0,0,1,-1,1,-1 };
+
+#define foreach(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); itr++)
+/* -----  xtimex  Problem:  / Link:  ----- */
+/* ------–â‘è------
+
+
+
+-----–â‘è‚±‚±‚Ü‚Å----- */
+/* -----‰ðà“™-----
+
+
+
+ ----‰ðà‚±‚±‚Ü‚Å---- */
+
+class ${ClassName} 
+{
+    public:
+	
+    ${Method.ReturnType} ${Method.Name}(${Method.Params}) 
+	{
+        return ${Method.ReturnType;zeroval};
+    }
+};
+
+/*
+
+struct ${ClassName} {
+${<foreach Method.Params p}
+  ${p.Type} ${p.Name};
+${<end}
+  ${Method.ReturnType} ${Method.Name}(${foreach Method.Params p , }${p.Type} _${p.Name}${end}) {
+    ${foreach Method.Params p , }${p.Name} = _${p.Name}${end};
+    return ${Method.ReturnType;zeroval};
+  }
+};
+
+
+*/
+
+${CutBegin}
+${<TestCode}
+${CutEnd}
+
