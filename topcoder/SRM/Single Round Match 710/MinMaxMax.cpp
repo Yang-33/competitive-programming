ce9578b442fcf46742f9a5791c08901902f4b6c3
Yang-33 <kasai.yuta0810@gmail.com>
2017-03-10 21:58:05 +0900

    have not solved yet srm710 hard div2 !

---
+++ topcoder/SRM/Single Round Match 710/MinMaxMax.cpp
@@ -0,0 +1,193 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <algorithm>
+#include <set>
+#include <sstream>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <list>
+#include <fstream>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll,ll> pll;
+
+#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
+#define FORR(i,s,e) for(ll (i)=(s);(i)>(e);(i)--)
+#define debug(x) cout<<#x<<": "<<x<<endl
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+const ll MOD = 1000000007;
+const int INF = 1e9;
+const ll LINF = 1e16;
+const double PI = acos(-1.0);
+int dx[8] = { 0,0,1,-1,1,1,-1,-1 };
+int dy[8] = { 1,-1,0,0,1,-1,1,-1 };
+
+#define foreach(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); itr++)
+/* -----  xtimex  Problem:  / Link:  ----- */
+/* ------問題------
+
+頂点重みv[i]と辺の重みw(a->b)があたえられる。
+i=>jまでのパスはその頂点間の最大重み頂点と、辺の最大重みの積と定義される。
+d[i][j]をそのようなパスの最小値とする。これを全頂点間で求め、その総和を求めよ。
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+意味が理解できなかった。解けず。ワーシャルフロイドっぽいなあとは思ったがやはり意味が分からなかった。
+
+
+
+ ----解説ここまで---- */
+
+class MinMaxMax 
+{
+    public:
+	
+    long long findMin(vector<int> a, vector<int> b, vector<int> w, vector<int> v) 
+	{
+		
+		
+        return 0;
+    }
+};
+
+/*
+
+struct MinMaxMax {
+  vector<int> a;
+  vector<int> b;
+  vector<int> w;
+  vector<int> v;
+  long long findMin(vector<int> _a, vector<int> _b, vector<int> _w, vector<int> _v) {
+    a = _a, b = _b, w = _w, v = _v;
+    return 0;
+  }
+};
+
+*/
+
+// CUT begin
+ifstream data("MinMaxMax.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> a, vector<int> b, vector<int> w, vector<int> v, long long __expected) {
+    time_t startClock = clock();
+    MinMaxMax *instance = new MinMaxMax();
+    long long __result = instance->findMin(a, b, w, v);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> a;
+        from_stream(a);
+        vector<int> b;
+        from_stream(b);
+        vector<int> w;
+        from_stream(w);
+        vector<int> v;
+        from_stream(v);
+        next_line();
+        long long __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(a, b, w, v, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1489115212;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 900 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "MinMaxMax (900 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
