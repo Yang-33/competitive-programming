95f17fd2fb4c20ee3c195bd1761c70040056b665
Yang-33 <kasai.yuta0810@gmail.com>
2017-03-25 21:53:25 +0900

    solve 406 div2 b unodered_set

---
+++ codeforces/406_b_div2_codeforces.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+#include<cmath>
+#include<string>
+#include<cstring>
+#include<vector>
+#include<map>
+#include<list>
+#include<stack>
+#include<queue>
+#include<set>
+#include<climits> //INT_MIN/MAX
+#include <unordered_set>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
+#define FORR(i,s,e) for(ll (i)=(s);(i)>(e);(i)--)
+#define debug(x) cout<<#x<<": "<<x<<endl
+#define mp make_pair
+#define pb push_back
+const ll MOD = 1000000007;
+const int INF = 1e9;
+const ll LINF = 1e16;
+const double PI = acos(-1.0);
+int dx[8] = { 0,0,1,-1,1,1,-1,-1 };
+int dy[8] = { 1,-1,0,0,1,-1,1,-1 };
+
+/* -----  xtimex  Problem: CodeForces406 b div2 / Link:  ----- */
+/* ------–â‘è------
+
+
+
+-----–â‘è‚±‚±‚Ü‚Å----- */
+/* -----‰ðà“™-----
+
+
+
+----‰ðà‚±‚±‚Ü‚Å---- */
+
+ll N,M;
+
+ll ans = 0LL;
+
+int main()
+{
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	cin >> N>>M;
+    bool flag[10010];
+
+    FOR(i, 0, M) {
+        unordered_set<int>s;
+        flag[i] = true;
+        int k; cin >> k;
+        if (k == 1) {
+            int m; cin >> m;
+            flag[i] = true;
+        }
+        FOR(x, 0, k) {
+            int m; cin >> m;
+            if (s.find(-m)!=s.end())flag[i] = false;
+            else s.insert(m);
+        }
+        
+
+    }
+
+    FOR(i, 0, M) {
+        if (flag[i] == true) {
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+     cout << "NO" << endl;
+
+	return 0;
+}
