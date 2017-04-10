80f9f4dbb6d60a65482301cf1b6e9278aa685ab3
Yang-33 <kasai.yuta0810@gmail.com>
2017-04-10 15:13:38 +0900

    put template

---
+++ template.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <map>
+#include <list>
+#include <stack>
+#include <queue>
+#include <set>
+#include <climits> //INT_MIN/MAX
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
+#define debug(x) cout << #x << ": " << x << endl
+#define mp make_pair
+#define pb push_back
+const ll MOD = 1000000007;
+const int INF = 1e9;
+const ll LINF = 1e16;
+const double PI = acos(-1.0);
+int dx[8] = {0, 0, 1, -1, 1, 1, -1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, 1, -1};
+
+/* -----  xtimex  Problem:  / Link:   ----- */
+/* ------問題------
+
+
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+
+
+----解説ここまで---- */
+
+ll N;
+
+ll ans = 0LL;
+
+int main() {
+  cin.tie(0);
+  ios_base::sync_with_stdio(false);
+
+  cin >> N;
+  cout << ans << endl;
+
+  return 0;
+}
