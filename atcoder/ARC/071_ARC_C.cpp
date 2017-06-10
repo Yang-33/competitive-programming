8bf5dc9fb4b71a70bce48eb62ff5dbfefc80d2c1
Yang-33 <kasai.yuta0810@gmail.com>
2017-06-10 22:15:51 +0900

    ARC071 C 実験 累積和 区間処理クエリ

--- atcoder/ARC/071_ARC_C.cpp
+++ atcoder/ARC/071_ARC_C.cpp
@@ -14,107 +14,73 @@ const ll MOD = 1000000007;
 const int INF = 1e9;
 const ll LINF = 1e16;
 const double PI = acos(-1.0);
-int dx[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-int dy[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+int dx[8] = {0, 0, 1, -1, 1, 1, -1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, 1, -1};
 
-/* -----  2017/04/08  Problem: ARC 071 C / Link: http://arc071.contest.atcoder.jp/tasks/arc071_c  ----- */
+/* -----  2017/06/10  Problem: ARC 071 C / Link: http://arc071.contest.atcoder.jp/tasks/arc071_c  ----- */
 /* ------問題------
 
 A, B からなる文字列に対して、次の操作を考えます。
 
 文字列中の 1 文字を選ぶ。それが A なら BB で、 B なら AA で置き換える。
 AAA か BBB であるような部分文字列を選び、消す。
-例えば、ABA という文字列で 1 番目の操作を 1 文字目に対して行うと、 BBBA となります。 
+例えば、ABA という文字列で 1 番目の操作を 1 文字目に対して行うと、 BBBA となります。
 また、BBBAAAA に対して 2 番目の操作を 4 文字目から 6 文字目に対して行うと、 BBBA となります。
 
 これらの操作を何回でも好きな順で行うことができます。
 
-文字列 S,T と q 個のクエリ ai,bi,ci,di が与えられます。
+文字列 S,T と q 個のクエリ ai,bi,ci,di が与えられます。 
 各クエリに対して、 S の部分文字列 SaiSai+1…Sbi を T の部分文字列 TciTci+1…Tdi にすることができるか判定してください。
 
 -----問題ここまで----- */
 /* -----解説等-----
 
-操作を繰り返すと、AAA => "", BBB => "", AB=BA=""であるということに気が付く。
-つまりA=1,B=2などとしたときにmod3 でうまく消えてくれそう。
-区間のクエリにこたえたいので累積和か、セグ木で処理すればよい。
+文字が減っていくだけではなく増やす操作もできる。何度も遷移させていると必ず文字列は数文字以内に落ち着き、これは復元可能であることが分かる。
+ABは削除可能、AAA,BBB,BA等が削除可能であることを考えると、A=1,B=2としてクエリの数列の和について3で割った余りが等しければ同じになることが分かる。
+クエリにこたえるには累積和かセグ木を用いればよい。
+
+これは実験系の問題かな。
 
 ----解説ここまで---- */
 
+
+
 ll N;
-
+ll sums[100010];
+ll sumt[100010];
 ll ans = 0LL;
-string ss, tt;
-#define MAX_N 300000
-string ass[4] = { "AAA","BBB","AB","BA" };
-ll gn;
-class segtree {
-public:
-	ll n;
-	string s[MAX_N];
-	const int NON = -1;
-
-	void init(int n) {
-		while (gn < n) {
-			gn *= 2;
-		}
-		FOR(i, 0, gn * 2 - 1) {
-			s[i] = "";
-		}
-	}
-
-
-	inline void fix(int  k) {
-		// ノードkに対応する　値に修正
-		//dat[k] = min(dat[k * 2 + 1], dat[k * 2 + 2]);//min/max
-		f[k] = f[k * 2 + 1] + f[k * 2 + 2];//sum
-		g[k] = g[k * 2 + 1] + g[k * 2 + 2] + R[k * 2 + 1] * L[k * 2 + 2];
-		L[k] = L[k * 2 + 1];
-		R[k] = R[k * 2 + 2];
-
-	}
-
-	void update(int k, string a) {
-		k += gn - 1;
-		s[k] = a;
-		while (k > 0) {
-			k = (k - 1) / 2;
-			s[k] = s[k * 2 + 1] + s[k * 2 + 2];
-			FOR(i, 0, s[k].size()-3) {
-				if (s[k].substr(i, 3) == ass[0])s[k].erase(i, 3);
-				else if (s[k].substr(i, 3) == ass[1])s[k].erase(i, 3);
-				else if (s[k].substr(i, 2) == ass[2])s[k].erase(i, 2);
-				else if (s[k].substr(i, 2) == ass[3])s[k].erase(i, 2);
-			}
-		}
-	}
-
-};
 
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
-	cin >> ss >> tt;
-	// update
-	segtree s, t;
-	s.init(ss.size());
-	t.init(tt.size());
 
-	FOR(i, 0, ss.size()) {
-		s.update(i, ss[i]);
+	string S, T;
+	cin >> S >> T;
+	sums[0] = 0;
+	FOR(i, 0, S.size()) {
+		if (S[i] == 'A')sums[i + 1] = 1;
+		else if (S[i] == 'B')sums[i + 1] = 2;
 	}
-	FOR(i, 0, tt.size()) {
-		t.rangefill(i, i + 1, tt[i], 0, 0, gn);
+	sumt[0] = 0;
+	FOR(i, 0, T.size()) {
+		if (T[i] == 'A')sumt[i + 1] = 1;
+		else if (T[i] == 'B')sumt[i + 1] = 2;
 	}
-	ll q;
-	cin >> q;
-	FOR(i, 0, q) {
-		int l, r, b;
-		cin >> l >> r >> b;
-		// query    
-		rangefill(l - 1, r, b, 0, 0, gn);
-		lazy_push(l, 0, gn);
-		cout << f[0] - g[0] << endl;
+	FOR(i, 0, S.size()) {
+		sums[i + 1] += sums[i];
 	}
+	FOR(i, 0, T.size()) {
+		sumt[i + 1] += sumt[i];
+	}
+	ll Q;  cin >> Q;
+	FOR(i, 0, Q) {
+		int s1, s2, t1, t2;
+		cin >> s1 >> s2 >> t1 >> t2;
+		if ((sums[s2] - sums[s1 - 1]) % 3 == (sumt[t2] - sumt[t1 - 1]) % 3)
+			cout << "YES" << endl;
+		else  cout << "NO" << endl;
+
+	}
+
 	return 0;
 }
