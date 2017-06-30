71528279bf44277b7f149cc7d0b84ab314bfd584
Yang-33 <kasai.yuta0810@gmail.com>
2017-06-30 14:23:09 +0900

    SRM717 div2

---
+++ topcoder/SRM/Single Round Match 717/NiceTable.cpp
@@ -0,0 +1,211 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <algorithm>
+#include <set>
+#include <sstream>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <list>
+#include <fstream>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
+#define FORR(i,s,e) for(ll (i)=(s);(i)>(e);(i)--)
+#define debug(x) cout<<#x<<": "<<x<<endl
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+const ll MOD = 1000000007;
+const int INF = 1e9;
+const ll LINF = 1e16;
+const double PI = acos(-1.0);
+int dx[8] = { 0,0,1,-1,1,1,-1,-1 };
+int dy[8] = { 1,-1,0,0,1,-1,1,-1 };
+
+#define foreach(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); itr++)
+/* -----  xtimex  Problem:  / Link:  ----- */
+/* ------–â‘è------
+
+-----–â‘è‚±‚±‚Ü‚Å----- */
+/* -----‰ðà“™-----
+
+ ----‰ðà‚±‚±‚Ü‚Å---- */
+
+class NiceTable
+{
+public:
+
+	string isNice(vector<string> t)
+	{
+		string ans[2] = { "Nice","Not nice" };
+		int ansid = 1;
+
+		int h = t.size();
+		int w = t[0].size();
+		FOR(a, 0, 1 << h) {
+			FOR(b, 0, 1 << w) {
+				int f = 1;
+				int x[5]; int y[5];
+				cout << "x :";
+				FOR(i, 0, h) {
+					x[i] = (a >> i)&1;
+					cout << (x[i]) << " ";
+				}cout << endl;
+				cout << "y:";
+				FOR(j, 0, w) {
+					y[j] = (b >> j)&1;
+					cout << y[j]<< " ";
+				}cout << endl;
+
+				cout << a << " " << b << endl;
+				FOR(i, 0, h) {
+					FOR(j, 0, w) {
+
+						if ((t[i][j] - '0') != ((((a >> i) & 1)^((b >> j) & 1)) & 1)) {
+							f = 0;
+						}
+					}
+				}
+				if (f == 1) {
+					ansid = 0;
+				}
+			}
+		}
+
+
+		return ans[ansid];
+	}
+};
+
+/*
+
+struct NiceTable {
+  vector<string> t;
+  string isNice(vector<string> _t) {
+	t = _t;
+	return "";
+  }
+};
+
+*/
+
+// CUT begin
+ifstream data("NiceTable.sample");
+
+string next_line() {
+	string s;
+	getline(data, s);
+	return s;
+}
+
+template <typename T> void from_stream(T &t) {
+	stringstream ss(next_line());
+	ss >> t;
+}
+
+void from_stream(string &s) {
+	s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+	int len;
+	from_stream(len);
+	ts.clear();
+	for (int i = 0; i < len; ++i) {
+		T t;
+		from_stream(t);
+		ts.push_back(t);
+	}
+}
+
+template <typename T>
+string to_string(T t) {
+	stringstream s;
+	s << t;
+	return s.str();
+}
+
+string to_string(string t) {
+	return "\"" + t + "\"";
+}
+
+bool do_test(vector<string> t, string __expected) {
+	time_t startClock = clock();
+	NiceTable *instance = new NiceTable();
+	string __result = instance->isNice(t);
+	double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+	delete instance;
+
+	if (__result == __expected) {
+		cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+		return true;
+	}
+	else {
+		cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+		cout << "           Expected: " << to_string(__expected) << endl;
+		cout << "           Received: " << to_string(__result) << endl;
+		return false;
+	}
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+	int cases = 0, passed = 0;
+	while (true) {
+		if (next_line().find("--") != 0)
+			break;
+		vector<string> t;
+		from_stream(t);
+		next_line();
+		string __answer;
+		from_stream(__answer);
+
+		cases++;
+		if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+			continue;
+
+		cout << "  Testcase #" << cases - 1 << " ... ";
+		if (do_test(t, __answer)) {
+			passed++;
+		}
+	}
+	if (mainProcess) {
+		cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+		int T = time(NULL) - 1498820403;
+		double PT = T / 60.0, TT = 75.0;
+		cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+		cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	cout.setf(ios::fixed, ios::floatfield);
+	cout.precision(2);
+	set<int> cases;
+	bool mainProcess = true;
+	for (int i = 1; i < argc; ++i) {
+		if (string(argv[i]) == "-") {
+			mainProcess = false;
+		}
+		else {
+			cases.insert(atoi(argv[i]));
+		}
+	}
+	if (mainProcess) {
+		cout << "NiceTable (250 Points)" << endl << endl;
+	}
+	return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
