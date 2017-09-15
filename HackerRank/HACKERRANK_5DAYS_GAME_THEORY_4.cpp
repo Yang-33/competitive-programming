03f51f84e1a00f23a934e8e1fa23e2eb4e1be147
Yang-33 <kasai.yuta0810@gmail.com>
2017-09-15 23:38:01 +0900

    hackerrank gametheoty 4 Nim grundy

---
+++ HackerRank/HACKERRANK_5DAYS_GAME_THEORY_4.cpp
@@ -0,0 +1,56 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2017/09/15  Problem: hackerrank_5days_game_theory_4  / Link: https://www.hackerrank.com/contests/5-days-of-game-theory/challenges/day-2-nim-game  ----- */
+/* ------問題------
+
+Nim.
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+Nim.理解したらXORをとりましょう。
+
+----解説ここまで---- */
+
+LL N;
+
+LL ans = 0LL;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	int T; cin >> T;
+	FOR(t, 0, T) {
+		cin >> N;
+		int subg = 0;
+		FOR(i, 0, N) {
+			int pile; cin >> pile;
+			subg ^= pile;
+		}
+		cout << ((subg!=0)?"First":"Second") << "\n";
+	}
+	return 0;
+}
