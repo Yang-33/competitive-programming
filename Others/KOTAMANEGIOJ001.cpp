544ccbfe54f37a8b02477583d4f87786ecb033cb
Yang-33 <kasai.yuta0810@gmail.com>
2017-10-02 21:54:01 +0900

    solve kotamanegi online judge 001

---
+++ Others/KOTAMANEGIOJ001.cpp
@@ -0,0 +1,45 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
+#define debug(x) cout << #x << ": " << x << endl
+#define mp make_pair
+#define pb push_back
+const ll MOD = 1000000007;
+const int INF = 1e9;
+const ll LINF = 1e16;
+const double PI = acos(-1.0);
+int dx[8] = {0, 0, 1, -1, 1, 1, -1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, 1, -1};
+
+/* -----  2017/10/02  Problem: Kotamanegi OJ 001  / Link: https://kotamanegi.com/JudgeSystem/Problems/view/index.php?page=1  ----- */
+/* ------問題------
+
+偶奇の判定
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+プログラミングはすごいぞ
+
+----解説ここまで---- */
+
+ll N;
+
+ll ans = 0LL;
+
+int main() {
+  cin.tie(0);
+  ios_base::sync_with_stdio(false);
+
+  cin >> N;
+  if (N % 2)cout << "Odd" << endl;
+  else cout << "Even" << endl;
+
+  return 0;
+}
