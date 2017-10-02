da812571acab93c25e302a844c9db88dc15e8452
Yang-33 <kasai.yuta0810@gmail.com>
2017-10-02 21:54:50 +0900

    solve kotamanegi 大手前コンテストA

---
+++ Others/KOTAMANEGIOJ_OTEMAE_A.cpp
@@ -0,0 +1,42 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
+#define debug(x) cout << #x << ": " << x << endl
+#define mp make_pair
+#define pb push_back
+const ll MOD = 1000000007;
+const int INF = 1e9;
+const ll LINF = 1e16;
+const double PI = acos(-1.0);
+int dx[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+int dy[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2017/08/01  Problem: KotamanegiOJ_Otemae A  / Link: https://kotamanegi.com/JudgeSystem/Problems/view/index.php?page=16  ----- */
+/* ------問題------
+
+Hello WOrld の出力
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+
+
+----解説ここまで---- */
+
+ll N;
+
+ll ans = 0LL;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	cout << "Hello World!" << endl;
+	return 0;
+}
