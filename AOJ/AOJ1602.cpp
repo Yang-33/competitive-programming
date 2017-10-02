54e435a72364ad672e5b3b1329706d7d2207dc76
Yang-33 <kasai.yuta0810@gmail.com>
2017-10-02 22:15:36 +0900

    solve AOJ1602

--- AOJ/AOJ1602.cpp
+++ AOJ/AOJ1602.cpp
@@ -3,78 +3,90 @@ using namespace std;
 
 typedef long long ll;
 typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
 
-#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
-#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
 #define debug(x) cout << #x << ": " << x << endl
-#define mp make_pair
-#define pb push_back
-const ll MOD = 1000000007;
+#define ALL(x) (x).begin(),(x).end()
+
 const int INF = 1e9;
 const ll LINF = 1e16;
-const double PI = acos(-1.0);
-int dx[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-int dy[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2017/07/08  Problem: AOJ 1602  / Link: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1602  ----- */
+/* -----  2017/10/02  Problem: ICPC国内2015 C / Link: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1602  ----- */
 /* ------問題------
 
-
+逆ポーランド記法っぽいやつ
+こたえは？
 
 -----問題ここまで----- */
 /* -----解説等-----
 
-構文解析。
-先に演算子が来るのでこれを形にする(言葉にできない)
+構文解析練習しなきゃ・・・
 
 ----解説ここまで---- */
 
-ll N;
-ll ans = 0LL;
 
-int expr0(vector<string> &s, int &i) {
-	int d = s[i].size();
-	int n = s.size();
-	char c = s[i][d - 1];
-	if (c == '+') {
-		int res = 0;
-		i++;
-		while (s[i].size() == d + 1 && i < n)
-		{
-			res += expr0(s, i);
-		}
-		return res;
+
+int N;
+
+int Count(string&s) {
+	int count = 0;
+	for (auto c : s) {
+		if (c == '.')count++;
 	}
-	else if (c == '*') {
-		int res = 1;
-		i++;
-		while (s[i].size() == d + 1 && i < n)
-		{
-			res *= expr0(s, i);
-		}
-		return res;
-	}
-	else {
-		i++;
-		return c - '0';
-	}
+	return count;
 }
 
+int f(bool x, int at, int n, vector<string>&s) {
+	int ret;
+	if (x) ret = 1;
+	else ret = 0;
+	for (int i = at; i < N; i++) {
+		if (Count(s[i]) >= n) {
+			if (s[i][n] == '*') { //ret *= f(true, i + 1, n + 1, s); 
+
+				if (x) ret *= f(true, i + 1, n + 1, s); //(s[i][n] - '0');
+				else ret += f(true, i + 1, n + 1, s);// (s[i][n] - '0');
+
+				continue;
+			}
+			else if (s[i][n] == '+') {
+				//ret += f(false, i + 1, n + 1, s); 
+
+				if (x) ret *= f(false, i + 1, n + 1, s);
+				//(s[i][n] - '0');
+				else ret += f(false, i + 1, n + 1, s);
+				//(s[i][n] - '0');
+
+				continue;
+			}
+			else if (s[i][n] == '.')continue;
+
+			if (x) ret *= (s[i][n] - '0');
+			else ret += (s[i][n] - '0');
+
+			//cout << "kassa" <<  ret << "/" << N << endl;
+		}
+		else break;
+	}
+	return ret;
+}
 
 
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
+
 	while (1) {
 		cin >> N;
 		if (N == 0)break;
-		vector<string>s(N+1);
-		FOR(i, 0, N)cin >> s[i];
-		int index = 0;
-		ans = expr0(s, index);
-		cout << ans << endl;
+		if (N == 1) { int ans; cin >> ans; cout << ans << endl; continue; }
+		vector<string>S(N + 5);
+		FOR(i, 0, N)cin >> S[i];
+		if (S[0][0] == '*') cout << f(true, 1, 1, S);
+		else cout << f(false, 1, 1, S);
+		cout << endl;
 	}
+
 	return 0;
 }
