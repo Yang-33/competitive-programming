8815725ce4f856358a3a3f83ea0716c0feb2cb79
Yang-33 <kasai.yuta0810@gmail.com>
2017-10-07 12:11:01 +0900

    solve codeforces 439 div2 D!

---
+++ codeforces/439_D_DIV2_CODEFORCES.cpp
@@ -0,0 +1,50 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2017/10/06  Problem: CodeForces439 D div2 / Link: http://codeforces.com/contest/869/problem/D  ----- */
+/* ------問題------
+
+完全二分木を与えられた辺でつなぐ。
+Pathの総数は？
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+方針に比べて実装が無理すぎる
+
+----解説ここまで---- */
+
+LL N;
+
+LL ans = 0LL;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	cin >> N;
+	cout << ans << "\n";
+
+	return 0;
+}
