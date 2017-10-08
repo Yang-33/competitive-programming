e1841fb1e761d7e7e32869642033e2a816932542
Yang-33 <kasai.yuta0810@gmail.com>
2017-10-08 17:59:20 +0900

    solve codeforces421 C

---
+++ codeforces/421_C_DIV2_CODEFORCES.cpp
@@ -0,0 +1,49 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
+#define FORR(i,s,e) for(ll (i)=(s);(i)>(e);(i)--)
+#define debug(x) cout<<#x<<": "<<x<<endl
+#define mp make_pair
+#define pb push_back
+const ll MOD = 1000000007;
+const int INF = 1e9;
+const ll LINF = 1e16;
+const double PI = acos(-1.0);
+int dx[8] = { 0,0,1,-1,1,1,-1,-1 };
+int dy[8] = { 1,-1,0,0,1,-1,1,-1 };
+
+/* -----  2017/06/27  Problem: CodeForces421 C div2 / Link: http://codeforces.com/contest/820/problem/C  ----- */
+/* ------問題------
+
+
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+解けない問題だったため、Unrated !w
+DPでは解けそうで解けないことが分かったのでDを考えていたけど本当でウケた
+
+----解説ここまで---- */
+
+ll N;
+
+ll ans = 0LL;
+
+int main()
+{
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+	ll a, b, l, r, r;
+	cin >> a >> b >> l >> r;
+
+
+
+	cout << ans << endl;
+
+	return 0;
+}
