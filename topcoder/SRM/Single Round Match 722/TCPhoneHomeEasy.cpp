0c6f57b6f8214b908b10e7f62f1b009a22736788
Yang-33 <kasai.yuta0810@gmail.com>
2017-10-12 22:24:09 +0900

    solve SRM 722 med

---
+++ topcoder/SRM/Single Round Match 722/TCPhoneHomeEasy.cpp
@@ -0,0 +1,198 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <algorithm>
+#include <set>
+#include <sstream>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <list>
+#include <fstream>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+#define foreach(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); itr++)
+/* -----  xtimex  Problem: SRM 722 div2 med TCPhoneHomeEasy / Link:  ----- */
+/* ------問題------
+
+10^digitの電話番号がある。
+特別なprefixのものは使えない。有効な電話番号は？
+ただし重複しない。
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+重複しないのでね、全部消していいよね
+重複する場合はsortして部分文字列が重複しているものを取り除いてから同様の処理をすればよい。
+
+ ----解説ここまで---- */
+
+class TCPhoneHomeEasy 
+{
+    public:
+	
+    int validNumbers(int digits, vector<string> specialPrefixes) 
+	{
+
+		LL sum = pow(10, digits);
+
+		FOR(i, 0, SZ(specialPrefixes)) {
+			int len = SZ(specialPrefixes[i]);
+			LL sub = pow(10, digits - len);
+			sum -= sub;
+
+		}
+        return sum;
+    }
+};
+
+/*
+
+struct TCPhoneHomeEasy {
+  int digits;
+  vector<string> specialPrefixes;
+  int validNumbers(int _digits, vector<string> _specialPrefixes) {
+    digits = _digits, specialPrefixes = _specialPrefixes;
+    return 0;
+  }
+};
+
+*/
+
+// CUT begin
+ifstream data("TCPhoneHomeEasy.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int digits, vector<string> specialPrefixes, int __expected) {
+    time_t startClock = clock();
+    TCPhoneHomeEasy *instance = new TCPhoneHomeEasy();
+    int __result = instance->validNumbers(digits, specialPrefixes);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int digits;
+        from_stream(digits);
+        vector<string> specialPrefixes;
+        from_stream(specialPrefixes);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(digits, specialPrefixes, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1507806257;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 400 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "TCPhoneHomeEasy (400 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
