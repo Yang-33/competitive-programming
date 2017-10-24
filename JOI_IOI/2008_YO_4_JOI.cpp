27e3dd02526d110805cca5ae114ff270ff8b5e75
Yang-33 <kasai.yuta0810@gmail.com>
2017-10-24 22:00:20 +0900

    solve joi2008 yo 4

--- JOI_IOI/2008_YO_4_JOI.cpp
+++ JOI_IOI/2008_YO_4_JOI.cpp
@@ -1,61 +1,93 @@
-#include <iostream>
-#include <set>
-#include <unordered_set>
+#include "bits/stdc++.h"
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> pii;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2017/03/31  Problem: JOI予選 2008 4  / Link: http://www.ioi-jp.org/joi/2007/2008-yo-prob_and_sol/2008-yo-t4/2008-yo-t4.html  ----- */
+/* -----  2017/10/18  Problem: 20_a  / Link: http://joi2010yo.contest.atcoder.jp/tasks/joi2010yo_d  ----- */
 /* ------問題------
 
-あなたは星空の写真の中から星座を探している．写真には必ず，探したい星座と同じ形・向き・大きさの図形がちょうど一つ含まれている．
-ただし，写真の中には星座を構成する星以外に余分な星が写っている可能性がある．
-
-例えば，図 1 の星座は図 2 の写真に含まれている（丸で囲んで示した）．与えられた星座の星の座標を x 方向に 2， y 方向に －3 だけ平行移動すると写真の中の位置になる．
-
-探したい星座の形と写真に写っている星の位置が与えられたとき，星座の座標を写真の中の座標に変換するために平行移動するべき量を答えるプログラムを書け．
+花子さんは n 枚（4 ≦ n ≦ 10）のカードを並べて遊んでいる． それぞれのカードには 1 以上 99 以下の整数が 1 つずつ書かれている． 花子さんは，これらのカードの中から k 枚（2 ≦ k ≦ 4）を選び， 横一列に並べて整数を作ることにした． 花子さんは全部で何種類の整数を作ることができるだろうか．
+例えば，1, 2, 3, 13, 21 の 5 枚のカードが与えられ，そこから 3 枚を選び整数を作ることを考える． 2, 1, 13 をこの順に並べると，整数 2113 を作ることができる． また，21, 1, 3 をこの順に並べても，同じ整数 2113 を作ることができる． このように，異なるカードの組み合わせから同じ整数が作られることもある．
+n 枚のカードに書かれた整数が与えられたとき， その中から k 枚を選び，横一列に並べることで作ることができる整数の個数を求めるプログラムを作成せよ．
 
 -----問題ここまで----- */
 /* -----解説等-----
 
-まずx[0],y[0]がどこに移動したかを設定し、その平行移動距離に対して (元の星座)＋(移動量)=(星)が成り立っていればよく、
-愚直に行って M*N*M,　探索を線形ではなく二分探索で行えばM*N*logN となる。どっちでもまにあう。
-JOI,set大活躍。
+4つしかないので筋肉？
 
 ----解説ここまで---- */
 
-ll M, N;
+LL N,K;
 
-ll ans = 0LL;
+LL ans = 0LL;
 
 int main() {
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-    int x[200], y[200];
-    cin >> M;
-    FOR(i, 0, M)cin >> x[i] >> y[i];
-    cin >> N;
-    set<pii>s;
-    FOR(i, 0, N) {
-        int a, b; cin >> a >> b;
-        s.insert(pii(a, b));
-    }
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
-    for (pii ss : s) {
-        bool flag = true;
-        FOR(j, 1, M) {
-            if (!s.count(pii(ss.first + x[j] - x[0], ss.second + y[j] - y[0]))) {
-                flag = false;
-                break;
-            }
-        }
-        if (flag)cout << ss.first - x[0] << " " << ss.second - y[0] << endl;
+	cin >> N>>K;
+	string a[10];
+	FOR(i, 0, N) {
+		cin >> a[i];
+	}
+	set<string >se;
+	if (K == 2) {
+		FOR(i, 0, N) {
+			FOR(j, 0, N) {
+				if (i == j)continue;
+				se.insert(a[i] + a[j]);
+			}
+		}
+	}
+	else if (K == 3) {
+		FOR(k, 0, N) {
+			FOR(i, 0, N) {
+				FOR(j, 0, N) {
+					if (k == i)continue;
+					if (k == j)continue;
+					if (i == j)continue;
+					se.insert(a[k]+a[i] + a[j]);
+				}
+			}
+		}
+	}
+	else if (K == 4) {
+		FOR(l,0,N)
+		FOR(k, 0, N) {
+			FOR(i, 0, N) {
+				FOR(j, 0, N) {
+					if (l == k)continue;
+					if (l == i)continue;
+					if (l == j)continue;
+					if (k == i)continue;
+					if (k == j)continue;
+					if (i == j)continue;
+					se.insert(a[l]+a[k] + a[i] + a[j]);
+				}
+			}
+		}
+	}
+	ans = SZ(se);
+	cout << ans << "\n";
 
-    }
-
-
-    return 0;
+	return 0;
 }
