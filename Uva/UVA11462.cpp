8282911110b91886156020781596fc20525b2e5d
Yang-33 <kasai.yuta0810@gmail.com>
2017-10-24 22:36:42 +0900

    solve uva 11462

---
+++ Uva/UVA11462.cpp
@@ -0,0 +1,53 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2017/10/20  Problem: Uva 11462  / Link:   ----- */
+/* ------問題------
+
+age sort
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+? sort
+
+----解説ここまで---- */
+
+int N;
+
+LL ans = 0LL;
+
+int main() {
+
+	while (scanf("%d", &N), N) {
+		VI a(N);
+		FOR(i, 0, N)scanf("%d", &a[i]);
+		SORT(a);
+		char c;
+		FOR(i, 0, N)c=((i!=N-1)?' ':'\n'),printf("%d%c", a[i],c);
+	}
+
+
+	return 0;
+}
