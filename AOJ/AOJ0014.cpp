e00a0b0e490da270b85c1ce3c1079b79cd66f31b
Yang-33 <kasai.yuta0810@gmail.com>
2017-10-29 23:05:47 +0900

    solve AOJ0014

---
+++ AOJ/AOJ0014.cpp
@@ -0,0 +1,53 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2017/10/03  Problem: AOJ 0014  / Link: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0014&lang=jp ----- */
+/* ------問題------
+
+分割積分
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+指定されたことをやる
+
+----解説ここまで---- */
+
+LL d;
+
+LL ans = 0LL;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	while (cin >> d) {
+		ans = 0;
+		FOR(i, 1, 600 / d) {
+			ans += (LL)pow(i*d, 2)*d;
+		}
+		cout << ans << "\n";
+	}
+	return 0;
+}
