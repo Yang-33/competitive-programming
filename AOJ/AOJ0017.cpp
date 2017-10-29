444050706b9f4867882caddbd7cd2ef790c06478
Yang-33 <kasai.yuta0810@gmail.com>
2017-10-29 23:07:54 +0900

    solve AOJ0017

---
+++ AOJ/AOJ0017.cpp
@@ -0,0 +1,65 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2017/10/03  Problem: AOJ 0017  / Link: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0017&lang=jp ----- */
+/* ------問題------
+
+シーザー暗号
+the this that のどれかが含まれるものが答えという情報がある
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+" the "とするのははじが見れないので困るが、これで通るのはダメそう。
+theirとかはoutなケースのはずなので、
+"the "," the "," the"で場合分けが必要な気がする。
+
+----解説ここまで---- */
+
+LL N;
+string Ss;
+LL ans = 0LL;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	while (getline(cin, Ss)) {
+
+		FOR(j, 0, 26) {
+			if (Ss.find("the") != -1 || Ss.find("that") != -1 || Ss.find("this") != -1) {
+				break;
+			}
+			FOR(i, 0, SZ(Ss)) {
+				if (isalpha(Ss[i])) {
+					if (Ss[i] == 'z')Ss[i] = 'a';
+					else Ss[i]++;
+				}
+			}
+		}
+		cout << Ss << endl; Ss.clear();
+	}
+	
+	return 0;
+}
