2b02c7b42feef01978f8d48c52e1c494a217ecf4
Yang-33 <kasai.yuta0810@gmail.com>
2017-10-29 23:02:28 +0900

    solve AOJ0002

---
+++ AOJ/AOJ0002.cpp
@@ -0,0 +1,58 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2017/10/03  Problem: AOJ 0002  / Link: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0002&lang=jp ----- */
+/* ------問題------
+2数の和の桁数
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+2数の和の桁数
+log(10)SUM +1
+
+----解説ここまで---- */
+
+LL N;
+
+LL ans = 0LL;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	int a, b;
+	while (cin >> a >> b) {
+		int sum = a + b;
+		ans = 0;
+		while (sum) {
+			ans++;
+			sum /= 10;
+		}
+		cout << ans << "\n";
+	}
+	
+
+	return 0;
+}
