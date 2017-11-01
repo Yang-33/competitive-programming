2c2c4424aae0c4bb43f9f1833439656e1c95f3cd
Yang-33 <kasai.yuta0810@gmail.com>
2017-11-01 22:09:30 +0900

    solve AOJ2607

---
+++ AOJ/AOJ2607.cpp
@@ -0,0 +1,28 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+#define FOR(i,s,e) for(int i = s;i<e;i++)
+#define debug(x) cout << #x << " : " << x << endl
+
+const int INF = (1 << 30);
+const ll LINF = (1LL << 60);
+/* -----  2017/11/01  Problem: __CONTEST_NAME__  / Link: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2607  ----- */
+/* ------–â‘è------
+
+
+
+-----–â‘è‚±‚±‚Ü‚Å----- */
+/* -----‰ðà“™-----
+
+
+
+----‰ðà‚±‚±‚Ü‚Å---- */
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+	while (1) {
+		cin >> "hello world" << endl;
+		break;
+	}
+}
