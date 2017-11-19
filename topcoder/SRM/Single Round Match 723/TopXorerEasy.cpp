2de9f7232346b2c75dbf64017e9984c6ce76b6d2
Yang-33 <kasai.yuta0810@gmail.com>
2017-11-19 15:47:20 +0900

    solve SRM723 div2 med

---
+++ topcoder/SRM/Single Round Match 723/TopXorerEasy.cpp
@@ -0,0 +1,212 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <algorithm>
+#include <set>
+#include <sstream>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <list>
+#include <fstream>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+#define foreach(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); itr++)
+
+/* -----  2017/10/02  Problem: SRM  / Link:   ----- */
+/* ------–â‘è------
+
+-----–â‘è‚±‚±‚Ü‚Å----- */
+/* -----‰ðà“™-----
+
+----‰ðà‚±‚±‚Ü‚Å---- */
+
+
+
+string to_any_base_String(int base, LL val) //“ñi”•\Ž¦
+{
+	const int size = 32;
+	int index = size - 1;
+	string str(size, '0');
+	while (val != 0) {
+		str[index] = '0' + val%base;
+		val /= base;
+		index--;
+	}
+	return str;
+}
+
+
+class TopXorerEasy 
+{
+    public:
+	
+    int maximalRating(int A, int B, int C) 
+	{
+		VL a(3); a[0] = A; a[1] = B; a[2] = C;
+		sort(ALL(a));
+		int ans = 0;
+		FORR(i, 31, -1) {
+			int c = 0;
+			FOR(j, 0, 3) {
+				if ((a[j]&(1<<i)) > 0) {
+					c++;
+				}
+			}
+			if (c > 1) {
+				ans |= (1 << (i + 1)) - 1;
+				return ans;
+			}
+			else if (c == 1) {
+				ans |= 1 << i;
+			}
+
+		}
+
+
+		return ans;
+    }
+};
+
+/*
+
+struct TopXorerEasy {
+  int A;
+  int B;
+  int C;
+  int maximalRating(int _A, int _B, int _C) {
+    A = _A, B = _B, C = _C;
+    return 0;
+  }
+};
+
+*/
+
+// CUT begin
+ifstream data("TopXorerEasy.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int A, int B, int C, int __expected) {
+    time_t startClock = clock();
+    TopXorerEasy *instance = new TopXorerEasy();
+    int __result = instance->maximalRating(A, B, C);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int A;
+        from_stream(A);
+        int B;
+        from_stream(B);
+        int C;
+        from_stream(C);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(A, B, C, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1511024816;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "TopXorerEasy (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
