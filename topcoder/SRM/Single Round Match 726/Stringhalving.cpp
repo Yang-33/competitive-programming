19b7eb743d467d48d528e4b7ebf25861f071affe
Yang-33 <kasai.yuta0810@gmail.com>
2017-12-20 04:31:40 +0900

    SRM726 div2

---
+++ topcoder/SRM/Single Round Match 726/Stringhalving.cpp
@@ -0,0 +1,195 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <algorithm>
+#include <set>
+#include <sstream>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <list>
+#include <fstream>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+#define foreach(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); itr++)
+
+/* -----  2017/10/02  Problem: SRM  / Link:   ----- */
+/* ------–â‘è------
+
+–Y‚ê‚½
+
+-----–â‘è‚±‚±‚Ü‚Å----- */
+/* -----‰ðà“™-----
+
+.
+
+----‰ðà‚±‚±‚Ü‚Å---- */
+
+class StringHalving
+{
+public:
+
+	string lexsmallest(string s)
+	{
+		string al = "abcdefghijklmnopqrstuvwxyz";
+		FOR(i, 0, 26) {
+			int ok = 1;
+			string st = s;
+			VI cnt(26, 0);
+			VI can(52, 0);
+			int pos = -1;
+			FOR(j, 0, SZ(s))if (s[j] - 'a' == i) {
+				pos = j; break;
+			}
+			if (pos == -1)continue;
+			FOR(j, 0, pos) {
+				cnt[s[j] - 'a']++;
+				if (cnt[s[j] - 'a'] == 2) {
+					ok = 0;
+				}
+			}
+			if (ok) {
+				return string(1, al[i]);
+			}
+
+		}
+	}
+};
+
+/*
+
+struct StringHalving {
+  string s;
+  string lexsmallest(string _s) {
+	s = _s;
+	return "";
+  }
+};
+
+*/
+
+// CUT begin
+ifstream data("Stringhalving.sample");
+
+string next_line() {
+	string s;
+	getline(data, s);
+	return s;
+}
+
+template <typename T> void from_stream(T &t) {
+	stringstream ss(next_line());
+	ss >> t;
+}
+
+void from_stream(string &s) {
+	s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+	stringstream s;
+	s << t;
+	return s.str();
+}
+
+string to_string(string t) {
+	return "\"" + t + "\"";
+}
+
+bool do_test(string s, string __expected) {
+	time_t startClock = clock();
+	StringHalving *instance = new StringHalving();
+	string __result = instance->lexsmallest(s);
+	double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+	delete instance;
+
+	if (__result == __expected) {
+		cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+		return true;
+	}
+	else {
+		cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+		cout << "           Expected: " << to_string(__expected) << endl;
+		cout << "           Received: " << to_string(__result) << endl;
+		return false;
+	}
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+	int cases = 0, passed = 0;
+	while (true) {
+		if (next_line().find("--") != 0)
+			break;
+		string s;
+		from_stream(s);
+		next_line();
+		string __answer;
+		from_stream(__answer);
+
+		cases++;
+		if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+			continue;
+
+		cout << "  Testcase #" << cases - 1 << " ... ";
+		if (do_test(s, __answer)) {
+			passed++;
+		}
+	}
+	if (mainProcess) {
+		cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+		int T = time(NULL) - 1513699204;
+		double PT = T / 60.0, TT = 75.0;
+		cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+		cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	cout.setf(ios::fixed, ios::floatfield);
+	cout.precision(2);
+	set<int> cases;
+	bool mainProcess = true;
+	for (int i = 1; i < argc; ++i) {
+		if (string(argv[i]) == "-") {
+			mainProcess = false;
+		}
+		else {
+			cases.insert(atoi(argv[i]));
+		}
+	}
+	if (mainProcess) {
+		cout << "Stringhalving (250 Points)" << endl << endl;
+	}
+	return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
