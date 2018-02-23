f914cf665b8378e3ee067145e46baaf0ca3553e0
Yang-33 <kasai.yuta0810@gmail.com>
2018-02-23 14:41:35 +0900

    ABC070 D ダイクストラ

--- atcoder/ABC/070_ABC_D.cpp
+++ atcoder/ABC/070_ABC_D.cpp
@@ -1,7 +1,10 @@
-﻿#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long LL;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
 #define ALL(a)  begin((a)),end((a))
 #define RALL(a) (a).rbegin(), (a).rend()
@@ -19,7 +22,7 @@ const int INF = 1e9;                          const LL LINF = 1e16;
 const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2017/10/02  Problem: Nagoya_univ_contest16_d  / Link: http://abc070.contest.atcoder.jp/tasks/abc070_d  ----- */
+/* -----  2018/02/15  Problem: 070_abc_d / Link: https://abc070.contest.atcoder.jp/tasks/abc070_d?lang=en  ----- */
 /* ------問題------
 
 N 頂点の木が与えられます。
@@ -27,71 +30,74 @@ N 頂点の木が与えられます。
 i(1≦i≦N−1) 番目の辺は 頂点 ai と 頂点 bi を距離 ci で結びます。
 
 また、Q 個の質問クエリと整数 K が与えられます。
-
 j(1≦j≦Q) 番目の質問クエリでは、頂点 xj から 頂点 K を経由しつつ、頂点 yj まで移動する場合の最短経路の距離を求めてください。
 
 -----問題ここまで----- */
 /* -----解説等-----
 
-木の上でKを経由するのは、Kから移動するのと変わらない。
-したがってダイクストラ or dfsを頂点Kからやって、dist[x]+dist[y]をすればよい。
+要はKからの距離を求めてと書いてあるので、d[K->x] + d[K->y]がわかれば良い。
+一回ダイクストラすればいいよね
 
 ----解説ここまで---- */
+void D(vector<vector<PLL>>& G, VL&dist, int s, int t) {
 
-LL N, Q, K;
-typedef pair<long long, long long >PLL;
-LL ans = 0LL;
-vector<vector<PLL>> G;
+	const int INF = INT_MAX;
+	const long long INIT_NUM = LLONG_MAX / 10;// dist の型
+#define DijkV 200020
 
-#define DijkV 100005
-long long dist[DijkV];
-void Dijkstra(int s, int t) {
-	const long long INIT_NUM = LLONG_MAX / 10;
-	FOR(i, 0, DijkV)dist[i] = INIT_NUM;
-
-	dist[s] = 0;
-	priority_queue<PLL, vector<PLL>, greater<PLL>> que;
-	que.push(PLL(0LL, s));
+	priority_queue<PLL, vector<PLL>, greater<PLL>> que; //優先度付きqueue 降順(距離、頂点)
+	que.push(PLL(0LL, s)); // push(距離,頂点)
 
 	while (!que.empty()) {
-		PLL p = que.top(); que.pop();
-		LL v;  LL d;
+		PLL p = que.top(); que.pop(); //queueのデータ構造
+		int v; /* 頂点*/ long long d; /* 頂点vまでの距離 */
 		tie(d, v) = p;
 
-		if (d > dist[v]) continue;
+		if (d > dist[v]) continue; //最適でないならば考慮しない
 
-		FOR(i, 0, (int)G[v].size()) {
-			int nv = G[v][i].first;
+		FOR(i, 0, (int)G[v].size()) { //頂点vからはi本の辺が存在
+			int nv = G[v][i].first; // v->nv
 			if (dist[nv] > dist[v] + G[v][i].second) {
 				dist[nv] = dist[v] + G[v][i].second;
-				que.push(PLL(dist[nv], nv));
+				que.push(PLL(dist[nv], nv)); //push(距離,頂点) 
 			}
 		}
 	}
+
+#undef DijkV
 }
+
+LL N;
+
+LL ans = 0LL;
 
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
 	cin >> N;
-	G = vector<vector<PLL>>(N+1);
-	int a, b, c;
-
-	FOR(i, 0, N - 1) {
+	vector<vector<PLL>> G(N);
+	FOR(i, 0, N-1) {
+		int a, b, c;
 		cin >> a >> b >> c;
+		a--, b--;
 		G[a].push_back(PLL(b, c));
 		G[b].push_back(PLL(a, c));
 	}
-	cin >> Q >> K;
-	Dijkstra(K, 0);
 
-	int x, y;
+	int Q, K; cin >> Q >> K;
+	K--;
+	VL dist(N, LINF);
+	dist[K] = 0;
+
+	D(G, dist, K,N);
 	FOR(i, 0, Q) {
+		int x, y;
 		cin >> x >> y;
-		cout << dist[x] + dist[y] << endl;
+		x--; y--;
+		ans = dist[x] + dist[y];
+		cout << ans << "\n";
 	}
-
 
 	return 0;
 }
