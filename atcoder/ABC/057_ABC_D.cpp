b74c72ba349cf2f02278685f900c7546da6d18fc
Yang-33 <kasai.yuta0810@gmail.com>
2018-02-23 14:40:19 +0900

    solve ABC057 D adhoc

--- atcoder/ABC/057_ABC_D.cpp
+++ atcoder/ABC/057_ABC_D.cpp
@@ -1,14 +1,29 @@
-#include<iostream>
-#include<algorithm>
-#include<cstdio>
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-#define debug(x) cout<<#x<<": "<<x<<endl
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/03/27  Problem: ABC 057 D / Link: http://abc057.contest.atcoder.jp/tasks/abc057_d ----- */
-/* ------蝠城｡---
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2018/02/15  Problem: 057_abc_d / Link: https://abc057.contest.atcoder.jp/tasks/abc057_d?lang=en  ----- */
+/* ------問題------
 
 N 個の品物が与えられます。
 i 番目の品物の価値は vi(1≦i≦N) です。
@@ -19,76 +34,73 @@ i 番目の品物の価値は vi(1≦i≦N) です。
 -----問題ここまで----- */
 /* -----解説等-----
 
-典型で解こうとしてしまったので半分全列挙や二分探索だと決めつけてしまったが、
-ただの平均の取り方の問題。
-降順に並べて先頭から撮っていく場合、A個とるのはかなり最適である。というのもA+1個めがA個目よりも小さいときには平均値が下がるので
-これは最適ではなくなるからである。
-A+1個めが前のものと同じであるときに組み合わせを考える。
-また、選んだk個≧Aがすべて同じときには別の場合分けをすればよい。
+選択区間は二種類しかなく、これを場合分けする
+1:全部A->たくさん選べるね
+2:そうではない->最小の要素の選択
 
 ----解説ここまで---- */
 
-ll N, A, B;
-ll v[60];
-ll ans = 0;
-ll comb[100][100];
+#define nCk_MAX 100
+long long  comb[nCk_MAX + 1][nCk_MAX + 1];
+/* nCk を求める */
+void calcomb() {
+	int  i, j;
+	for (i = 0; i <= nCk_MAX; i++) {
+		for (j = 0; j <= i; j++) {
+			if (j == 0 || j == i)
+				comb[i][j] = 1;
+			else
+				comb[i][j] = comb[i - 1][j - 1] + comb[i - 1][j];
+		}
+	}
+}
+#undef nCk_MAX
+LL N,A,B;
 
-double ave = 0;
+LL ans = 0LL;
 
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
-    cin >> N >> A >> B;
-    FOR(i, 0, N) {
-        cin >> v[i];
-    }
+	cin >> N>>A>>B;
+	VL a(N);
+	FOR(i, 0, N) {
+		cin >> a[i];
+	}
 
+	calcomb();
+	double ave;
+	SORT(a);
+	reverse(ALL(a));
+	if (a[0] == a[A - 1]) {
+		int cnt = 0;
+		FOR(i, 0, N) {
+			if (a[0] == a[i])cnt++;
+		}
+		FOR(i, A, min((LL)cnt,B)+ 1) {
+			ans += comb[cnt][i];
+		}
+		ave = a[0];
+	}
+	else {
+		// A個が良いが、最後の選択ができる
+		int cnt = 0;
+		FOR(i, 0, N) {
+			if (a[A - 1] == a[i])cnt++;
+		}
+		int cnt2 = 0;
+		LL sum = 0;
+		FOR(i, 0, A) {
+			sum += a[i];
+			if (a[A - 1] == a[i])cnt2++;
+		}
+		ans = comb[cnt][cnt2];
+		ave = (double)sum / A;
 
-    for (int i = 0; i <= 60; i++) {
-        for (int j = 0; j <= i; j++) {
-            if (j == 0 || j == i)
-                comb[i][j] = 1;
-            else
-                comb[i][j] = comb[i - 1][j - 1] + comb[i - 1][j];
-        }
-    }
+	}
+	cout <<fixed<<setprecision(10)<< ave << endl;
+	cout << ans << "\n";
 
-
-    sort(v, v + N);
-    reverse(v, v + N);
-
-    if (v[0] != v[A - 1]) {
-        int cnt = 0, ne = 0;
-        FOR(i, 0, N) {
-            if (v[A - 1] == v[i]) {
-                cnt++;
-                if (i < A) {
-                    ne++;
-                }
-            }
-            if (i < A)ave += v[i];
-        }
-        //debug(cnt);
-        ave /= A;
-        ans = comb[cnt][ne];
-    }
-    else if (v[0] == v[A - 1]) {
-        int cnt = 0;
-        FOR(i, 0, N) {
-            if (v[0] == v[i])cnt++;
-        }
-        ave = v[0];
-        FOR(i, A, B + 1) {
-            if (cnt>=i) {
-                ans += comb[cnt][i];
-            }
-        }
-
-    }
-
-    printf("%.10f\n", ave);
-    cout << ans << endl;
-    return 0;
+	return 0;
 }
