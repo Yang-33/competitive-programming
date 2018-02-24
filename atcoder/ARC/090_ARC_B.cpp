2803c42c5d5a0446e4f152d0c33fb90dc6e6216b
Yang-33 <kasai.yuta0810@gmail.com>
2018-02-24 14:21:05 +0900

    solve ARC090 B

--- atcoder/ARC/090_ARC_B.cpp
+++ atcoder/ARC/090_ARC_B.cpp
@@ -22,7 +22,7 @@ const int INF = 1e9;                          const LL LINF = 1e16;
 const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2018/01/29  Problem: ARC 090 B / Link: http://arc090.contest.atcoder.jp/tasks/arc090_b  ----- */
+/* -----  2018/02/23  Problem: 090_arc_b / Link: https://abc087.contest.atcoder.jp/tasks/arc090_b?lang=en  ----- */
 /* ------問題------
 
 x 軸上に N 人の人が立っています。 人 i の位置を xi とします。 任意の i に対して、xi は 0 以上 109 以下の整数です。 同じ位置に複数の人が立っていることもありえます。
@@ -32,76 +32,53 @@ x 軸上に N 人の人が立っています。 人 i の位置を xi としま�
 -----問題ここまで----- */
 /* -----解説等-----
 
-相対値でいいので、一定の距離が決まっていれば反対側には負の辺を張れば良い。
-連結とは限らない。
-
-具体的には一箇所値を決めたら、その相対値を決定していく。
-もし決めた値にアクセスするときは矛盾していないかを確認すれば良い。
-
-別解として重み付きUFがあるらしい。(理解していない)
+この前やった
+相対的な距離は重み付きUFでもできるけどグラフとしては逆辺に負のコストを与えれば良い。
 
 ----解説ここまで---- */
 
 LL N, M;
 
-LL ans = 0LL;
+int ans;
 
-void dfs(int v, int p, LL sum, vector<vector<PLL>>&G, VI& checked, VL &we) {
-
-	//debug(v);
-	checked[v] = 1;
-
+void dfs(int v, int p, LL d, VL& dist, VI& visit, vector<vector<PLL>>& G) {
+	if (!visit[v])dist[v] = d, visit[v] = 1;
 	FOR(i, 0, SZ(G[v])) {
 		int nx = G[v][i].first;
-		LL pc = G[v][i].second;
-		if (checked[nx]) {
-			if (we[nx] != sum + pc) {
-				ans = 0; return;
+		LL cost = G[v][i].second;
+		if (nx != p) {
+			if (visit[nx]) {
+				if (dist[v] + cost != dist[nx])ans = 0;
 			}
-			else { // ok
-				;
+			else {
+				dfs(nx, v, d + cost, dist, visit, G);
 			}
-		}
-		else {
-			we[nx] = we[v] + pc;
-			dfs(nx, v, we[nx], G, checked, we);
+
 		}
 	}
-
 }
-
 
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
 	cin >> N >> M;
-	VL L(M,0), R(M,0), D(M,0);
-	VI checked(N, 0);
-	VL we(N, 0);
-	vector<vector<PLL>> G(N, vector<PLL>());
-
+	vector<vector<PLL>> G(N);
 	FOR(i, 0, M) {
-		cin >> L[i] >> R[i] >> D[i];
-		L[i]--;
-		R[i]--;
-		G[L[i]].push_back(PLL(R[i], D[i]));
-		G[R[i]].push_back(PLL(L[i], -D[i]));
+		int a, b, c;
+		cin >> a >> b >> c;
+		a--, b--;
+		G[a].push_back(PLL(b, c));
+		G[b].push_back(PLL(a, -c));
 	}
+	VL dist(N, 0);
+	VI visit(N, 0);
 	ans = 1;
-	/*debug(ans);
-	we[L[0]] = 0;*/
-
 	FOR(i, 0, N) {
-		if (checked[i] == 0)
-			dfs(i, -1, 0, G, checked, we);
+		if (!visit[i])dfs(i, -1, 0, dist, visit, G);
 	}
-	if (ans == 1) {
-		cout << "Yes" << endl;
-	}
-	else {
-		cout << "No" << endl;
-	}
+
+	cout << (ans==1 ? "Yes" : "No") << "\n";
 
 	return 0;
 }
