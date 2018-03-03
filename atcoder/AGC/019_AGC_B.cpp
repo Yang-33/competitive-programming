ef7ad897cfd48011c21c7a7ce51bfe75f132eee8
Yang-33 <kasai.yuta0810@gmail.com>
2018-03-03 23:31:39 +0900

    solve AGC019 B 文字列 反転

--- atcoder/AGC/019_AGC_B.cpp
+++ atcoder/AGC/019_AGC_B.cpp
@@ -1,4 +1,4 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
 using VS = vector<string>;    using LL = long long;
@@ -22,10 +22,9 @@ const int INF = 1e9;                          const LL LINF = 1e16;
 const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2017/10/04  Problem: Nagoya_univ_contest_17_f  / Link: http://agc019.contest.atcoder.jp/tasks/agc019_b  ----- */
+/* -----  2018/03/03  Problem: 019_agc_b / Link: https://agc019.contest.atcoder.jp/tasks/agc019_b ----- */
 /* ------問題------
 
-AGC019
 英小文字からなる文字列 A=A1A2…An があります。
 あなたは 1≤i≤j≤n であるような任意の二つの添字 i,j を選び、A のうち部分文字列 AiAi+1…Aj を反転することができます。
 この操作は一回まで行うことができます。
@@ -34,7 +33,7 @@ AGC019
 -----問題ここまで----- */
 /* -----解説等-----
 
-部分問題として見れるので、重複数え上げがないようにすればよい。
+アルファベットについての累積和を取ると、部分問題として見れるので、重複数え上げがないようにすればよい。
 [str] +'a'のとき、aを含む反転をして一致するのは反転の区間の左端がaとなる場合である。
 ex: strstrstr|abca| -> strstrstr|abca|
 なぜなら、[str]内の計算で cを右端とした場合にbc->cbを事前に計算終えているからである。
@@ -42,30 +41,30 @@ ex: strstrstr|abca| -> strstrstr|abca|
 これを答えとしてよい。
 この方法では反転するものしか数えないので、全く反転しない場合の数1を加える。
 
+
 ----解説ここまで---- */
 
 LL N;
-int cnt[26][200005];//1 index
+
 LL ans = 0LL;
 
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
+	string s;
+	cin >> s;
 
-	string S;
-	cin >> S;
-	FOR(a, 0, SZ(S)) {
-		FOR(i, 0, 26) {
-			cnt[i][a + 1] += cnt[i][a];
-			if (S[a] - 'a' == i)cnt[i][a + 1]++;
+	VVI cnt(SZ(s) + 1, VI(26, 0));
+	FOR(i, 0, SZ(s)) {
+		FOR(j, 0, 26) {
+			cnt[i + 1][j] += cnt[i][j] + (s[i] - 'a' == j);
 		}
 	}
-	FOR(i, 1, SZ(S)) {
-		ans += (i - cnt[S[i] - 'a'][i]);
+	ans = 1;
+	FOR(i, 0, SZ(s)) {
+		ans += i - cnt[i][s[i] - 'a'];
 	}
-
-	ans++;
 	cout << ans << "\n";
 
 	return 0;
