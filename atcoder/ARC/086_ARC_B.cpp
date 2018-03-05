e84ab2927f07485bf63cfc2c029f7f5690e2b976
Yang-33 <kasai.yuta0810@gmail.com>
2018-03-05 23:40:33 +0900

    solve ARC086 D パズル 累積和の性質

--- atcoder/ARC/086_ARC_B.cpp
+++ atcoder/ARC/086_ARC_B.cpp
@@ -22,82 +22,68 @@ const int INF = 1e9;                          const LL LINF = 1e16;
 const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2017/12/10  Problem: ARC 086 B / Link: http://arc086.contest.atcoder.jp/tasks/arc086_b  ----- */
+/* -----  2018/03/05  Problem: 086_arc_b / Link: https://arc086.contest.atcoder.jp/tasks/arc086_b?lang=en  ----- */
 /* ------問題------
 
-すぬけ君は長さN の数列a を持っています。
-a の (1-indexedでの)i 番目の数はai です。
-
+すぬけ君は長さ N の数列 a を持っています。a の (1-indexedでの) i 番目の数は ai です。
 すぬけ君は以下の操作を何度でも行うことができます。
-操作：1 以上N 以下の整数x,y を選び、ay にax を加算する。
-すぬけ君はこの操作を0 回以上2N 回以下行ってa が下記の条件を満たすようにしたいです。そのような操作手順の一例を示してください。 なお、この問題の制約下で、条件を満たすような操作の手順が必ず存在することが証明できます。
-条件：単調増加列
+操作：1 以上 N 以下の整数 x,y を選び、ay に ax を加算する。
+すぬけ君はこの操作を 0 回以上 2N 回以下行って a が下記の条件を満たすようにしたいです。そのような操作手順の一例を示してください。 なお、この問題の制約下で、条件を満たすような操作の手順が必ず存在することが証明できます。
+
+条件：a1≤a2≤…≤aN
 
 -----問題ここまで----- */
 /* -----解説等-----
 
-まず値の正負が一致すれば嬉しいなあという気持ちになる。
-どちらかに揃えるため、絶対値が大きな値がどちらにあるかを探す。
-先に大きな値を作って、これを2倍しながら、小さい方に加算していけば良い。
-2N回あれば必ずできる。
-思いつくのにとてもじかんがかかった。
-
-累積和の解法は賢いなあと思うなど。
-こちらはならしてから順に足していけば良い。
+N-1回で符号を全部おなじになるように整地する。
+その後N-1回で累積和を取れば良い
 
 ----解説ここまで---- */
 
-LL N, L, R;
+LL N;
+
+LL ans = 0LL;
 
 int main() {
-	ios::sync_with_stdio(false);
 	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
 	cin >> N;
-	vector<LL>v(N);
-	L = 0;
-	R = 0;
-	for (int i = 0; i < N; i++) {
-		cin >> v[i];
-		L = min(L, v[i]);
-		R = max(R, v[i]);
+	VL a(N);
+	FOR(i, 0, N) {
+		cin >> a[i];
 	}
-	vector<pair<int, int>>ans;
-	if (abs(R) > abs(L)) {
-		int index = 0;
-		for (int i = 1; i < N; i++) {// +
-			if (v[i] > v[index]) {
-				index = i;// + max
-			}
-		}
-		ans.push_back({ index,N - 1 });
-		ans.push_back({ index,N - 1 });
-		ans.push_back({ index,N - 1 });
-		for (int i = 1; i < N - 1; i++) {
-			ans.push_back({ N - 1,N - 1 });
-			ans.push_back({ N - 1,i });
-		}
-		ans.push_back({ N - 1,N - 1 });
+	LL Min = LINF;
+	LL Max = -LINF;
+	FOR(i, 0, N) {
+		Min = min(Min, a[i]);
+		Max = max(Max, a[i]);
 	}
-	else {
-		int index = 0;
-		for (int i = 1; i < N; i++) {
-			if (v[i] < v[index]) {
-				index = i;
-			}
+	cout << 2 * N - 1 << endl;
+	if (abs(Max) > abs(Min)) {
+		int pos; // 全部正
+		FOR(i, 0, N) {
+			if (Max == a[i])pos = i;
 		}
-		ans.push_back({ index,0 });
-		ans.push_back({ index,0 });
-		ans.push_back({ index,0 });
-		for (int i = N - 2; i >= 1; i--) {
-			ans.push_back({ 0,0 });
-			ans.push_back({ 0,i });
+		FOR(i, 0, N) {
+			cout << pos + 1 << " " << i + 1 << endl;
 		}
-		ans.push_back({ 0,0 });
+		FOR(i, 0, N - 1) {
+			cout << i + 1 << " " << i + 2 << endl;
+		}
 	}
-	cout << ans.size() << endl;
-	for (auto i : ans) {
-		cout << i.first + 1 << " " << i.second + 1 << endl;
+	else { //全部負
+		int pos;
+		FOR(i, 0, N) {
+			if (Min == a[i])pos = i;
+		}
+		FOR(i, 0, N) {
+			cout << pos + 1 << " " << i + 1 << endl;
+		}
+		FORR(i, N - 2, 0 - 1) {
+			cout << i + 2 << " " << i + 1 << endl;
+		}
 	}
+
 	return 0;
 }
