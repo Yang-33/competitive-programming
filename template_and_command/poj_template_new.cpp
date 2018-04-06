b57cdb8c03170da5923083ad7037e634a62bbf4e
Yuta Kasai <kasai.yuta0810@gmail.com>
2018-04-06 15:03:47 +0900

    for poj template.cpp

---
+++ template_and_command/poj_template_new.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <algorithm>
+#include <set>
+#include <sstream>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <list>
+#include <fstream>
+#include <functional>
+using namespace std;
+
+typedef long long LL;
+typedef vector<int> VI;
+typedef vector<LL> VL;
+typedef vector<VI> VVI;
+typedef vector<VL> VVL;
+typedef pair<int,int> PII;
+typedef pair<LL,LL> PLL;
+typedef vector<string> VS;
+
+#define ALL(a)  a.begin(),a.end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  __MAKE_TIME__  Problem: __PROBLEM__ / Link: __CONTEST_URL__  ----- */
+/* ------問題------
+
+
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+
+
+----解説ここまで---- */
+
+
+int main() {
+	int N;
+	scanf("%d", &N);
+	
+	return 0;
+}
