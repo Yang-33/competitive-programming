446da540207f9452fb836247aa799e3c6cf26205
Yang-33 <kasai.yuta0810@gmail.com>
2018-04-10 23:20:06 +0900

    solve yukicoder034 ダイクストラ

--- Yukicoder/YUKICODER034.cpp
+++ Yukicoder/YUKICODER034.cpp
@@ -1,90 +1,111 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
-#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
-#define debug(x) cout << #x << ": " << x << endl
-#define mp make_pair
-#define pb push_back
-const ll MOD = 1000000007;
-const int INF = 1e8;
-const ll LINF = 1e16;
-const double PI = acos(-1.0);
-int dx[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-int dy[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2017/06/11  Problem: yukicoder034  / Link: http://yukicoder.me/problems/no/34  ----- */
+/* -----  2018/04/10  Problem: yukicoder 034  / Link: http://yukicoder.me/problems/no/034  ----- */
 /* ------問題------
 
+太郎君は砂漠を歩く行商人です。
+太郎君はこれから次の街へ行こうとしています。
 
+砂漠には移動しやすい場所とそうでない場所があり、
+太郎君は長年の経験から、
+その場所に行くとどれくらいの体力を消耗するかを知っています。
+
+砂漠は際限なく続いていますが、太郎君が知っているのは N×N マスの範囲だけで、
+その外側に行くと命の危険があるため絶対に行きません。
+
+いま太郎君は体力 V で (SX,SY) の場所に立っており、次の街は (GX,GY) の場所にあります。
+太郎君は、辺を共有する前後左右の隣接マスへのみ移動することができ、
+今居るマスから隣のマスへ移動するときに1回の移動とみなし、
+さらに、移動した先の砂漠レベル(LXY)分の体力が減ります。
+移動先の砂漠レベルが0の場合、体力値は減りませんが、
+太郎君の体力が 0以下 になった時点で太郎君が死んでしまいます。
+街に着いた瞬間に死んでしまってもいけません。
+
+太郎君は、商品をできるだけ早く捌きたいので、
+「太郎君が死なずに」「最も早く次の街へ着く」には、
+どれくらい時間がかかるか計算してください。
 
 -----問題ここまで----- */
 /* -----解説等-----
 
-移動する分だけ体力が減少してしまう。
-従って更新回数は盤面の大きさを大きく超えることはなく、上からN^2*Vで大きく抑えることができる。
-また体力の減少の制約から、200*9ほどの体力があれば必ずゴールに到達できるため、状態数は10^4*10^4の10%ほどでしかなくなる。
-したがってTLEは回避できる。
+時間を最短距離とみたグラフについて、体力の状態もさまざまなので、体力ごとに頂点を作成する。
+ただし辺のコストが1なのでBFSになる。
+dist(y,x,v):=体力がvで点(y,x)にいるときの最小時間として計算する。
 
-また最小の移動回数が知りたいので、ノードの更新は必要ない。
-これは移動回数について一方通行のダイクストラ法で地点を更新していけばよく、各地点の情報は
-dist[Y][X][残りの体力]:=最小移動回数として遷移していけばよい。
-
-...c++使いこなせてない。
+別解としてDPの添字と目的値を変更するアレで、
+dist(距離,y,x):=必要な最小体力とすると速いらしい。
 
 ----解説ここまで---- */
 
-ll N, V;
-int sx, sy, tx, ty;
-int masu[100][100];
-int dist[100][100][10010];
-ll ans = 0LL;
 
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
-	cin >> N >> V;
-	cin >> sx >> sy >> tx >> ty;
-	sx--; sy--; tx--; ty--;
+	LL N, V; cin >> N >> V;
+	int sy, sx, ty, tx; cin >> sx >> sy >> tx >> ty;
+	sy--, sx--, tx--, ty--;
+	VVI cost(N, VI(N));
 	FOR(i, 0, N) {
-		FOR(j, 0, N)cin >> masu[i][j];
+		FOR(j, 0, N) {
+			cin >> cost[i][j];
+		}
 	}
-	//input
-	FOR(i, 0, 100)FOR(j, 0, 100)FOR(k,0,10010)dist[i][j][k] = INF;
-	
-	using tp = tuple<int, int, int>;
-	queue<tp>q;
-	q.push(tp(sy, sx, V));
-	dist[sy][sx][V] = 0;
-	ans = -1;
-	while (!q.empty()) {
-		//debug(q.size());
-		int y, x, v;
-		tie(y, x, v) = q.front(); q.pop();
-		if (y == ty&&x == tx) {
-			cout << dist[ty][tx][v] << endl;
+	using tp = tuple<int, int, int, int>;
+	queue<tp>pq;
+	vector<VVI>dist(N, VVI(N, VI(V, INF)));
+	dist[sy][sx][V - 1] = 0;
+	pq.push(tp(0, V - 1, sy, sx));
+
+	while (!pq.empty()) {
+		int d, v, y, x;
+		tie(d, v, y, x) = pq.front(); pq.pop();
+		if (dist[y][x][v] < d)continue;
+		if (ty == y&&tx == x) {
+			cout << d << endl;
 			return 0;
 		}
-		FOR(i, 0, 4) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
+		FOR(k, 0, 4) {
+			int ny = y + DY[k], nx = x + DX[k];
 			if (0 <= ny&&ny < N && 0 <= nx&&nx < N) {
-				int nv = v - masu[ny][nx];
-				if (nv > 0 && dist[ny][nx][nv] == INF) {
-					dist[ny][nx][nv] = dist[y][x][v] + 1;
-					q.push(tp(ny, nx, nv));
+				int nv = v - cost[ny][nx];
+				if (nv >= 0) {
+					if (dist[ny][nx][nv] > dist[y][x][v] + 1) {
+						dist[ny][nx][nv] = dist[y][x][v] + 1;
+						pq.push(tp(dist[ny][nx][nv], nv, ny, nx));
+					}
 				}
 			}
 		}
+
 	}
-
-
-	cout << ans << endl;
+	//int ans = INF;
+	//FOR(i, 0, V) {
+	//	ans = min(ans, dist[ty][tx][i]);
+	//}
+	cout <<-1<< "\n";
 
 	return 0;
 }
