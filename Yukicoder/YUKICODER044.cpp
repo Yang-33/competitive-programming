199af13746c50de2a6c64df40548a2bf39e9cef9
Yang-33 <kasai.yuta0810@gmail.com>
2018-04-11 23:37:29 +0900

    solve yukicoder044 DP

--- Yukicoder/YUKICODER044.cpp
+++ Yukicoder/YUKICODER044.cpp
@@ -1,56 +1,63 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
-#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
-#define debug(x) cout << #x << ": " << x << endl
-#define mp make_pair
-#define pb push_back
-const ll MOD = 1000000007;
-const int INF = 1e9;
-const ll LINF = 1e16;
-const double PI = acos(-1.0);
-int dx[8] = {0, 0, 1, -1, 1, 1, -1, -1};
-int dy[8] = {1, -1, 0, 0, 1, -1, 1, -1};
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2017/06/11  Problem: yukicoder044 / Link: http://yukicoder.me/problems/no/44  ----- */
+/* -----  2018/04/11  Problem: yukicoder 044  / Link: http://yukicoder.me/problems/no/044  ----- */
 /* ------問題------
 
+あなたは、Nマスのすごろくをしている。
+毎ターン「１」または「２」前に進むことができる。
+あなたは最初「０」のマスのスタートにいる。
+
+ちょうどNマスに行く方法は何パターンありますか？
 
 
 -----問題ここまで----- */
 /* -----解説等-----
 
-階段状に進むやつとかフィボナッチ数的なやつ。
-絵を描いてわかるタイプ。
-状態を前に遷移させていこう。
+状態の遷移がとてもわかり易い。
+あるマスへの到達は、1,2歩前からのみ。
+dp(i):=iにたどり着くまでの組み合わせ で解ける。
+提出したときにFibonacci numberであることに気がついた。
 
 ----解説ここまで---- */
 
-ll N;
-ll dp[55];
-ll ans = 0LL;
+LL N;
+
+LL ans = 0LL;
 
 int main() {
-  cin.tie(0);
-  ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
-  cin >> N;
-  FOR(i, 0, N+1) {
-	  dp[i] = 0;
-  }
-  dp[0] = 0;
-  dp[1] = 1;
-  dp[2] = 2;
-  FOR(i, 3, N+1) {
-	  dp[i] = dp[i-1] + dp[i-2];
-  }
-  ans = dp[N];
-  cout << ans << endl;
+	cin >> N;
+	VL dp(N + 1, 0);
+	dp[0] = dp[1] = 1;
+	FOR(i, 2, N + 1) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	ans = dp[N];
+	cout << ans << "\n";
 
-  return 0;
+	return 0;
 }
