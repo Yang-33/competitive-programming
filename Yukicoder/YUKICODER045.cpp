502409f5c9f21ba7a7ffebd0e748f2a821711a14
Yang-33 <kasai.yuta0810@gmail.com>
2018-04-11 23:37:35 +0900

    solve yukicoder045 DP

--- Yukicoder/YUKICODER045.cpp
+++ Yukicoder/YUKICODER045.cpp
@@ -1,61 +1,69 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
-#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
-#define debug(x) cout << #x << ": " << x << endl
-#define mp make_pair
-#define pb push_back
-const ll MOD = 1000000007;
-const int INF = 1e9;
-const ll LINF = 1e16;
-const double PI = acos(-1.0);
-int dx[8] = {0, 0, 1, -1, 1, 1, -1, -1};
-int dy[8] = {1, -1, 0, 0, 1, -1, 1, -1};
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2017/06/11  Problem: yukicoder045 / Link: http://yukicoder.me/problems/no/45  ----- */
+/* -----  2018/04/11  Problem: yukicoder 045  / Link: http://yukicoder.me/problems/no/045  ----- */
 /* ------問題------
 
+あなたは、回転寿司にきている。
+お寿司はN皿が順番に流れてくる。N皿のお寿司のそれぞれの美味しさがViで表される。
+流れてくるお寿司が自分の前に来た時に取ることができるが、このお店のルールで、
+連続で皿を取ることが出来ない。
+もちろん、自分の前を過ぎたお寿司も取ることが出来ない。
 
+この時、あなたが得られる美味しさの最大の合計値を求めてください。
+お寿司は一周回ってくることはないとする。
 
 -----問題ここまで----- */
 /* -----解説等-----
 
-dp[ お寿司の番号 ]:=　この番号から始めたときのお寿司の最大おいしさ
-として、お寿司を食べるときは次のお寿司を食べてない状態での最適値を現在の値に加算する。
-お寿司を食べない方が最適な場合もあるので食べない場合、すなわち次を食べる場合の最適値を現在の値にする。
-このため、このMAXを保存して後ろからみていけばよい。
-前からでもできた。後ろ2つ目からとるときは食べてもよいときで、直近の後ろからとるときは食べてはいけないとしてループすればよい。
+
 
 ----解説ここまで---- */
 
-ll N;
-ll v[1010];
-ll dp[1010];
-ll ans = 0LL;
+LL N;
+
+LL ans = 0LL;
 
 int main() {
-  cin.tie(0);
-  ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
-  cin >> N;
-  FOR(i, 0, N) {
-	  cin >> v[i];
-  }
-  FOR(i, 0, 1010)dp[i] = 0;
-  FORR(i, N - 1, -1) {
-	  ll res = 0;
-	  FOR(j, i+2, N) {
-		  res = max(res, dp[j]);
-	  }
-	  dp[i] = max(dp[i + 1], v[i] + res);
-  }
-  ans = dp[0];
-  cout << ans << endl;
+	cin >> N;
+	VI v(N);
 
-  return 0;
+	FOR(i, 0, N) {
+		cin >> v[i];
+	}
+	VVL dp(N + 1, VL(2, 0));
+	FOR(i, 0, N) {
+		dp[i + 1][1] = max(dp[i + 1][1], dp[i][0] + v[i]);
+		dp[i + 1][0] = max(dp[i + 1][0], dp[i][1]);
+		dp[i + 1][0] = max(dp[i + 1][0], dp[i][0]);
+	}
+	ans = max(dp[N][0], dp[N][1]);
+
+	cout << ans << "\n";
+
+	return 0;
 }
