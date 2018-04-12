1cd1f4b77e8ee96cce680440c32a4859b8b3d3ab
Yang-33 <kasai.yuta0810@gmail.com>
2018-04-12 23:04:52 +0900

    solve yukicoder058 DP 確率

--- Yukicoder/YUKICODER058.cpp
+++ Yukicoder/YUKICODER058.cpp
@@ -1,87 +1,102 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
-#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
-#define debug(x) cout << #x << ": " << x << endl
-#define mp make_pair
-#define pb push_back
-const ll MOD = 1000000007;
-const int INF = 1e9;
-const ll LINF = 1e16;
-const double PI = acos(-1.0);
-int dx[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-int dy[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2017/06/11  Problem: yukicoder058 / Link: http://yukicoder.me/problems/no/58  ----- */
+/* -----  2018/04/12  Problem: yukicoder 058  / Link: http://yukicoder.me/problems/no/058  ----- */
 /* ------問題------
 
+太郎君と二郎君はサイコロで勝負することになりました。
+
+太郎君と二郎君は、お互いにN個のサイコロを持ち、一斉に転がします。
+そして、出た目の合計が大きいほうが勝ち、また、合計が等しければ引き分けという取り決めになっています。
+
+ところが太郎君は卑怯にも、イカサマなサイコロを使ってしまいます。
+
+普通のサイコロは、立方体の6面に1から6までの目が刻印されていますが、
+イカサマなサイコロは、立方体の6面に4から6までの目が2つずつ刻印されており、
+1から3の目は絶対に出ません。
+
+二郎君はN個の普通のサイコロを使用しますが、
+太郎君はN個のサイコロのうちK個に、このイカサマなサイコロを使用し、
+(N−K)個は普通のサイコロを使用します。
+
+普通のサイコロもイカサマなサイコロも、6面のうちどの面が出るかは均等であるとしたとき、
+太郎君が「勝つ」確率を求めてください。
 
 
 -----問題ここまで----- */
 /* -----解説等-----
 
-引き分けもある。
-dpで両人のサイコロの出目の確率を
-dp[ 振ったサイコロの数 ][ 出目の総和 ]:=　振った回数に対して出目の総和の確率
-として求めて、出目が相手よりも大きいものを勝つ場合としてカウントする。
+イカサマサイドは場合分けがあるけど、基本的には
+dp(i,sum):=i個サイコロをふってその出目の合計値がsumになる確率
+としてDPしてあげればよい。
+
 
 ----解説ここまで---- */
-
-ll N, K;
-
-double  ans = 0.0;
 
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
+	LL N, K;
 	cin >> N >> K;
+	vector<vector<double>>dpTaro(N + 1, vector <double>(61, 0));
+	vector<vector<double>>dpJiro = dpTaro;
+	dpJiro[0][0] = dpTaro[0][0] = 1;
 
-	double dpa[12][100];
-	FOR(i, 0, 100)FOR(j, 0, 12)dpa[j][i] = 0;
-	dpa[0][0] = 1;
-
+	//Jiro
 	FOR(i, 0, N) {
-		FOR(d, 1, 6 + 1)
-			FORR(j, 70, -1) {
-			dpa[i + 1][d + j] += dpa[i][j]/6.0;
+		FOR(d, 1, 6 + 1) {
+			FORR(sum, 60, 0 - 1) {
+				dpJiro[i + 1][sum + d] += dpJiro[i][sum] / 6.0;
+			}
 		}
 	}
 
-	double dpb[12][100];
-	FOR(i, 0, 12)FOR(j, 0, 100)dpb[i][j] = 0;
-	dpb[0][0] = 1;
+	// Taro
 	FOR(i, 0, N) {
-		if (i < N - K)
-			FOR(d, 1, 6 + 1)
-			FORR(j, 70, -1) {
-			dpb[i + 1][d + j] += dpb[i][j]/6.0;
+		if (i < K) {
+			FOR(d, 4, 6 + 1) {
+				FORR(sum, 60, 0 - 1) {
+					dpTaro[i + 1][sum + d] += 2.0*dpTaro[i][sum] / 6.0;
+				}
+			}
 		}
 		else {
-			FOR(d, 4, 6 + 1)
-				FORR(j, 70, -1) {
-				dpb[i + 1][d + j] += (2 * dpb[i][j])/6.0;
+			FOR(d, 1, 6 + 1) {
+				FORR(sum, 60, 0 - 1) {
+					dpTaro[i + 1][sum + d] += dpTaro[i][sum] / 6.0;
+				}
 			}
-
 		}
 	}
-	//dpb[N][num]:=N回目にnumが出る総数
-	FOR(i, 0, 100) {
+	double ans = 0;
+	FOR(i, 0, 61) {
 		FOR(j, 0, i) {
-			ans += dpb[N][i] * dpa[N][j];
+			ans += dpJiro[N][j] * dpTaro[N][i];
 		}
 	}
-	printf("%.10lf\n",ans);
-	//double x = 3.5*N;
-	//double y = 0;
-	//y += 1.0*(N - K)*3.5;
-	//y += 1.0*K*5.0;
-	//ans = y / (x + y);
+	cout <<fixed<<setprecision(10)<< ans << "\n";
 
 	return 0;
 }
