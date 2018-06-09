a5f2abeaa217ddc44fc880e45c4a10da5a092144
Yang-33 <kasai.yuta0810@gmail.com>
2018-06-10 02:29:40 +0900

    solve 097_ARC_B.

---
+++ atcoder/ARC/097_ARC_B.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+
+// yukicoder‚ÉŽR‚Ì‚æ‚¤‚É‚ ‚é‚½‚ßA”½ŽË“I‚É‰ð‚¯‚é
+
+struct UnionFind {
+	vector<int> data;
+	UnionFind(int n) { data.assign(n, -1); }
+	bool unionSet(int x, int y) {
+		x = root(x); y = root(y);
+		if (x != y) {
+			if (data[y] < data[x]) swap(x, y);
+			data[x] += data[y]; data[y] = x;
+		}
+		return x != y;
+	}
+	bool same(int x, int y) { return root(x) == root(y); }
+	int root(int x) { return data[x] < 0 ? x : data[x] = root(data[x]); }
+	int size(int x) { return -data[root(x)]; }
+};
+
+
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	int N, M; cin >> N >> M;
+	VI p(N);
+
+	FOR(i, 0, N) {
+		cin >> p[i];
+		p[i]--;
+	}
+
+	UnionFind uf(N);
+	FOR(i, 0, M) {
+		int x, y;
+		cin >> x >> y;
+		x--, y--;
+		uf.unionSet(x, y);
+	}
+	int ans = 0;
+	FOR(i, 0, N) {
+		if (uf.same(i, p[i]))ans++;
+	}
+
+	cout << ans << "\n";
+
+	return 0;
+}
