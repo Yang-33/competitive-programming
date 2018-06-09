11c3ceee5e39ac5bbc5b5109b2ad77fd1868efff
Yang-33 <kasai.yuta0810@gmail.com>
2018-06-10 02:32:02 +0900

    solve WORLD_CODE_SPRINT_13_A.

---
+++ HackerRank/WORLD_CODE_SPRINT_13_A.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2018/05/26  Problem: world_code_sprint_13 A / Link: __CONTEST_URL__  ----- */
+/* ------問題------
+
+
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+問題文の出力が本物の正答とことなる
+ウケる
+
+----解説ここまで---- */
+
+LL N;
+
+LL ans = 0LL;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	cin >> N;
+	VI a(N);
+	set<int>se;
+	FOR(i, 0, N) {
+		se.insert(i + 1);
+	}
+	FOR(i, 0, N) {
+		cin >> a[i];
+		se.erase(a[i]);
+	}
+	cout << SZ(se) << endl;
+	for (auto it : se) {
+		cout << it <<" ";
+	}
+
+	return 0;
+}
