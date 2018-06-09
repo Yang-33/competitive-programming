e71bc87c4c6e693f3d2de802fcedbfc79556d122
Yang-33 <kasai.yuta0810@gmail.com>
2018-06-10 02:47:57 +0900

    solve GCJ2018_R2_d.

---
+++ GoogleCodeJam/GCJ2018_R2_d.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2018/05/19  Problem: GCJ2018_R2 D / Link: __CONTEST_URL__  ----- */
+/* ------問題------
+
+
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+無理
+
+----解説ここまで---- */
+
+void solve() {
+
+	int N;
+	cin >> N;
+	VI a(N);
+	FOR(i, 0, N) {
+		cin >> a[i];
+	}
+
+
+
+	if (0) {
+		cout << "IMPOSSIBLE" << endl;
+	}
+	else {
+		cout << 1 << endl; //ans
+		FOR(i, 0, 1) {
+			FOR(j, 0, N) {
+
+			}
+		}
+	}
+}
+
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	int T; cin >> T;
+	FOR(cx, 1, T + 1) {
+		cout << "Case #" << cx << ": ";
+		solve();
+	}
+
+	return 0;
+}
