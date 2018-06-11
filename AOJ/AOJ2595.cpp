85e3845129cdb23cca397b09338a326493b89297
Yang-33 <kasai.yuta0810@gmail.com>
2018-06-11 16:10:24 +0900

    solve AOJ2595.

--- AOJ/AOJ2595.cpp
+++ AOJ/AOJ2595.cpp
@@ -1,4 +1,4 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
 using VS = vector<string>;    using LL = long long;
@@ -8,104 +8,83 @@ using VL = vector<LL>;        using VVL = vector<VL>;
 
 #define ALL(a)  begin((a)),end((a))
 #define RALL(a) (a).rbegin(), (a).rend()
-#define PB push_back
-#define EB emplace_back
-#define MP make_pair
 #define SZ(a) int((a).size())
 #define SORT(c) sort(ALL((c)))
 #define RSORT(c) sort(RALL((c)))
 #define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
 #define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
 #define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
-#define debug(x) cerr << #x << ": " << x 
+#define debug(x) cerr << #x << ": " << x << endl
 const int INF = 1e9;                          const LL LINF = 1e16;
 const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2017/11/08  Problem: NU_26 A / Link: __CONTEST_URL__  ----- */
+/* -----  2018/06/10  Problem: AOJ 2595 / Link: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2595  ----- */
 /* ------問題------
 
-
+N枚のクッキーがある。
+D日以内に食べたい。
+一日にX枚未満しか食べれないが、食べ方の組み合わせはどれだけか。
+食べない日があっても良い。
 
 -----問題ここまで----- */
 /* -----解説等-----
 
-
+食べない日のことを考えると、食べる日だけの組み合わせを考えて、Kにちたべるとしたら
+comb(D,K)*(K日でN枚食べる組み合わせ)
+を計算すればいいことになる。
+愚直なDPを考えるとO(KNX)となる。
+dp[k][n]=k日毎日食べて、n枚食べる組み合わせ
+だがdp[k+1][n]+=dp[k][n-x]の部分は範囲和なので、適切に扱うことでO(logX)なりO(1)になる。
+したがって解ける。
+最後のcombinationの部分は、大きい組み合わせを小さい方のオーダーでやるテクで解決できる。(Dは大きいけどmin=2*0^3なので)
 
 ----解説ここまで---- */
 
-long long  powmod(long long  x, long long  n, long long  mod) {
-	long long  res = 1;
-	while (n > 0) {
-		if (n & 1) {
-			res = (res*x) % mod;
-		}
-		x = (x*x) % mod;
-		n >>= 1;
-	}
-	return res;
-}
-LL F[10005], R[10005];
-void make() {
-	F[0] = 1;
-	for (long long i = 1; i < 10005; i++) F[i] = F[i - 1] * i%MOD;
-	for (long long i = 0; i < 10005; i++) R[i] = powmod(F[i], MOD - 2, MOD);
-}
-LL N, D, X;
 
-LL ans = 0LL;
-inline long long  inverse(long long  a, long long  m) {
-	long long  b = m, u = 1, v = 0;
-	while (b) {
-		long long  t = a / b;
-		swap(a -= t * b, b);
-		swap(u -= t * v, v);
-	}
-	return (u % m + m) % m;
+const long long mod = 1e9 + 7;
+
+long long modpow(long long a, long long b) { //a==0の処理は問題次第
+	if (b == 0) return 1;
+	return modpow(a * a % mod, b / 2) * (b & 1 ? a : 1) % mod;
 }
+
+long long modinv(long long a) {
+	return modpow(a, mod - 2);
+}
+
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
-	make();
-	while (cin >> N >> D >> X) {
-		if (N == 0)break;
-		if (X == 1) {
-			cout << 0 << endl;
-			continue;
-		}
-		ans = 0;
+	LL N, D, X;
+
+	while (cin >> N >> D >> X, N || D || X) {
+
 		VVL dp(N + 1, VL(N + 1, 0));
 		dp[0][0] = 1;
-		FOR(p, 1, N + 1) {
+		FOR(i, 0, N) { // i日食べる日がある
+			// X:固定param
 			LL sum = 0;
-			FOR(j, 1, N + 1) {// dp part 
-				sum = (sum + dp[p - 1][j - 1]) % MOD;
-				dp[p][j] = sum;
-				if (j - X + 1 >= 0)sum = (sum - dp[p - 1][j - X + 1] + MOD) % MOD;
+			FOR(n, 1, N + 1) {
+				sum += dp[i][n - 1], sum %= MOD;
+				if (n >= X)sum -= (dp[i][n - X]) % MOD, sum += MOD, sum %= MOD;
+				dp[i + 1][n] += sum;
+				dp[i + 1][n] %= MOD;
 			}
 		}
-		//FOR(i, 0, N + 1) {
-		//	FOR(j, 0, N + 1) {
-		//		debug(dp[i][j]) << " ";
-		//	}cout << endl;
-		//}
-		ans = 0;
-		LL DCJ = 1;
-		FOR(i, 1, min(D, N) + 1) {
-			DCJ = (DCJ*((D - (LL)i + 1LL+MOD) % MOD) % MOD) % MOD;//mul
-			//DCJ = (DCJ*R[i] % MOD) % MOD;// div
-			DCJ = (DCJ*inverse(i,MOD) % MOD) % MOD;// div
-			ans += dp[i][N] * DCJ%MOD;
+		LL ans = 0;
+		LL comb = 1;
+		FOR(i, 1, N + 1) {
+			comb *= ((D - i + 1+mod)%mod);
+			comb %= mod;
+			comb *= modinv(i);
+			comb %= mod;
+			LL ret = dp[i][N] * comb;
+			ans += ret%MOD;
 			ans %= MOD;
-			//debug(DCJ) << endl;
-			//debug(dp[i][N]) << endl;
 		}
-
-		ans %= MOD;
-		ans = ans%MOD;
 		cout << ans << "\n";
-
 	}
 
 	return 0;
