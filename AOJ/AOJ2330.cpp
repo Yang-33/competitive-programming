1671ec0eee0210ec28010a89a8135e38ee212d63
Yang-33 <kasai.yuta0810@gmail.com>
2018-06-15 23:35:26 +0900

    solve AOJ2330.

---
+++ AOJ/AOJ2330.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2018/06/16  Problem: AOJ 2330 / Link: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2330  ----- */
+/* ------問題------
+
+偽コインの発見
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+これ有名なんだね
+どんな枚数でもlog_{3} {N}回でできる。
+
+----解説ここまで---- */
+
+
+
+void solve() {
+
+	int N;
+	cin >> N;
+	LL p = 1;
+	int ans = 0;
+	while (p < N) {
+		ans++;
+		p *= 3;
+	}
+	cout << ans << endl;
+}
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	solve();
+	return 0;
+}
