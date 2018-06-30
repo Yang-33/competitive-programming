cd374c87c438b8618745f40fe7db7e6d9a519bbd
Yang-33 <kasai.yuta0810@gmail.com>
2018-06-30 22:04:17 +0900

    solve AOJ0109.

--- AOJ/AOJ0109.cpp
+++ AOJ/AOJ0109.cpp
@@ -1,116 +1,96 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef pair<ll, ll> pii;
-typedef pair<ll, ll> pll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
-#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
-#define debug(x) cout << #x << ": " << x << endl
-#define mp make_pair
-#define pb push_back
-const ll MOD = 1000000007;
-const ll INF = 1e9;
-const ll LINF = 1e16;
-const double PI = acos(-1.0);
-ll dx[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-ll dy[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2017/05/12  Problem:  / Link:   ----- */
+/* -----  2018/06/30  Problem: AOJ 0109 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/0109  ----- */
 /* ------問題------
 
-
+四則演算の構文解析
 
 -----問題ここまで----- */
 /* -----解説等-----
 
-かっこがある
-* / は計算順序の優先度が
-カッコから出るときに次の要素を見ないといけない
-
+BNFをみて練習
+演算の読み込みに注意して、読み込んでi++的なことをする。or 保存
 
 ----解説ここまで---- */
 
-ll N;
 
-ll ans = 0LL;
+LL expr1(int&, string&);
+LL expr2(int&, string&);
+LL expr3(int&, string&);
+LL getnum(int&, string&);
 
-ll parsenum(ll &i, string &s);
-ll expr1(ll &i, string &s);
-ll expr2(ll &i, string &s);
-ll expr3(ll &i, string &s);
-
-
-ll parsenum(ll &i, string &s) {
-	ll res = 0;
-	while (isdigit(s[i])) {
-		res *= 10;
-		res += s[i] - '0';
-		i++;
-	}
-	return res;
-}
-
-/*評価開始優先度０*/
-ll expr1(ll &i, string &s) {
-	ll ret = expr2(i, s);
-	while (1) {
-		if (s[i] == '+') {
-			i++;
-			ret += expr2(i, s);
-		}
-		else if (s[i] == '-') {
-			i++;
-			ret -= expr2(i, s);
-		}
-		else break;
+// + - 
+LL expr1(int & i, string &s) {
+	LL ret = expr2(i, s);
+	while (s[i] == '+' || s[i] == '-') {
+		if (s[i] == '+')ret += expr2(++i, s);
+		if (s[i] == '-')ret -= expr2(++i, s);
 	}
 	return ret;
 }
-
-/*評価開始優先度１*/
-ll expr2(ll &i, string &s) {
-	ll ret = expr3(i, s);
-	while (1) {
-		if (s[i] == '*') {
-			i++;
-			ret *= expr3(i, s);
-		}
-		else if (s[i] == '/') {
-			i++;
-			ret /= expr3(i, s);
-		}
-		else break;
+// * / 
+LL expr2(int & i, string &s) {
+	LL ret = expr3(i, s);
+	while (s[i] == '*' || s[i] == '/') {
+		if (s[i] == '*')ret *= expr3(++i, s);
+		if (s[i] == '/')ret /= expr3(++i, s);
 	}
 	return ret;
-}
 
-/*評価開始優先度２*/
-ll expr3(ll &i, string &s) {
-	int ret;
+}
+// (re expr) or num
+LL expr3(int & i, string &s) {
 	if (s[i] == '(') {
 		i++;
-		ret = expr1(i, s);
-		i++; // !!!!
+		LL ret = expr1(i, s);
+		i++;// ')'
+		return ret;
 	}
-	else ret = parsenum(i, s);
-	
+	else {
+		return getnum(i, s);
+	}
+}
+LL getnum(int& i, string &s) {
+	LL ret = 0;
+	while (isdigit(s[i])){
+		ret *= 10; 
+		ret += s[i] - '0';
+		i++;
+	}
 	return ret;
 }
+
 
 
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
-	cin >> N;
-	string S[55];
-	FOR(i, 0, N) {
-		cin >> S[i];
-		ll p = 0;
-		cout << expr1(p, S[i]) << endl;
-
+	int N; cin >> N;
+	FOR(shuri, 0, N) {
+		string s; cin >> s;
+		int i = 0;
+		cout << expr1(i, s) << endl;
 	}
+
 	return 0;
 }
