1b1b661bba36bbff7c876f0e8d91eed245cd0f87
Yang-33 <kasai.yuta0810@gmail.com>
2018-08-04 23:50:51 +0900

    solve MUJIN2018_A.

---
+++ atcoder/MUJIN2018_A.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2018/08/04  Problem: mujin2018 A / Link: https://beta.atcoder.jp/contests/mujin-pc-2018  ----- */
+/* ------問題------
+
+
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+
+
+----解説ここまで---- */
+
+LL N;
+
+LL ans = 0LL;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+
+	string s;
+	cin >> s;
+	int f = 1;
+	if (SZ(s) < 5) {
+		cout << "No" << endl;
+	}
+	else {
+		string M = "MUJIN";
+		FOR(i, 0, 5) {
+			f &= (s[i] == M[i]);
+		}
+		if (f) {
+			cout << "Yes" << endl;
+		}
+		else cout << "No" << endl;
+	}
+
+
+	return 0;
+}
