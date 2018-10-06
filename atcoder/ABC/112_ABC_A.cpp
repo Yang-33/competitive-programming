6d530baacfaf3a6227afc0b5497897e127ae8aec
Yang-33 <kasai.yuta0810@gmail.com>
2018-10-06 23:47:08 +0900

    solve 112_ABC_A.

---
+++ atcoder/ABC/112_ABC_A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+
+	int N; cin >> N;
+	if (N == 1) {
+		cout << "Hello World" << endl;
+	}
+	else {
+		int A, B; cin >> A >> B;
+		cout << A + B << endl;
+	}
+
+	return 0;
+}
