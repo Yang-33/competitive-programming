d9d0334672de44b8be390030aab62f9c33e09a63
Yang-33 <kasai.yuta0810@gmail.com>
2018-10-19 23:42:12 +0900

    solve 037_ARC_B.

---
+++ atcoder/ARC/037_ARC_B.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DEBUG(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2018/10/19  Problem: ARC 037 B / Link: http://arc037.contest.atcoder.jp/tasks/arc037_b  ----- */
+/* ------問題------
+
+バウムテストとは、被験者に「木」の絵を描かせることで被験者の心理状態を読み取る心理検査である。
+この検査を受けた高橋君は、 N 個の頂点と M 本の辺からなる無向グラフを描いた。このグラフの連結成分のうち木であるようなもの、すなわち閉路を持たないものの個数を求めよ。
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+sample3狂おしいほど好き
+
+----解説ここまで---- */
+
+
+int dfs(int v, int p, VI &visit, const VVI& G) {
+	visit[v] = 1;
+	int res = 1;
+	FOR(i, 0, SZ(G[v])) {
+		int nx = G[v][i];
+		if (nx != p) {
+			if (visit[nx])return 0;
+			res &= dfs(nx, v, visit, G);
+		}
+	}
+	return res;
+}
+
+LL ans = 0LL;
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL N, M; cin >> N >> M;
+	VVI G(N);
+	FOR(i, 0, M) {
+		int a, b; cin >> a >> b;
+		a--, b--;
+		G[a].push_back(b);
+		G[b].push_back(a);
+	}
+
+	VI visit(N, 0);
+	FOR(i, 0, N) {
+		if (!visit[i]) {
+			ans += dfs(i, -1, visit, G);
+		}
+	}
+
+	cout << ans << "\n";
+
+	return 0;
+}
