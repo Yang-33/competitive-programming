5733b461275a75b480011be21b3659df764a33b2
Yang-33 <kasai.yuta0810@gmail.com>
2018-10-28 18:25:46 +0900

    solve 034_ARC_C.

---
+++ atcoder/ARC/034_ARC_C.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2018/10/27  Problem: ARC 034 C / Link: http://arc034.contest.atcoder.jp/tasks/arc034_c  ----- */
+/* ------問題------
+
+2 個の正整数 A,B が与えられる。
+A! の約数であり、かつ B! の倍数でもあるような正整数の個数を 1,000,000,007 で割った余りを求めよ。
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+最低でB!,最大でA!である。
+B!にかけてもいいのはA!の約数である必要である。
+これはA!/B!にでてくる数なので、結局約数の選択数を求めれば良い。
+
+----解説ここまで---- */
+
+map<int, int>cnt;
+void make_factor(long long  n) {
+	for (long long i = 2; i * i <= n; i++) {
+		while (n%i == 0) {
+			cnt[i]++;
+			n /= i;
+		}
+	}
+	if (n != 1)cnt[n]++;
+}
+
+template <::std::uint_fast32_t MODULO> class modint {
+public:
+	using uint32 = ::std::uint_fast32_t; using uint64 = ::std::uint_fast64_t; using value_type = uint32; uint32 a; modint() noexcept : a(0) {}modint(const uint32 x) noexcept : a(x) {}modint operator+(const modint &o) const noexcept { return a + o.a < MODULO ? modint(a + o.a) : modint(a + o.a - MODULO); }modint operator-(const modint &o) const noexcept { return modint(a < o.a ? a + MODULO - o.a : a - o.a); }modint operator*(const modint &o) const noexcept { return modint(static_cast<uint64>(a) * o.a % MODULO); }modint operator/(const modint &o) const { return modint(static_cast<uint64>(a) * (~o).a % MODULO); }modint &operator+=(const modint &o) noexcept { return *this = *this + o; }modint &operator-=(const modint &o) noexcept { return *this = *this - o; }modint &operator*=(const modint &o) noexcept { return *this = *this * o; }modint &operator/=(const modint &o) { return *this = *this / o; }modint operator~() const noexcept { return pow(MODULO - 2); }modint operator-() const noexcept { return a ? modint(MODULO - a) : *this; }modint operator++() noexcept { return a == MODULO - 1 ? a = 0 : ++a, *this; }modint operator--() noexcept { return a ? --a : a = MODULO - 1, *this; }bool operator==(const modint &o) const noexcept { return a == o.a; }bool operator!=(const modint &o) const noexcept { return a != o.a; }bool operator<(const modint &o) const noexcept { return a < o.a; }bool operator<=(const modint &o) const noexcept { return a <= o.a; }bool operator>(const modint &o) const noexcept { return a > o.a; }bool operator>=(const modint &o) const noexcept { return a >= o.a; }explicit operator bool() const noexcept { return a; }explicit operator uint32() const noexcept { return a; }modint pow(uint32 x) const noexcept { uint64 t = a, u = 1; while (x) { if (x & 1)u = u * t % MODULO; t = (t * t) % MODULO; x >>= 1; }	return modint(u); }
+	uint32 get() const noexcept { return a; }
+};
+using mint = modint<MOD>;
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	int A, B; cin >> A >> B;
+
+	FOR(i, B + 1, A + 1) {
+		make_factor(i);
+	}
+	vector<mint>fact(1000, 0);
+	fact[1] = 1;
+	FOR(i, 2, 1000) {
+		fact[i] = fact[i - 1] * i;
+	}
+	mint ans = 1;
+	for (auto it : cnt) {
+		ans *= (it.second + 1);
+	}
+
+	cout << ans.get() << "\n";
+
+	return 0;
+}
