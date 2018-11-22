e99e70ab5d470a1aa275538033ff395822c127f9
Yang-33 <kasai.yuta0810@gmail.com>
2018-11-22 23:48:02 +0900

    solve 017_ARC_B.

---
+++ atcoder/ARC/017_ARC_B.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2018/11/22  Problem: ARC 017 B / Link: http://arc017.contest.atcoder.jp/tasks/arc017_b  ----- */
+/* ------問題------
+
+
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+
+
+----解説ここまで---- */
+
+
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL N, K; cin >> N >> K;
+	VL a(N);
+	FOR(i, 0, N) {
+		cin >> a[i];
+	}
+	// K個が昇順であるようなものの個数
+	VI b(N, 0);
+	FOR(i, 0, N - 1) {
+		if (a[i] < a[i + 1]) {
+			b[i] = 1;
+		}
+	}
+	// b(i):=i,i+1は真に大きい順か?
+	LL ans = 0;
+	int sub = 0;
+	FOR(i, 0, N - 1) {
+		DD(de(b[i], sub, ans));
+		if (b[i]) {
+			sub++;
+			if (sub >= K - 1)ans++;
+		}
+		else {
+			sub = 0;
+		}
+	}
+	if (K == 1)ans = N;
+	cout << ans << endl;
+	return 0;
+}
