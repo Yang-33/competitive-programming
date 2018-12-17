81a6f89c8a9ba2b959d4552a09b502790f56ea31
Yang-33 <kasai.yuta0810@gmail.com>
2018-12-17 21:50:06 +0900

    solve AVITOCOOLCHALLENGE2018_C.

---
+++ codeforces/AVITOCOOLCHALLENGE2018_C.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 998244353;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2018/12/16  Problem: avitoCoolChallenge2018 C / Link: __CONTEST_URL__  ----- */
+/* ------問題------
+
+長さNの列がある。M色でぬるとき、色の変化がK回となるような塗り方の総数は？
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+
+分割位置ははN-1個からK個選ぶのでnCk(N-1,K)
+色の塗り方は、K+1箇所について、隣り合う色が異なればよいので、
+M(初期色)*(M-1)^Kとなる。
+場合分けしたけど意味なかった。
+いいかげんmodintのやつを書き換えたい。
+
+----解説ここまで---- */
+
+template <::std::uint_fast32_t MODULO> class modint {
+public:
+	using uint32 = ::std::uint_fast32_t; using uint64 = ::std::uint_fast64_t; using value_type = uint32; uint32 a; modint() noexcept : a(0) {}modint(const uint32 x) noexcept : a(x) {}modint operator+(const modint &o) const noexcept { return a + o.a < MODULO ? modint(a + o.a) : modint(a + o.a - MODULO); }modint operator-(const modint &o) const noexcept { return modint(a < o.a ? a + MODULO - o.a : a - o.a); }modint operator*(const modint &o) const noexcept { return modint(static_cast<uint64>(a) * o.a % MODULO); }modint operator/(const modint &o) const { return modint(static_cast<uint64>(a) * (~o).a % MODULO); }modint &operator+=(const modint &o) noexcept { return *this = *this + o; }modint &operator-=(const modint &o) noexcept { return *this = *this - o; }modint &operator*=(const modint &o) noexcept { return *this = *this * o; }modint &operator/=(const modint &o) { return *this = *this / o; }modint operator~() const noexcept { return pow(MODULO - 2); }modint operator-() const noexcept { return a ? modint(MODULO - a) : *this; }modint operator++() noexcept { return a == MODULO - 1 ? a = 0 : ++a, *this; }modint operator--() noexcept { return a ? --a : a = MODULO - 1, *this; }bool operator==(const modint &o) const noexcept { return a == o.a; }bool operator!=(const modint &o) const noexcept { return a != o.a; }bool operator<(const modint &o) const noexcept { return a < o.a; }bool operator<=(const modint &o) const noexcept { return a <= o.a; }bool operator>(const modint &o) const noexcept { return a > o.a; }bool operator>=(const modint &o) const noexcept { return a >= o.a; }explicit operator bool() const noexcept { return a; }explicit operator uint32() const noexcept { return a; }modint pow(uint32 x) const noexcept { uint64 t = a, u = 1; while (x) { if (x & 1)u = u * t % MODULO; t = (t * t) % MODULO; x >>= 1; }	return modint(u); }
+	uint32 get() const noexcept { return a; }
+};
+using mint = modint<MOD>;
+const long long mod = MOD;
+//a==0の処理は問題次第 a>=mod && a%mod==0なら、返却値は0
+long long modpow(long long a, long long b) {
+	if (b == 0) return 1;
+	return modpow(a * a % mod, b / 2) * (b & 1 ? a : 1) % mod;
+}
+
+long long modinv(long long a) {
+	return modpow(a, mod - 2);
+}
+
+vector<long long> fact, inv_fact;
+
+void init_fact(int n) {
+	fact.resize(n);
+	fact[0] = 1;
+	for (int i = 1; i < n; i++) {
+		fact[i] = i * fact[i - 1] % mod;
+	}
+	inv_fact.resize(n);
+	inv_fact[n - 1] = modinv(fact[n - 1]);
+	for (int i = n - 2; i >= 0; i--) {
+		inv_fact[i] = (i + 1) * inv_fact[i + 1] % mod;
+	}
+}
+
+long long nPr(int n, int r) {
+	if (n < r || n < 0 || r < 0) return 0;
+	return fact[n] * inv_fact[n - r] % mod;
+}
+
+long long nCr(int n, int r) {
+	if (n < r || n < 0 || r < 0) return 0;
+	return fact[n] * inv_fact[r] % mod * inv_fact[n - r] % mod;
+}
+
+long long nHr(int n, int r) {
+	if (n == 0 && r == 0) return 1;
+	return nCr(n + r - 1, r);
+}
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL N, M, K; cin >> N >> M >> K;
+	init_fact(1e5);
+	if (M == 1) {
+		if (K == 0) {
+			cout << 1 << endl;
+		}
+		else {
+			cout << 0 << endl;
+		}
+	}
+	else {
+		mint ans = M;
+		mint mn = M - 1;
+		mn = mn.pow(K);
+		ans *= mn;
+		mint knCk = nCr(N-1, K);
+		DD(de(ans.get(),knCk.get()))
+		ans *= knCk;
+		cout << ans.get() << endl;
+
+	}
+	
+//	cout << ans << "\n";
+
+	return 0;
+}
