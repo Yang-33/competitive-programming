fa226480e7907c5b10617d249fe6b7668ab898ca
Yang-33 <kasai.yuta0810@gmail.com>
2018-12-31 02:09:16 +0900

    solve SRM 615 div1 easy

---
+++ topcoder/SRM/SRM 615/AmebaDiv1.cpp
@@ -0,0 +1,205 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <algorithm>
+#include <set>
+#include <sstream>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <list>
+#include <fstream>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+#define foreach(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); itr++)
+
+/* -----  xtimex  Problem: SRM 615 div1 easy  / Link:   ----- */
+/* ------問題------
+
+任意整数のサイズのアメーバがいる。今X[i]のサイズのアメーバに順に出会う。
+今のサイズとX[i]が等しければ、これと合体し2倍のサイズになる。
+最終的なサイズに含まれない数は何個あるか?
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+(2)とかなら最後2にならないという感じ。
+まず、X[i]に含まれないサイズは最終的なサイズが同じままである。
+したがってX[i]から始めて、そこからたどり着く数に含まれない数を見れば良い。
+
+あとちょっと英語に戸惑った。
+
+----解説ここまで---- */
+
+class AmebaDiv1
+{
+public:
+
+	int count(vector<int> X)
+	{
+		set<int>S,T;
+		FOR(i, 0, SZ(X)) {
+			S.insert(X[i]);
+			int now = X[i];
+			FOR(i, 0, SZ(X)) {
+				if (now == X[i])now *= 2;
+			}
+			int ok = 1;
+			FOR(i, 0, SZ(X)) {
+				ok &= now != X[i];
+			}
+			if(!ok)T.insert(now);
+		}
+		// S\Tを求めれば良いが元が一致しているため
+		return SZ(S)-SZ(T);
+	}
+};
+
+/*
+
+struct AmebaDiv1 {
+  vector<int> X;
+  int count(vector<int> _X) {
+	X = _X;
+	return 0;
+  }
+};
+
+*/
+
+// CUT begin
+ifstream data("AmebaDiv1.sample");
+
+string next_line() {
+	string s;
+	getline(data, s);
+	return s;
+}
+
+template <typename T> void from_stream(T &t) {
+	stringstream ss(next_line());
+	ss >> t;
+}
+
+void from_stream(string &s) {
+	s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+	int len;
+	from_stream(len);
+	ts.clear();
+	for (int i = 0; i < len; ++i) {
+		T t;
+		from_stream(t);
+		ts.push_back(t);
+	}
+}
+
+template <typename T>
+string to_string(T t) {
+	stringstream s;
+	s << t;
+	return s.str();
+}
+
+string to_string(string t) {
+	return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> X, int __expected) {
+	time_t startClock = clock();
+	AmebaDiv1 *instance = new AmebaDiv1();
+	int __result = instance->count(X);
+	double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+	delete instance;
+
+	if (__result == __expected) {
+		cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+		return true;
+	}
+	else {
+		cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+		cout << "           Expected: " << to_string(__expected) << endl;
+		cout << "           Received: " << to_string(__result) << endl;
+		return false;
+	}
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+	int cases = 0, passed = 0;
+	while (true) {
+		if (next_line().find("--") != 0)
+			break;
+		vector<int> X;
+		from_stream(X);
+		next_line();
+		int __answer;
+		from_stream(__answer);
+
+		cases++;
+		if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+			continue;
+
+		cout << "  Testcase #" << cases - 1 << " ... ";
+		if (do_test(X, __answer)) {
+			passed++;
+		}
+	}
+	if (mainProcess) {
+		cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+		int T = time(NULL) - 1545962202;
+		double PT = T / 60.0, TT = 75.0;
+		cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+		cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	cout.setf(ios::fixed, ios::floatfield);
+	cout.precision(2);
+	set<int> cases;
+	bool mainProcess = true;
+	for (int i = 1; i < argc; ++i) {
+		if (string(argv[i]) == "-") {
+			mainProcess = false;
+		}
+		else {
+			cases.insert(atoi(argv[i]));
+		}
+	}
+	if (mainProcess) {
+		cout << "AmebaDiv1 (250 Points)" << endl << endl;
+	}
+	return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
