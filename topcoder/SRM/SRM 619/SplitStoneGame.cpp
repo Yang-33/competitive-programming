a1fffe84b0971a08362396564924bd089b0801e3
Yang-33 <kasai.yuta0810@gmail.com>
2018-12-31 02:10:12 +0900

    solve SRM 619 div1 easy

---
+++ topcoder/SRM/SRM 619/SplitStoneGame.cpp
@@ -0,0 +1,200 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <algorithm>
+#include <set>
+#include <sstream>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <list>
+#include <fstream>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+#define debug(x) cerr << #x << ": " << x << endl
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+#define foreach(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); itr++)
+
+/* -----  xtimex  Problem: SRM 619 div1 easy  / Link:   ----- */
+/* ------問題------
+
+2人でゲームを行う。1より大きいタイルの山を選択し、これを空でない2つの山に分け、他の山に別々に配置する。
+これを行えなくなった方の負けとする。
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+各手順で山は1ずつ減少し、残りが2個になると手番が回ってきた方の負けである。
+また、1より大きい山が1つでも存在すればゲームは山が2個になるまで続けることができるので、勝敗は以下
+3個以上の山で、山の数が奇数で、1より大きい山が1つでも存在するとき、先手の勝ち
+そうでないなら負け
+
+----解説ここまで---- */
+
+class SplitStoneGame
+{
+public:
+
+	string winOrLose(vector<int> number)
+	{
+		const string WIN = "WIN";
+		const string LOSE = "LOSE";
+		int N = SZ(number);
+		if (N < 3) {
+			return LOSE;
+		}
+		int cnt = 0;
+		FOR(i, 0, N) {
+			if (number[i] > 1)cnt++;
+		}
+
+		return (cnt&&N % 2 ? WIN : LOSE);
+	}
+};
+
+/*
+
+struct SplitStoneGame {
+  vector<int> number;
+  string winOrLose(vector<int> _number) {
+	number = _number;
+	return "";
+  }
+};
+
+*/
+
+// CUT begin
+ifstream data("SplitStoneGame.sample");
+
+string next_line() {
+	string s;
+	getline(data, s);
+	return s;
+}
+
+template <typename T> void from_stream(T &t) {
+	stringstream ss(next_line());
+	ss >> t;
+}
+
+void from_stream(string &s) {
+	s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+	int len;
+	from_stream(len);
+	ts.clear();
+	for (int i = 0; i < len; ++i) {
+		T t;
+		from_stream(t);
+		ts.push_back(t);
+	}
+}
+
+template <typename T>
+string to_string(T t) {
+	stringstream s;
+	s << t;
+	return s.str();
+}
+
+string to_string(string t) {
+	return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> number, string __expected) {
+	time_t startClock = clock();
+	SplitStoneGame *instance = new SplitStoneGame();
+	string __result = instance->winOrLose(number);
+	double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+	delete instance;
+
+	if (__result == __expected) {
+		cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+		return true;
+	}
+	else {
+		cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+		cout << "           Expected: " << to_string(__expected) << endl;
+		cout << "           Received: " << to_string(__result) << endl;
+		return false;
+	}
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+	int cases = 0, passed = 0;
+	while (true) {
+		if (next_line().find("--") != 0)
+			break;
+		vector<int> number;
+		from_stream(number);
+		next_line();
+		string __answer;
+		from_stream(__answer);
+
+		cases++;
+		if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+			continue;
+
+		cout << "  Testcase #" << cases - 1 << " ... ";
+		if (do_test(number, __answer)) {
+			passed++;
+		}
+	}
+	if (mainProcess) {
+		cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+		int T = time(NULL) - 1545962251;
+		double PT = T / 60.0, TT = 75.0;
+		cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+		cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	cout.setf(ios::fixed, ios::floatfield);
+	cout.precision(2);
+	set<int> cases;
+	bool mainProcess = true;
+	for (int i = 1; i < argc; ++i) {
+		if (string(argv[i]) == "-") {
+			mainProcess = false;
+		}
+		else {
+			cases.insert(atoi(argv[i]));
+		}
+	}
+	if (mainProcess) {
+		cout << "SplitStoneGame (250 Points)" << endl << endl;
+	}
+	return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
