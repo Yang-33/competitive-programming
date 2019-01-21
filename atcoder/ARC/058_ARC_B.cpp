9324aaa8b2ca04505cbc75d9fed868b55df412e7
Yang-33 <kasai.yuta0810@gmail.com>
2019-01-21 17:07:12 +0900

    solve 058_ARC_B.

--- atcoder/ARC/058_ARC_B.cpp
+++ atcoder/ARC/058_ARC_B.cpp
@@ -8,21 +8,23 @@ using VL = vector<LL>;        using VVL = vector<VL>;
 
 #define ALL(a)  begin((a)),end((a))
 #define RALL(a) (a).rbegin(), (a).rend()
-#define PB push_back
-#define EB emplace_back
-#define MP make_pair
 #define SZ(a) int((a).size())
 #define SORT(c) sort(ALL((c)))
 #define RSORT(c) sort(RALL((c)))
 #define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
 #define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
 #define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
-#define debug(x) cerr << #x << ": " << x << endl
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
 const int INF = 1e9;                          const LL LINF = 1e16;
 const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2018/02/15  Problem: 058_arc_b / Link: https://arc058.contest.atcoder.jp/tasks/arc058_b?lang=en ----- */
+/* -----  2019/01/20  Problem: ARC 058 B / Link: http://arc058.contest.atcoder.jp/tasks/arc058_b  ----- */
 /* ------問題------
 
 縦 H マス、横 W マスのマス目があります。 いろはちゃんは、今一番左上のマス目にいます。 そして、右か下に1マス移動することを繰り返し、一番右下のマス目へと移動します。 ただし、下から A 個以内、かつ左から B 個以内のマス目へは移動することは出来ません。
@@ -32,48 +34,62 @@ int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 
 -----問題ここまで----- */
 /* -----解説等-----
 
-point(A,w)上までの移動とpoint(A,w+1)からの移動を考えたとき、これらはすべて独立した経路を通過する
+経路を区別する方法を考える．
+禁止領域と同じX座標から右に必ず移動するとすれば，O(H)個の経路を見れば良いことになる．
+経路同士の積の和が答え．
 
 ----解説ここまで---- */
 
 
-LL F[200005], R[200005];
-long long  powmod(long long  x, long long  n, long long  MOD) {
-	long long  res = 1;
-	while (n > 0) {
-		if (n & 1) {
-			res = (res*x) % MOD;
-		}
-		x = (x*x) % MOD;
-		n >>= 1;
+template <::std::uint_fast32_t MODULO> class modint {
+public:
+	using uint32 = ::std::uint_fast32_t; using uint64 = ::std::uint_fast64_t; using value_type = uint32; uint32 a; modint() noexcept : a(0) {}modint(const uint32 x) noexcept : a(x) {}modint operator+(const modint &o) const noexcept { return a + o.a < MODULO ? modint(a + o.a) : modint(a + o.a - MODULO); }modint operator-(const modint &o) const noexcept { return modint(a < o.a ? a + MODULO - o.a : a - o.a); }modint operator*(const modint &o) const noexcept { return modint(static_cast<uint64>(a) * o.a % MODULO); }modint operator/(const modint &o) const { return modint(static_cast<uint64>(a) * (~o).a % MODULO); }modint &operator+=(const modint &o) noexcept { return *this = *this + o; }modint &operator-=(const modint &o) noexcept { return *this = *this - o; }modint &operator*=(const modint &o) noexcept { return *this = *this * o; }modint &operator/=(const modint &o) { return *this = *this / o; }modint operator~() const noexcept { return pow(MODULO - 2); }modint operator-() const noexcept { return a ? modint(MODULO - a) : *this; }modint operator++() noexcept { return a == MODULO - 1 ? a = 0 : ++a, *this; }modint operator--() noexcept { return a ? --a : a = MODULO - 1, *this; }bool operator==(const modint &o) const noexcept { return a == o.a; }bool operator!=(const modint &o) const noexcept { return a != o.a; }bool operator<(const modint &o) const noexcept { return a < o.a; }bool operator<=(const modint &o) const noexcept { return a <= o.a; }bool operator>(const modint &o) const noexcept { return a > o.a; }bool operator>=(const modint &o) const noexcept { return a >= o.a; }explicit operator bool() const noexcept { return a; }explicit operator uint32() const noexcept { return a; }modint pow(uint32 x) const noexcept { uint64 t = a, u = 1; while (x) { if (x & 1)u = u * t % MODULO; t = (t * t) % MODULO; x >>= 1; }	return modint(u); }
+	uint32 get() const noexcept { return a; }
+};
+using mint = modint<MOD>;
+
+const long long mod = 1e9 + 7;
+long long modpow(long long a, long long b) {
+	if (b == 0) return 1;
+	return modpow(a * a % mod, b / 2) * (b & 1 ? a : 1) % mod;
+}
+long long modinv(long long a) {
+	return modpow(a, mod - 2);
+}
+
+vector<long long> fact, inv_fact;
+
+void init_fact(int n) {
+	fact.resize(n);
+	fact[0] = 1;
+	for (int i = 1; i < n; i++) {
+		fact[i] = i * fact[i - 1] % mod;
 	}
-	return res;
-}
-void make() {
-	F[0] = 1;
-	for (int i = 1; i<200005; i++) F[i] = F[i - 1] * i%MOD;
-	for (int i = 0; i<200005; i++) R[i] = powmod(F[i], MOD - 2, MOD);
-}
-
-LL nCr(int a, int b) {
-	return F[a] * R[b] % MOD*R[a - b] % MOD;
+	inv_fact.resize(n);
+	inv_fact[n - 1] = modinv(fact[n - 1]);
+	for (int i = n - 2; i >= 0; i--) {
+		inv_fact[i] = (i + 1) * inv_fact[i + 1] % mod;
+	}
 }
 
-LL H, W, A, B;
-LL ans = 0LL;
+long long nCr(int n, int r) {
+	if (n < r || n < 0 || r < 0) return 0;
+	return fact[n] * inv_fact[r] % mod * inv_fact[n - r] % mod;
+}
+
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
-	cin >> H >> W >> A >> B;
-	make();
-	FOR(h, 0, H - A) {
-		ans += (nCr(B - 1 + h, B - 1) * nCr(W - 1 - B + H - 1 - h, W - 1 - B)) % MOD;
-		ans %= MOD;
+	init_fact(200005);
+	LL H, W, A, B; cin >> H >> W >> A >> B;
+	mint ans = 0;
+	FOR(i, 0, H - A) {
+		mint sub = (nCr(B - 1 + i, B - 1)*nCr(W - B - 1 + H - 1 - i, W - B - 1)) % mod;
+		ans += sub;
 	}
 
-
-	cout << ans << "\n";
+	cout << ans.get() << endl;
 
 	return 0;
 }
