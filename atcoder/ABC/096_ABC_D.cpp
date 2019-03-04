447e2c4dcb1506ceac486cc9dc15aaf0919d5ddc
Yang-33 <kasai.yuta0810@gmail.com>
2019-03-04 16:01:26 +0900

    solve 096_ABC_D.

---
+++ atcoder/ABC/096_ABC_D.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2019/03/04  Problem: ABC 096 D / Link: http://abc096.contest.atcoder.jp/tasks/abc096_d  ----- */
+vector<long long > make_prime(long long  N) {
+	vector<long long >a;
+	vector<bool> prime(N + 1);
+	FOR(i, 0, N + 1) {
+		prime[i] = 1;
+	}prime[0] = prime[1] = 0;
+
+	for (LL i = 2; i <= N; i++) {
+		if (prime[i]) {
+			a.push_back((long long)i);
+			for (LL j = i * 2; j <= N; j += i)
+				prime[j] = 0;
+		}
+	}
+	return a;
+}
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL N; 
+	cin >> N;
+	auto primes = make_prime(55555);
+	LL cnt = 0;
+	VL ans;
+	for (auto it : primes) {
+		if (it % 5 == 1)cnt++,ans.push_back(it);
+	}
+	DD(de(cnt));
+	FOR(i, 0, N) {
+		cout << ans[i] << " \n"[i == N - 1];
+	}
+
+	return 0;
+}
