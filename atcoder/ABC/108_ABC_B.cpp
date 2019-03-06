d28d846806e9a7eadb8be3c935a08c496adfeb39
Yang-33 <kasai.yuta0810@gmail.com>
2019-03-06 16:56:25 +0900

    solve 108_ABC_B.

---
+++ atcoder/ABC/108_ABC_B.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2019/02/28  Problem: ABC 108 B / Link: http://abc108.contest.atcoder.jp/tasks/abc108_b  ----- */
+/* ------問題------
+
+
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+
+
+----解説ここまで---- */
+
+LL ans = 0LL;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	VL x(4), y(4);
+	FOR(i, 0, 2) {
+		cin >> x[i] >> y[i];
+	}
+	FOR(i, 0, 2) {
+		LL X = x[i + 1] - x[i];
+		LL Y = y[i + 1] - y[i];
+		swap(X, Y);
+		X = -X;
+		x[i + 2] = x[i + 1] + X;
+		y[i + 2] = y[i + 1] + Y;
+	}
+	FOR(i, 2, 4) {
+		cout << x[i] << " " << y[i] << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
