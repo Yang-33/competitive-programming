90981c5195ca7c2d11b84ff19e9c7e0da23d4280
Yang-33 <kasai.yuta0810@gmail.com>
2019-03-06 16:56:28 +0900

    solve 113_ABC_D.

---
+++ atcoder/ABC/113_ABC_D.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2019/02/27  Problem: ABC 113 D / Link: http://abc113.contest.atcoder.jp/tasks/abc113_d  ----- */
+/* ------問題------
+
+
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+ちゃんと問題を理解していなさそう
+2^pみたいなのはだめで，れんぞくしていなければよい→はい
+
+----解説ここまで---- */
+
+template <::std::uint_fast32_t MODULO> class modint {
+public:
+	using uint32 = ::std::uint_fast32_t; using uint64 = ::std::uint_fast64_t; using value_type = uint32; uint32 a; modint() noexcept : a(0) {}modint(const uint32 x) noexcept : a(x) {}modint operator+(const modint &o) const noexcept { return a + o.a < MODULO ? modint(a + o.a) : modint(a + o.a - MODULO); }modint operator-(const modint &o) const noexcept { return modint(a < o.a ? a + MODULO - o.a : a - o.a); }modint operator*(const modint &o) const noexcept { return modint(static_cast<uint64>(a) * o.a % MODULO); }modint operator/(const modint &o) const { return modint(static_cast<uint64>(a) * (~o).a % MODULO); }modint &operator+=(const modint &o) noexcept { return *this = *this + o; }modint &operator-=(const modint &o) noexcept { return *this = *this - o; }modint &operator*=(const modint &o) noexcept { return *this = *this * o; }modint &operator/=(const modint &o) { return *this = *this / o; }modint operator~() const noexcept { return pow(MODULO - 2); }modint operator-() const noexcept { return a ? modint(MODULO - a) : *this; }modint operator++() noexcept { return a == MODULO - 1 ? a = 0 : ++a, *this; }modint operator--() noexcept { return a ? --a : a = MODULO - 1, *this; }bool operator==(const modint &o) const noexcept { return a == o.a; }bool operator!=(const modint &o) const noexcept { return a != o.a; }bool operator<(const modint &o) const noexcept { return a < o.a; }bool operator<=(const modint &o) const noexcept { return a <= o.a; }bool operator>(const modint &o) const noexcept { return a > o.a; }bool operator>=(const modint &o) const noexcept { return a >= o.a; }explicit operator bool() const noexcept { return a; }explicit operator uint32() const noexcept { return a; }modint pow(int x) const noexcept {  uint64 t = a, u = 1; while (x>0) { if (x & 1)u = u * t % MODULO; t = (t * t) % MODULO; x >>= 1; }	return modint(u); }
+	uint32 get() const noexcept { return a; }
+};
+using mint = modint<MOD>;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL H, W, K; cin >> H >> W >> K;
+
+	// stay, L, R;
+	mint two = 2;
+	vector<mint> dp(W);
+	dp[0] = 1;
+	FOR(i, 0, H) {
+		vector<mint>nx(W);
+		FOR(state, 0, 1 << (W - 1)) {
+			bool valid = 1;
+			FOR(k, 0, W - 2) {
+				if ((state & 1 << k) && (state & 1 << (k + 1))) valid = false;
+			}
+			if (!valid)continue;
+			FOR(k, 0, W) {
+				if (state & 1 << k) {
+					nx[k + 1] += dp[k];
+				}
+				else if (k && (state & 1 << (k - 1))) {
+					nx[k - 1] += dp[k];
+				}
+				else {
+					nx[k] += dp[k];
+				}
+			}
+		}
+		nx.swap(dp);
+	}
+	LL ans = dp[K - 1].get();
+	cout << ans << "\n";
+
+	return 0;
+}
