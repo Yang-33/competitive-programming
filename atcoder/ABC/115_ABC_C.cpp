d312e1b3490b887184fec6da3a9211703744e2dc
Yang-33 <kasai.yuta0810@gmail.com>
2019-03-06 16:56:29 +0900

    solve 115_ABC_C.

---
+++ atcoder/ABC/115_ABC_C.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2019/02/27  Problem: ABC 115 C / Link: http://abc115.contest.atcoder.jp/tasks/abc115_c  ----- */
+/* ------問題------
+
+
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+
+
+----解説ここまで---- */
+
+LL ans = 0LL;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL N, K; cin >> N >> K;
+	VL a(N);
+	FOR(i, 0, N) {
+		cin >> a[i];
+	}
+
+	SORT(a);
+	ans = LINF;
+	FOR(i, 0, N) {
+		int j = i + K - 1;
+		if (j < N) {
+			LL ret = a[j] - a[i];
+			ans = min(ans, ret);
+		}
+	}
+
+
+	cout << ans << "\n";
+
+	return 0;
+}
