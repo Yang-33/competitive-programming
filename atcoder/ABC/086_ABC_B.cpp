077f0ff7ed9058e6b3fcaeca694d4ed4d32ba68b
Yang-33 <kasai.yuta0810@gmail.com>
2019-03-12 00:01:43 +0900

    solve 086_ABC_B.

---
+++ atcoder/ABC/086_ABC_B.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2019/03/11  Problem: ABC 086 B / Link: http://abc086.contest.atcoder.jp/tasks/abc086_b  ----- */
+
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+
+	string a, b; cin >> a >> b;
+	string s = a + b;
+	auto f = [&]() {
+		LL n = stoll(s);
+		FOR(i, 1, 10001) {
+			LL c = i;
+			if (c*c == n)return 1;
+		}
+		return 0;
+	};
+	LL ans = f();
+	
+	
+	cout << (ans?"Yes":"No") << "\n";
+
+	return 0;
+}
