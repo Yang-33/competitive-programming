db55d011d85b6c70b336a8826038b53406fd1b3d
Yang-33 <kasai.yuta0810@gmail.com>
2019-03-12 00:01:45 +0900

    solve 088_ABC_D.

--- atcoder/ABC/088_ABC_D.cpp
+++ atcoder/ABC/088_ABC_D.cpp
@@ -15,31 +15,22 @@ using VL = vector<LL>;        using VVL = vector<VL>;
 #define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
 #define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
 //#pragma GCC optimize ("-O3") 
-//#ifdef YANG33
-//#include "mydebug.hpp"
-//#else
-//#define DD(x) 
-//#endif
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
 const int INF = 1e9;                          const LL LINF = 1e16;
 const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2018/10/23  Problem: ABC 088 D / Link: http://abc088.contest.atcoder.jp/tasks/abc088_d  ----- */
-/* ------問題------
-
-
-
------問題ここまで----- */
-/* -----解説等-----
-
-
-
-----解説ここまで---- */
+/* -----  2019/03/11  Problem: ABC 088 D / Link: http://abc088.contest.atcoder.jp/tasks/abc088_d  ----- */
 
 inline bool IN(int y, int x, int H, int W) { return (0 <= y && y < H && 0 <= x && x < W); }
 
-int bfs2D(pair<int, int> s, pair<int, int> t, int Height, int Width, const VS & masu) {
+int bfs2D(pair<int, int> s, pair<int, int> t, int Height, int Width, VS & masu) {
 	vector<vector<int> > d(Height, vector<int>(Width, 1e9));
+
 
 	d[s.first][s.second] = 0;
 	queue<pair<int, pair<int, int>>>q;
@@ -49,10 +40,10 @@ int bfs2D(pair<int, int> s, pair<int, int> t, int Height, int Width, const VS & 
 		pair<int, pair<int, int>>a = q.front(); q.pop();
 		int dist = a.first; pair<int, int> pos = a.second;
 		if (d[pos.first][pos.second] < dist)continue;
-		FOR(k, 0, 4) {
+		FOR(k, 0, 4) { //辺が存在するかはチェックしてないよ
 			int ny = pos.first + DY[k]; int nx = pos.second + DX[k];
 			if (IN(ny, nx, Height, Width)) {
-				if (masu[ny][nx] == '.')
+				if (masu[ny][nx] == '.')// 辺の チェックしてるよ
 					if (d[ny][nx] > d[pos.first][pos.second] + 1) {
 						d[ny][nx] = d[pos.first][pos.second] + 1;
 						q.push(make_pair(d[ny][nx], pair<int, int>(ny, nx)));
@@ -63,26 +54,28 @@ int bfs2D(pair<int, int> s, pair<int, int> t, int Height, int Width, const VS & 
 
 	return d[t.first][t.second]; // (int)
 }
-
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
+	LL H, W; cin >> H >> W;
+	vector<string> a(H);
+	for (int i = 0; i < H; ++i) {
+		cin >> a[i];
+	}
 
-	int H, W; cin >> H >> W;
-	int ans = H * W;
-	VS vs(H);
-	int C = 0;
+	
+	LL ans = 0LL;
 	FOR(i, 0, H) {
-		cin >> vs[i];
 		FOR(j, 0, W) {
-			if (vs[i][j] == '#')C++;
+			if (a[i][j] == '.')ans++;
 		}
 	}
-	int res = bfs2D(PII(0, 0), PII(H - 1, W - 1), H, W, vs);
-	ans = H * W - C - (res + 1);
-	if (res == INF)ans = -1;
-	cout << ans << "\n";
+	
+	int res = bfs2D(PII(0, 0), PII(H - 1, W - 1), H, W, a);
+	ans -= res+1;
+	
+	cout << (res==1e9?-1:ans) << "\n";
 
 	return 0;
 }
