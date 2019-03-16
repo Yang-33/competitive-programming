1621903ea72fe292f2c66dae56e571c055e37740
Yang-33 <kasai.yuta0810@gmail.com>
2019-03-16 16:43:36 +0900

    solve 079_ABC_C.

--- atcoder/ABC/079_ABC_C.cpp
+++ atcoder/ABC/079_ABC_C.cpp
@@ -8,69 +8,57 @@ using VL = vector<LL>;        using VVL = vector<VL>;
 
 #define ALL(a)  begin((a)),end((a))
 #define RALL(a) (a).rbegin(), (a).rend()
-#define PB push_back
-#define EB emplace_back
-#define MP make_pair
 #define SZ(a) int((a).size())
 #define SORT(c) sort(ALL((c)))
 #define RSORT(c) sort(RALL((c)))
 #define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
 #define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
 #define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
-#define debug(x) cerr << #x << ": " << x << endl
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
 const int INF = 1e9;                          const LL LINF = 1e16;
 const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2018/02/14  Problem: 079_abc_c / Link: https://abc079.contest.atcoder.jp/tasks/abc079_c?lang=en  ----- */
-/* ------問題------
+/* -----  2019/03/12  Problem: ABC 079 C / Link: http://abc079.contest.atcoder.jp/tasks/abc079_c  ----- */
 
-駅の待合室に座っているjoisinoお姉ちゃんは、切符を眺めています。
-切符には 4 つの 0 以上 9 以下の整数 A,B,C,D が整理番号としてこの順に書かれています。
-A op1 B op2 C op3 D = 7 となるように、op1,op2,op3 に '+' か '-' を入れて式を作って下さい。
-なお、答えが存在しない入力は与えられず、また答えが複数存在する場合はどれを出力してもよいものとします。
-
-
------問題ここまで----- */
-/* -----解説等-----
-
-2^N全探索
-
-----解説ここまで---- */
-
-LL N;
-
-LL ans = 0LL;
 
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
-	cin >> N;
+	string s; cin >> s;
 	VI a(4);
 	FOR(i, 0, 4) {
-		a[i] = N / (int)pow(10, 3 - i);
-		a[i] %= 10;
+		a[i] = s[i] - '0';
 	}
-	string OP = "+-";
-	FOR(op, 0, 1<<3) {
-		int ret =a[0];
-		FOR(i, 1, 4) {
-			if (op&(1 << (i - 1))) {
-				ret -= a[i];
+	FOR(state, 0, 1 << 3) {
+		int ret = a.front();
+		FOR(i, 0, 3) {
+			if (state & 1 << i) {
+				ret -= a[i + 1];
 			}
-			else ret += a[i];
+			else {
+				ret += a[i + 1];
+			}
 		}
+
+
 		if (ret == 7) {
-			cout << a[0];
-			FOR(i, 1, 4) {
-				cout << OP[(op&(1 << (i - 1))) ? 1 : 0];
+			FOR(i, 0, 4) {
 				cout << a[i];
+				if (i < 3) {
+					cout << "+-"[state >> i & 1];
+				}
 			}
 			cout << "=7" << endl;
-			break;
+			return 0;
 		}
 	}
-
+	
 	return 0;
 }
