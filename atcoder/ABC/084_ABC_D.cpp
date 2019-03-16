a5f47d7ce6143b90c37ff186b95b499f4122a111
Yang-33 <kasai.yuta0810@gmail.com>
2019-03-16 16:43:40 +0900

    solve 084_ABC_D.

--- atcoder/ABC/084_ABC_D.cpp
+++ atcoder/ABC/084_ABC_D.cpp
@@ -8,75 +8,69 @@ using VL = vector<LL>;        using VVL = vector<VL>;
 
 #define ALL(a)  begin((a)),end((a))
 #define RALL(a) (a).rbegin(), (a).rend()
-#define PB push_back
-#define EB emplace_back
-#define MP make_pair
 #define SZ(a) int((a).size())
 #define SORT(c) sort(ALL((c)))
 #define RSORT(c) sort(RALL((c)))
 #define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
 #define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
 #define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
-#define debug(x) cerr << #x << ": " << x << endl
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
 const int INF = 1e9;                          const LL LINF = 1e16;
 const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2018/02/23  Problem: 084_abc_d / Link: https://abc084.contest.atcoder.jp/tasks/abc084_d?lang=en  ----- */
-/* ------問題------
+/* -----  2019/03/12  Problem: ABC 084 D / Link: http://abc084.contest.atcoder.jp/tasks/abc084_d  ----- */
 
-「N も (N+1)÷2 も素数」を満たす奇数 N を 2017に似た数 とします。
-Q 個のクエリが与えられます。
-クエリ i(1≦i≦Q) では奇数 li,ri が与えられるので、li≦x≦ri かつ 2017に似た数 となる奇数 x の個数を求めてください。
 
------問題ここまで----- */
-/* -----解説等-----
+struct Cumusum {
+	vector<LL>csum;
+	Cumusum(vector<LL>&a) {
+		csum.assign((int)a.size() + 1, 0);
+		for (int i = 0; i < (int)a.size(); i++) {
+			csum[i + 1] = csum[i] + a[i];
+		}
+	}
+	LL query(int l, int r) {
+		return csum[r] - csum[l];
+	}
+};
 
-素数表ができれば後は適当に2017っぽい数がわかるので累積和をとれば十分高速。
-
-----解説ここまで---- */
-
-LL N;
-
-LL ans = 0LL;
-#define primeN 100000
-bool prime[primeN + 1];// 外部だとハッシュ表みたいになる
-void make_prime() {
-	FOR(i, 0, primeN + 1) {
+auto make_prime(long long  N) {
+	vector<long long >a;
+	vector<LL> prime(N + 1,0);
+	FOR(i, 0, N + 1) {
 		prime[i] = 1;
 	}prime[0] = prime[1] = 0;
 
-	for (int i = 2; i <= primeN; i++) {
+	for (LL i = 2; i <= N; i++) {
 		if (prime[i]) {
-			for (int j = i * 2; j <= primeN; j += i)
+			a.push_back((long long)i);
+			for (LL j = i * 2; j <= N; j += i)
 				prime[j] = 0;
 		}
 	}
+	return prime;
 }
-#undef primeN 
-
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
-	make_prime();
-
-	VI like2017(100001, 0);
-	FOR(i, 1, 100001) {
-		if (i % 2 == 1 && prime[i] && prime[(i + 1) / 2]) {
-			like2017[i] = 1;
-		}
+	auto ispr = make_prime(1e5 + 10);
+	VL like2017(1e5 + 10);
+	FOR(i, 3, 1e5 + 10) {
+		if (ispr[i] && ispr[(i + 1) / 2])like2017[i] = 1;
 	}
-	VI sum(100002, 0);
-	FOR(i, 0, 100001) {
-		sum[i + 1] += sum[i]+ like2017[i];
-	}
-
-	cin >> N;
-	FOR(i, 0, N) {
-		int L, R;
-		cin >> L >> R;
-		cout << sum[R+1] - sum[L] << endl;
+	Cumusum psum(like2017);
+	LL Q; cin >> Q;
+	FOR(_, 0, Q) {
+		LL L, R; cin >> L >> R;
+		LL ans = psum.query(L,R+1);
+		cout << (ans) << "\n";
 	}
 
 	return 0;
