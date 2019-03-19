f24317506668069e1dd4b4d2b48db16ab7761816
Yang-33 <kasai.yuta0810@gmail.com>
2019-03-19 23:31:23 +0900

    solve 070_ABC_D.

--- atcoder/ABC/070_ABC_D.cpp
+++ atcoder/ABC/070_ABC_D.cpp
@@ -8,95 +8,86 @@ using VL = vector<LL>;        using VVL = vector<VL>;
 
 #define ALL(a)  begin((a)),end((a))
 #define RALL(a) (a).rbegin(), (a).rend()
-#define PB push_back
-#define EB emplace_back
-#define MP make_pair
 #define SZ(a) int((a).size())
 #define SORT(c) sort(ALL((c)))
 #define RSORT(c) sort(RALL((c)))
 #define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
 #define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
 #define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
-#define debug(x) cerr << #x << ": " << x << endl
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
 const int INF = 1e9;                          const LL LINF = 1e16;
 const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2018/02/15  Problem: 070_abc_d / Link: https://abc070.contest.atcoder.jp/tasks/abc070_d?lang=en  ----- */
-/* ------問題------
+/* -----  2019/03/19  Problem: ABC 070 D / Link: http://abc070.contest.atcoder.jp/tasks/abc070_d  ----- */
 
-N 頂点の木が与えられます。
-木とはグラフの一種であり、頂点の数を N とすると、辺の数が N−1 本である閉路のない連結グラフです。
-i(1≦i≦N−1) 番目の辺は 頂点 ai と 頂点 bi を距離 ci で結びます。
-
-また、Q 個の質問クエリと整数 K が与えられます。
-j(1≦j≦Q) 番目の質問クエリでは、頂点 xj から 頂点 K を経由しつつ、頂点 yj まで移動する場合の最短経路の距離を求めてください。
-
------問題ここまで----- */
-/* -----解説等-----
-
-要はKからの距離を求めてと書いてあるので、d[K->x] + d[K->y]がわかれば良い。
-一回ダイクストラすればいいよね
-
-----解説ここまで---- */
-void D(vector<vector<PLL>>& G, VL&dist, int s, int t) {
-
-	const int INF = INT_MAX;
-	const long long INIT_NUM = LLONG_MAX / 10;// dist の型
-#define DijkV 200020
-
-	priority_queue<PLL, vector<PLL>, greater<PLL>> que; //優先度付きqueue 降順(距離、頂点)
-	que.push(PLL(0LL, s)); // push(距離,頂点)
-
-	while (!que.empty()) {
-		PLL p = que.top(); que.pop(); //queueのデータ構造
-		int v; /* 頂点*/ long long d; /* 頂点vまでの距離 */
-		tie(d, v) = p;
-
-		if (d > dist[v]) continue; //最適でないならば考慮しない
-
-		FOR(i, 0, (int)G[v].size()) { //頂点vからはi本の辺が存在
-			int nv = G[v][i].first; // v->nv
-			if (dist[nv] > dist[v] + G[v][i].second) {
-				dist[nv] = dist[v] + G[v][i].second;
-				que.push(PLL(dist[nv], nv)); //push(距離,頂点) 
+struct LCAT {
+	int N;
+	int it;
+	int Root;
+	vector<vector<PII>>table;
+	vector<int>id;
+	vector<vector<pair<int, LL>>>G;
+	vector<LL>distarray;
+	LCAT(int N) :N(N), it(0), Root(0), table(18, vector<PII>(2 * N)), id(N), G(N), distarray(N) {}
+	void add_edge(int a, int b, LL c = 1) {
+		G[a].emplace_back(b, c);
+		G[b].emplace_back(a, c);
+	}
+	void build(int root) {
+		Root = root;
+		dfs(root, -1, 0, 0);
+		int m = (N << 1) - 1;
+		int h = 31 - __builtin_clz(m);
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j + (1 << i) < m; j++) {
+				table[i + 1][j] = min(table[i][j], table[i][j + (1 << i)]);
 			}
 		}
 	}
-
-#undef DijkV
-}
-
-LL N;
-
-LL ans = 0LL;
-
+	void dfs(int v, int p, int d, LL distd) {
+		id[v] = it;
+		distarray[v] = distd;
+		table[0][it++] = { d, v };
+		for (auto to : G[v]) {
+			if (to.first == p)continue;
+			dfs(to.first, v, d + 1, distd + to.second);
+			table[0][it++] = { d, v };
+		}
+	}
+	int lca(int u, int v) {
+		u = id[u], v = id[v];
+		if (u > v) swap(u, v);
+		int b = 31 - __builtin_clz(v + 1 - u);
+		return min(table[b][u], table[b][v + 1 - (1 << b)]).second;
+	}
+	LL dist(int u, int v) {
+		return distarray[u] + distarray[v] - 2 * distarray[lca(u, v)];
+	}
+};
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
-	cin >> N;
-	vector<vector<PLL>> G(N);
-	FOR(i, 0, N-1) {
-		int a, b, c;
-		cin >> a >> b >> c;
+	LL N; cin >> N;
+	LCAT tree(N);
+	FOR(i, 0, N - 1) {
+		LL a, b, c; cin >> a >> b >> c;
 		a--, b--;
-		G[a].push_back(PLL(b, c));
-		G[b].push_back(PLL(a, c));
+		tree.add_edge(a, b, c);
 	}
-
-	int Q, K; cin >> Q >> K;
+	LL Q, K; cin >> Q >> K;
 	K--;
-	VL dist(N, LINF);
-	dist[K] = 0;
-
-	D(G, dist, K,N);
-	FOR(i, 0, Q) {
-		int x, y;
-		cin >> x >> y;
-		x--; y--;
-		ans = dist[x] + dist[y];
-		cout << ans << "\n";
+	tree.build(K);
+	FOR(_, 0, Q) {
+		LL a, b; cin >> a >> b;
+		a--, b--;
+		cout << tree.distarray[a] + tree.distarray[b] << endl;
 	}
 
 	return 0;
