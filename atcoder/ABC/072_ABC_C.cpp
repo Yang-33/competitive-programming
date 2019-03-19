f7a2570d389a5832c8a81e0c5b984b1287e33b6b
Yang-33 <kasai.yuta0810@gmail.com>
2019-03-19 23:31:24 +0900

    solve 072_ABC_C.

---
+++ atcoder/ABC/072_ABC_C.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2019/03/19  Problem: ABC 072 C / Link: http://abc072.contest.atcoder.jp/tasks/abc072_c  ----- */
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL N; cin >> N;
+	VL a(N);
+	VL b(1e5 + 10);
+	FOR(i, 0, N) {
+		cin >> a[i];
+		a[i]++;
+		b[a[i]]++;
+	}
+	LL ans = 0;
+	FOR(i, 0, N) {
+		LL c = a[i];
+		ans = max(ans, b[c - 1] + b[c] + b[c + 1]);
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
