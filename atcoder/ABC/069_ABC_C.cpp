28d9b454c51775ffaaa72d5ef1edb9c0bb0f7d16
Yang-33 <kasai.yuta0810@gmail.com>
2019-03-19 23:31:22 +0900

    solve 069_ABC_C.

---
+++ atcoder/ABC/069_ABC_C.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2019/03/19  Problem: ABC 069 C / Link: http://abc069.contest.atcoder.jp/tasks/abc069_c  ----- */
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL N; cin >> N;
+	VL a(N);
+	FOR(i, 0, N) {
+		cin >> a[i];
+	}
+	LL two = 0;
+	LL four = 0;
+	FOR(i, 0, N) {
+		LL cnt = 0;
+		while (a[i] % 2 == 0) {
+			a[i] /= 2;
+			cnt++;
+		}
+		if (cnt > 1)four++;
+		else if (cnt)two++;
+	}
+	LL ans = (four * 2 + 1 >= N) || (2 * four + two >= N);
+
+	cout << (ans ? "Yes" : "No") << endl;
+
+	return 0;
+}
