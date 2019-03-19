e127fb061d69a6e665ff28b39db68fb91519be06
Yang-33 <kasai.yuta0810@gmail.com>
2019-03-19 23:31:26 +0900

    solve 075_ABC_D.

--- atcoder/ABC/075_ABC_D.cpp
+++ atcoder/ABC/075_ABC_D.cpp
@@ -1,42 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <::std::uint_fast32_t MODULO> class modint {
-public:
-	using uint32 = ::std::uint_fast32_t; using uint64 = ::std::uint_fast64_t; using value_type = uint32; uint32 a; modint() noexcept : a(0) {}modint(const uint32 x) noexcept : a(x) {}modint operator+(const modint &o) const noexcept { return a + o.a < MODULO ? modint(a + o.a) : modint(a + o.a - MODULO); }modint operator-(const modint &o) const noexcept { return modint(a < o.a ? a + MODULO - o.a : a - o.a); }modint operator*(const modint &o) const noexcept { return modint(static_cast<uint64>(a) * o.a % MODULO); }modint operator/(const modint &o) const { return modint(static_cast<uint64>(a) * (~o).a % MODULO); }modint &operator+=(const modint &o) noexcept { return *this = *this + o; }modint &operator-=(const modint &o) noexcept { return *this = *this - o; }modint &operator*=(const modint &o) noexcept { return *this = *this * o; }modint &operator/=(const modint &o) { return *this = *this / o; }modint operator~() const noexcept { return pow(MODULO - 2); }modint operator-() const noexcept { return a ? modint(MODULO - a) : *this; }modint operator++() noexcept { return a == MODULO - 1 ? a = 0 : ++a, *this; }modint operator--() noexcept { return a ? --a : a = MODULO - 1, *this; }bool operator==(const modint &o) const noexcept { return a == o.a; }bool operator!=(const modint &o) const noexcept { return a != o.a; }bool operator<(const modint &o) const noexcept { return a < o.a; }bool operator<=(const modint &o) const noexcept { return a <= o.a; }bool operator>(const modint &o) const noexcept { return a > o.a; }bool operator>=(const modint &o) const noexcept { return a >= o.a; }explicit operator bool() const noexcept { return a; }explicit operator uint32() const noexcept { return a; }modint pow(uint32 x) const noexcept { uint64 t = a, u = 1; while (x) { if (x & 1)u = u * t % MODULO; t = (t * t) % MODULO; x >>= 1; }	return modint(u); }
-	uint32 get() const noexcept { return a; }
-};
-using mint = modint<1000000007>;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
+/* -----  2019/03/19  Problem: ABC 075 D / Link: http://abc075.contest.atcoder.jp/tasks/abc075_d  ----- */
 
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
-	int N;
-	cin >> N;
-	vector<int> c(N);
-	vector<set<int>>G(2e5 + 10);
-	for (int i = 0; i < N; ++i) {
-		cin >> c[i];
-		G[c[i]].insert(i);
+	LL N, K; cin >> N >> K;
+	VL x(N), y(N);
+	FOR(i, 0, N) {
+		cin >> x[i] >> y[i];
 	}
+	auto in = [](LL a, LL b, LL c) {
+		return a <= b && b <= c;
+	};
+	LL ans = LLONG_MAX;
 
-	vector<mint>dp(N + 1, 0);
-	dp[0] = 1;
-	for (int i = 0; i < N; i++)
-	{
-		dp[i + 1] += dp[i];
-		auto it = G[c[i]].upper_bound(i);
-		if (it != G[c[i]].end() && *it != i + 1) {
-			dp[*it] += dp[i];
+	FOR(i, 0, N) {
+		FOR(j, 0, N) {
+			FOR(k, 0, N) {
+				FOR(l, 0, N) {
+					LL cnt = 0;
+					LL xmax = -LINF, xmin = LINF;
+					LL ymax = -LINF, ymin = LINF;
+					FOR(id, 0, N) {
+						if (in(x[i], x[id], x[j]) && in(y[k], y[id], y[l])) {
+							cnt++;
+							xmax = max(xmax, x[id]);
+							xmin = min(xmin, x[id]);
+							ymax = max(ymax, y[id]);
+							ymin = min(ymin, y[id]);
+						}
+					}
+					if (cnt >= K) {
+						ans = min(ans, (xmax - xmin)*(ymax - ymin));
+					}
+				}
+			}
 		}
 	}
-
-	mint ans = dp.back();
-
-	cout << (ans).get() << "\n";
+	cout << ans << endl;
 
 	return 0;
 }
