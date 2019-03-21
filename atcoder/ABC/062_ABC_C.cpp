ca75c7654ab20de7534a551e52d1a020dbd0756e
Yang-33 <kasai.yuta0810@gmail.com>
2019-03-21 23:29:29 +0900

    solve 062_ABC_C.

---
+++ atcoder/ABC/062_ABC_C.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2019/03/21  Problem: ABC 062 C / Link: http://abc062.contest.atcoder.jp/tasks/abc062_c  ----- */
+
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL H, W; cin >> H >> W;
+	auto f = [](LL H, LL W) {
+		DD(de("---------"))
+			LL ret = LINF;
+		FOR(i, 1, W) { // |
+					   // H*i, []
+			{
+				VL a({ H*i,(H / 2) * (W - i),((H + 1) / 2) * (W - i) });
+				ret = min(ret, *max_element(ALL(a)) - *min_element(ALL(a)));
+			}
+			{
+				VL a({ H*i,H*((W - i) / 2), H*((W - i + 1) / 2) });
+				ret = min(ret, *max_element(ALL(a)) - *min_element(ALL(a)));
+			}
+		}
+
+		return ret;
+	};
+
+
+	LL ans = f(H, W);
+	swap(H, W);
+	ans = min(ans, f(H, W));
+	cout << (ans) << "\n";
+
+	return 0;
+}
