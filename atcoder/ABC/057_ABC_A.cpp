cef52f3c166ae5fbfdb6c5f99d0a58d19b846e2b
Yang-33 <kasai.yuta0810@gmail.com>
2019-03-21 23:29:24 +0900

    solve 057_ABC_A.

--- atcoder/ABC/057_ABC_A.cpp
+++ atcoder/ABC/057_ABC_A.cpp
@@ -1,35 +1,43 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/03/27  Problem: ABC 057 A / Link: http://abc057.contest.atcoder.jp/tasks/abc057_a ----- */
-/* ------蝠城｡---
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-イルカはプログラミングコンテスト好きで、今日はAtCoderのコンテストに参加します。
-現在時刻は、24 時間表記 (0:00〜23:59) で A 時ちょうどであり、コンテストがちょうど B 時間後に始まります。
-コンテストの開始時刻は、24 時間表記で何時ちょうどでしょうか?
+/* -----  2019/03/21  Problem: ABC 057 A / Link: http://abc057.contest.atcoder.jp/tasks/abc057_a  ----- */
 
------問題ここまで----- */
-/* -----解説等-----
 
-mod => 24
-
-----解説ここまで---- */
-
-ll N,M;
-
-ll ans = 0LL;
-
-int main()
-{
+int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
-    cin >> N >> M;
-    cout << (N + M) % 24 << endl;
+	LL A, B; cin >> A >> B;
+	A += B;
+	
+	LL ans = A%24;
+	
+	
+	cout << (ans) << "\n";
 
-	cout << ans << endl;
-	//
 	return 0;
 }
