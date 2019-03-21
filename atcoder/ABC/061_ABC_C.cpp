461d9b26130ed93eeca143aa01bf01cfa0cd8e8a
Yang-33 <kasai.yuta0810@gmail.com>
2019-03-21 23:29:28 +0900

    solve 061_ABC_C.

--- atcoder/ABC/061_ABC_C.cpp
+++ atcoder/ABC/061_ABC_C.cpp
@@ -8,62 +8,43 @@ using VL = vector<LL>;        using VVL = vector<VL>;
 
 #define ALL(a)  begin((a)),end((a))
 #define RALL(a) (a).rbegin(), (a).rend()
-#define PB push_back
-#define EB emplace_back
-#define MP make_pair
 #define SZ(a) int((a).size())
 #define SORT(c) sort(ALL((c)))
 #define RSORT(c) sort(RALL((c)))
 #define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
 #define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
 #define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
-#define debug(x) cerr << #x << ": " << x << endl
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
 const int INF = 1e9;                          const LL LINF = 1e16;
 const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2018/02/14  Problem: 061_abc_c / Link: https://abc061.contest.atcoder.jp/tasks/abc061_c?lang=en  ----- */
-/* ------問題------
+/* -----  2019/03/21  Problem: ABC 061 C / Link: http://abc061.contest.atcoder.jp/tasks/abc061_c  ----- */
 
-空の配列が 1 つあります。
-この配列に、整数を配列に挿入する操作を N 回行います。
-i(1≦i≦N) 回目の操作では、配列に整数 ai を bi 個挿入します。
-N 回の挿入操作後の配列の中で、K 番目に小さい数を求めてください。
-例えば、配列が {1,2,2,3,3,3} の時、4 番目に小さい数は 3 となります。
-
------問題ここまで----- */
-/* -----解説等-----
-
-sortして中身を計算すれば良い。
-
-----解説ここまで---- */
-
-LL N,K;
-
-LL ans = 0LL;
 
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
-	cin >> N >> K;
-	vector<PII>v;
+	LL N, K; cin >> N >> K;
+	map<LL, LL>M;
 	FOR(i, 0, N) {
-		int a, b; cin >> a >> b;
-		v.push_back(PII(a, b));
+		LL a, c; cin >> a >> c;
+		M[a] += c;
 	}
-	SORT(v);
-	LL cnt = 0;
-	FOR(i, 0, SZ(v)) {
-		PII a = v[i];
-
-		cnt += a.second;
-		if (cnt >= K) {
-			ans = a.first;
-			break;
+	for (auto it : M) {
+		K -= it.second;
+		if (K <=0) {
+			cout << it.first << endl;
+			return 0;
 		}
 	}
-	cout << ans << "\n";
+	
 
 	return 0;
 }
