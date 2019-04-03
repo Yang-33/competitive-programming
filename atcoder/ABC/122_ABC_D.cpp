7d0eb42b11e3c9cfaf3fe3986b90031014ff4358
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-03 15:17:07 +0900

    solve 122_ABC_D.

---
+++ atcoder/ABC/122_ABC_D.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2019/03/24  Problem: ABC 122 D / Link: http://abc122.contest.atcoder.jp/tasks/abc122_d  ----- */
+
+
+template <::std::uint_fast32_t MODULO> class modint {
+public:
+	using uint32 = ::std::uint_fast32_t; using uint64 = ::std::uint_fast64_t; using value_type = uint32; uint32 a; modint() noexcept : a(0) {}modint(const uint32 x) noexcept : a(x) {}modint operator+(const modint &o) const noexcept { return a + o.a < MODULO ? modint(a + o.a) : modint(a + o.a - MODULO); }modint operator-(const modint &o) const noexcept { return modint(a < o.a ? a + MODULO - o.a : a - o.a); }modint operator*(const modint &o) const noexcept { return modint(static_cast<uint64>(a) * o.a % MODULO); }modint operator/(const modint &o) const { return modint(static_cast<uint64>(a) * (~o).a % MODULO); }modint &operator+=(const modint &o) noexcept { return *this = *this + o; }modint &operator-=(const modint &o) noexcept { return *this = *this - o; }modint &operator*=(const modint &o) noexcept { return *this = *this * o; }modint &operator/=(const modint &o) { return *this = *this / o; }modint operator~() const noexcept { return pow(MODULO - 2); }modint operator-() const noexcept { return a ? modint(MODULO - a) : *this; }modint operator++() noexcept { return a == MODULO - 1 ? a = 0 : ++a, *this; }modint operator--() noexcept { return a ? --a : a = MODULO - 1, *this; }bool operator==(const modint &o) const noexcept { return a == o.a; }bool operator!=(const modint &o) const noexcept { return a != o.a; }bool operator<(const modint &o) const noexcept { return a < o.a; }bool operator<=(const modint &o) const noexcept { return a <= o.a; }bool operator>(const modint &o) const noexcept { return a > o.a; }bool operator>=(const modint &o) const noexcept { return a >= o.a; }explicit operator bool() const noexcept { return a; }explicit operator uint32() const noexcept { return a; }modint pow(uint32 x) const noexcept { uint64 t = a, u = 1; while (x) { if (x & 1)u = u * t % MODULO; t = (t * t) % MODULO; x >>= 1; }	return modint(u); }
+	uint32 get() const noexcept { return a; }
+};
+using mint = modint<MOD>;
+
+
+mint dp[102][5][5][5];
+
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL N;
+	cin >> N;
+	const int A = 1;
+	const int T = 2;
+	const int G = 3;
+	const int C = 4;
+	dp[0][0][0][0] = 1;
+	FOR(i, 0, N) {
+		FOR(a, 0, 5) {
+			FOR(b, 0, 5) {
+				FOR(c, 0, 5) {
+					FOR(d, 1, 5) {
+						if (b == A && c == G && d == C)continue;
+						if (b == G && c == A && d == C)continue;
+						if (b == A && c == C && d == G)continue;
+						if (a == A && b == G && d == C)continue;
+						if (a == A && c == G && d == C)continue;
+						dp[i + 1][b][c][d] += dp[i][a][b][c];
+					}
+				}
+			}
+		}
+	}
+
+
+	mint ans = 0;
+	if (N <= 2) {
+		if (N == 1)ans = 4;
+		else if (N == 2)ans = 16;
+	}
+	else {
+		FOR(a, 1, 5) {
+			FOR(b, 1, 5) {
+				FOR(c, 1, 5) {
+					ans += dp[N][a][b][c];
+				}
+			}
+		}
+	}
+
+
+	cout << (ans).get() << "\n";
+
+	return 0;
+}
