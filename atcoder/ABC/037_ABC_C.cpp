a1a32bac03a94083cbc7f16ba5acb96fa8d6d9ce
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-05 16:26:41 +0900

    solve 037_ABC_C.

---
+++ atcoder/ABC/037_ABC_C.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2019/04/05  Problem: ABC 037 C / Link: http://abc037.contest.atcoder.jp/tasks/abc037_c  ----- */
+
+struct Cumusum {
+	vector<LL>csum;
+	Cumusum(vector<LL>&a) {
+		csum.assign((int)a.size() + 1, 0);
+		for (int i = 0; i < (int)a.size(); i++) {
+			csum[i + 1] = csum[i] + a[i];
+		}
+	}
+	LL query(int l, int r) {
+		return csum[r] - csum[l];
+	}
+};
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL N, K; cin >> N >> K;
+	vector<LL> a(N);
+	for (int i = 0; i < N; ++i) {
+		cin >> a[i];
+	}
+	Cumusum asum(a);
+	LL ans = 0LL;
+	FOR(i, 0, N - K + 1) {
+		ans += asum.query(i, i + K);
+	}
+
+
+	cout << (ans) << "\n";
+
+	return 0;
+}
