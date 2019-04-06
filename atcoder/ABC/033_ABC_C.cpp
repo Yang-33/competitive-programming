fc42265690aafd9db37c250894edbe63486cac87
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-06 17:18:08 +0900

    solve 033_ABC_C.

--- atcoder/ABC/033_ABC_C.cpp
+++ atcoder/ABC/033_ABC_C.cpp
@@ -1,59 +1,58 @@
-#include<iostream>
-#include<string>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-#define debug(x) cout<<#x<<": "<<x<<endl
-typedef long long ll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* 2017/01/29 問題 ----- ABC033 C /Link http://abc033.contest.atcoder.jp/tasks/abc033_c */
-/* -----解説等-----
-問題: 次のような制約を満たす数式 S が与えられます。
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 
-演算子は + (加算) と * (乗算) のみからなる。乗算を優先して計算する。
-括弧は存在しない。
-それぞれの項は、 1 桁の整数である。
-例えば、1+3*4*0 、 1+2+3+4+5 などの数式はこの条件を満たしますが、
-12+3+5、4*6*7-3、(3+4)*5+2 のような数式は 条件を満たさないため、入力として与えられません。
-
-あなたは、この数式のうち数字の部分をいくつか選んで 0 に書き換えることで、この式の値を 0 にしたいです。
-式の値を 0 にするために 0 に書き換えなければならない数字の個数の最小値を求めてください。
-
-
-状態を三つに分けることができる。
-+...+のあいだに(...は数字や演算*) 0が存在しない
-+...+のあいだに(...は数字や演算*) 0が存在する
-+...+を見切った
-
-ゼロを発見したら区間の中で０を追加しなくてもよく、なければついかをする。
-最後の区間は+が登場しないので自分で加算をさせる。
-もしくは +S+ としてもよい。
-
-*/
-
-string S;
-int flag = 1;
-int ans = 0;
-
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
+/* -----  2019/04/06  Problem: ABC 033 C / Link: http://abc033.contest.atcoder.jp/tasks/abc033_c  ----- */
 
 
-    cin >> S;
-    FOR(i, 0, S.size()) {
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
-        if (S[i] == '0')flag = 0;
-        if (S[i] == '+') {
-            ans += flag;
-            flag = 1;
-        }
+	string s; cin >> s;
+	// +で切る
+	vector<string>a;
+	string kou;
+	FOR(i, 0, SZ(s)) {
+		if (s[i] == '+') {
+			a.push_back(kou);
+			kou.clear();
+		}
+		else {
+			kou += string(1, s[i]);
+		}
+	}
+	if (SZ(kou))a.push_back(kou);
+	// *内に0のみがあればそれを使用，そうでなければ1
+	LL ans = 0LL;
+	for (auto it : a) {
+		ans += !(count(ALL(it), '0') > 0);
+	}
 
-    }
-    ans += flag;
 
-    cout << ans << endl;
 
-    return 0;
+	cout << (ans) << "\n";
+
+	return 0;
 }
