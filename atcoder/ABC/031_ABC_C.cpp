2d51b8af3c4ecbc84e8f77ec5cf4f18084af4a61
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-06 17:18:07 +0900

    solve 031_ABC_C.

--- atcoder/ABC/031_ABC_C.cpp
+++ atcoder/ABC/031_ABC_C.cpp
@@ -1,75 +1,67 @@
-#include<iostream>
-#include<algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-const int INF = 1e9;
-typedef long long ll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+
+/* -----  2019/04/06  Problem: ABC 031 C / Link: http://abc031.contest.atcoder.jp/tasks/abc031_c  ----- */
 
 
-/* -----  2017/02/20  Problem: ABC031 C / Link: http://abc031.contest.atcoder.jp/tasks/abc031_c ----- */
-/* ------問題------
-高橋君と青木君は長さ N の数列 S を用いたゲームを行う。
-ゲームは高橋君の手番と青木君の手番 1 回ずつからなる。
-ゲームは以下の要領で行われる。
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
-最初に高橋君が数列の要素のうち 1 つに丸をつける。
-次に青木君が数列の要素で高橋君が丸を付けなかったもののうち 1 つに丸をつける。
-高橋君と青木君が丸を付けた 2 つの要素に対して、その 2 つの要素およびそれらの間にあるすべての要素を残して、それ以外の要素をすべて削除する。残った数列を T と置く。
-数列 T のうち、数列 T 内で左から奇数番目の要素の合計が高橋君の得点、偶数番目の要素の合計が青木君の得点となる。
-青木君は、丸を付けられる要素の中で、青木君が得点を最も多く得られる要素に丸を付ける。そのような要素が複数あるならばそれらのうち最も左側にある要素に丸を付ける。
+	LL N; cin >> N;
+	vector<LL> A(N);
+	for (int i = 0; i < N; ++i) {
+		cin >> A[i];
+	}
+	LL ans = -LINF;
+	FOR(i, 0, N) {
+		LL ret = -LINF;
+		LL subans = -LINF;
+		FOR(j, 0, N) {
+			if (i == j)continue;
+			int a = i, b = j;
+			if (a > b)swap(a, b);
+			LL ta = 0, ao = 0;
+			FOR(k, a, b + 1) {
+				if (k % 2 == a % 2) {
+					ta += A[k];
+				}
+				else {
+					ao += A[k];
+				}
+			}
+			if (ret < ao) {
+				ret = ao;
+				subans = ta;
+			}
+		}
+		ans = max(ans, subans);
+	}
+	
+	
+	cout << (ans) << "\n";
 
-高橋君は青木君の丸の付け方を知っている。高橋君が得られる得点として考えられる得点の最大値を求めよ。
-*/
-/* -----解説等-----
-
-区間の両端に対して問題となっている動作を行う。
-Nが小さいのでN^3の全探索でまにあう。
-尺取り法だとN^3?ではなくN^2で解ける気もする。
-
-*/
-
-ll N;
-int a[50];
-ll ans = 0LL;
-
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-
-    cin >> N;
-    FOR(i, 0, N) {
-        cin >> a[i];
-    }
-
-    ans = -INF;
-    FOR(i, 0, N) {
-        int _p2 = -INF; ll tmp;
-        FOR(j, 0, N) {
-            
-            int l = min(i, j);
-            int r = max(i, j);
-            if (l == r)continue;
-            
-            int p1 = 0, p2 = 0;
-            int prm = 1;
-            
-            FOR(k, l, r + 1) {
-                if (prm % 2 == 1) p1 += a[k];
-                else p2 += a[k];
-                prm++;
-            }
-            
-            if (p2 > _p2) {
-                _p2 = p2;
-                tmp = p1;
-            }
-        }
-        ans = max(ans, tmp);
-    }
-
-    cout << ans << endl;
-
-    return 0;
+	return 0;
 }
