ccdc70ebc66bd9ad4e92ff31810885ab2a755728
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-06 18:18:21 +0900

    solve 006_ARC_B.

--- atcoder/ARC/006_ARC_B.cpp
+++ atcoder/ARC/006_ARC_B.cpp
@@ -22,64 +22,35 @@ using VL = vector<LL>;        using VVL = vector<VL>;
 #endif
 const int INF = 1e9;                          const LL LINF = 1e16;
 const LL MOD = 1000000007;                    const double PI = acos(-1.0);
-int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2018/12/15  Problem: ARC 006 B / Link: http://arc006.contest.atcoder.jp/tasks/arc006_b  ----- */
-/* ------問題------
+/* -----  2019/04/06  Problem: ARC 006 B / Link: http://arc006.contest.atcoder.jp/tasks/arc006_b  ----- */
 
-　高橋君は学校で班のリーダーを決めなければいけなくなったので、あみだくじを用いて決めることにしました。
- 　あみだくじとは、複数の縦線から 1 本を選び、その上端から下端へと辿っていき、途中で横線があれば、その横線を通り繋がっている隣接する縦線へと移動し、また下へと進みます。
-  　今日はたまたま手元に紙がなかったので、パソコン上で |、-、o を用いて以下のようなあみだくじを作りました。
-   | | | | | | | | |
-   |-| | |-| | |-| |
-   | | |-| | |-| | |
-   | |-| | | | | |-|
-   | | | |-| | | |-|
-   | | |-| |-| | | |
-   |-| | |-| | |-| |
-   | | | | | |-| | |
-   o
-   　o がある位置に到達した人がリーダーになります。
-	　実は高橋君はリーダーになりたかったので、どの縦線を選べば o に辿り着くのか知りたいです。
-
-	 　左から何番目の縦線を選べばリーダーになれるのかを求めなさい。
-
------問題ここまで----- */
-/* -----解説等-----
-
-シュミレーションすればよい。
-
-----解説ここまで---- */
-
-LL ans = 0LL;
 
 int main() {
-	LL N, L; cin >> N >> L;
+	int N, L; cin >> N >> L;
 	cin.ignore();
-	VS vs(L);
-	FOR(i, 0, L) {
+	vector<string>vs(L + 1);
+	FOR(i, 0, L + 1) {
 		getline(cin, vs[i]);
 	}
-	string tar; getline(cin, tar);
-	int pos = -1;
-	FOR(i, 0, SZ(tar)) {
-		if (tar[2 * i] == 'o')pos = 2 * i;
-	}
-
-	FORR(y, L - 1, 0 - 1) {
-		DD(de(pos, SZ(vs[y]), y, vs[y][pos - 1], vs[y][pos], vs[y][pos + 1]));
-		if (pos&&vs[y][pos - 1] == '-') {
+	reverse(ALL(vs));
+	int pos = vs.front().find('o');
+	FOR(i, 1, L + 1) {
+		int l = pos - 1;
+		int r = pos + 1;
+		if (l > 0 && vs[i][l] == '-') {
 			pos -= 2;
 		}
-		else if (vs[y][pos + 1] == '-') {
+		else if (r < SZ(vs[i]) && vs[i][r] == '-') {
 			pos += 2;
 		}
+
 	}
 
-	ans = pos / 2 + 1;
+	LL ans = 1 + pos/2;
 
 
-	cout << ans << "\n";
+	cout << (ans) << "\n";
 
 	return 0;
 }
