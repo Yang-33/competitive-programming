cd1726680b20800b08b96183aacd9a2f635280c0
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-07 22:59:05 +0900

    solve 030_ABC_C.

--- atcoder/ABC/030_ABC_C.cpp
+++ atcoder/ABC/030_ABC_C.cpp
@@ -1,88 +1,78 @@
-#include<iostream>
-#include<cstdio>
-#include<algorithm>
-#include<cmath>
-#include<string>
-#include<cstring>
-#include<vector>
-#include<map>
-#include<list>
-#include<stack>
-#include<queue>
-#include<climits> //INT_MIN/MAX
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-#define FORR(i,s,e) for(ll (i)=(s);(i)>(e);(i)--)
-#define MOD 1000000007
-#define debug(x) cout<<#x<<": "<<x<<endl
-const int INF = 1e9;
-typedef long long ll;
-int dx[8] = { 1,1,1,0,0,-1,-1,-1 };
-int dy[8] = { 1,0,-1,1,-1,1,0,-1 };
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/02/20  Problem: ABC030 C / Link: http://abc030.contest.atcoder.jp/tasks/abc030_c ----- */
-/* ------問題------
-ウナギの高橋くんは飛行機に乗ることが趣味です。今回は空港Aと空港Bを往復することにしました。
-空港Aから空港Bの飛行機には X 時間かかり、空港Bから空港Aへの飛行機には Y 時間かかります。
-空港Aから空港Bへの飛行機は N 本あり、i 番目の便は ai 時に出発します。 空港Bから空港Aへの飛行機は M 本あり、j 番目の便は bj 時に出発します。
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 
-ある飛行機には、出発する空港に出発する時刻以前にいれば乗ることができます。
-出発する時刻ちょうどに到着した場合も、すぐに飛行機に乗って出発できます。
-高橋くんははじめ空港Aに 0 時にいます。 空港Aと空港Bの間を最大何往復できるか調べてください。
-*/
-/* -----解説等-----
+/* -----  2019/04/07  Problem: ABC 030 C / Link: http://abc030.contest.atcoder.jp/tasks/abc030_c  ----- */
 
-はやく行っても損はないので、はやく行く。
-次の便の検索には時刻が昇順であることに気付き、lower_boundを使うまたはお手製の二分探索機を
-つかって検索すれば、Ｏ(max(N,M)log(max(N,M)))で解ける。
+// [L,R] 
+int intervalScheduling(vector<PLL>& sc, bool duplicate = 0) {
+	sort(sc.begin(), sc.end(), [](const PLL& L, const PLL & R) {
+		if (L.second == R.second) {
+			return L.first < R.first;
+		}
+		else {
+			return L.second < R.second;
+		}
+	});
 
-*/
+	int ret = 0;
+	int Rpos = 0;
+	FOR(i, 0, SZ(sc)) {
+		if (Rpos < sc[i].first + duplicate) {
+			ret++;
+			Rpos = sc[i].second;
+		}
+	}
 
-ll N,M,X,Y;
-vector<int> a;
-vector<int> b;
-int ans = 0LL;
+	return ret;
+}
 
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
-    cin >> N >> M >> X >> Y;
+	LL N, M; cin >> N >> M;
+	LL X, Y; cin >> X >> Y;
+	vector<LL> a(N);
+	for (int i = 0; i < N; ++i) {
+		cin >> a[i];
+	}
+	vector<LL> b(M);
+	for (int i = 0; i < M; ++i) {
+		cin >> b[i];
+	}
+	vector<PLL>interval;
+	FOR(i, 0, N) {
+		int bidx = lower_bound(ALL(b), a[i] + X) - b.begin();
+		if (bidx == M)continue;
+		LL at = b[bidx] + Y;
+		interval.push_back(PLL(a[i], at));
+	}
+	int ans = intervalScheduling(interval, 1);
 
-    FOR(i, 0, N) {
-        int d;
-        cin >> d;
-        a.push_back(d);
-    }
-    FOR(i, 0, M) {
-        int d;
-        cin >> d;
-        b.push_back(d);
-    }
 
-    int state = 0;
-    int ntime = 0;
-    while (1) {
-        if (state == 0) {
-            auto it = lower_bound(a.begin(), a.end(),ntime) ;
-            int id = distance(a.begin(), it);
-            if (id == N)break;
-            ntime = a[id] + X;
-            state = 1;
-        }
-        else {
-            auto it  = lower_bound(b.begin(), b.end(), ntime);
-            int id = distance(b.begin(), it);
-            if (id == M) break;
-            ntime = b[id] + Y;
-            ans++;
-            state = 0;
-        }
-    }
+	cout << (ans) << "\n";
 
-    cout << ans << endl;
-
-    return 0;
+	return 0;
 }
