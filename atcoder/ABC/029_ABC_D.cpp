b2f7ee7652c4739530f132fc6d37fd410079142c
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-07 22:59:04 +0900

    solve 029_ABC_D.

--- atcoder/ABC/029_ABC_D.cpp
+++ atcoder/ABC/029_ABC_D.cpp
@@ -1,67 +1,58 @@
-#include<iostream>
-#include<string>
-#include<stdlib.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-typedef long long ll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/02/22  Problem: ABC029 D / Link: http://abc029.contest.atcoder.jp/tasks/abc029_d ----- */
-/* ------問題------
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 
-高橋君は 1 以上 N 以下のすべての整数を十進表記で一回ずつ紙に書きました。
-この作業で、高橋君は 1 という数字を何個書いたでしょうか。
-整数 N (1 ≦ N < 10^9) が与えられる。
+/* -----  2019/04/07  Problem: ABC 029 D / Link: http://abc029.contest.atcoder.jp/tasks/abc029_d  ----- */
 
------問題ここまで----- */
-/* -----解説等-----
 
-digit dp
-通常の検索を行うと同じ部分を何度も検索してしまうのでこれをメモしておくことで高速になる。
-1の個数に着目したい のでこれを桁dpの条件に追加する。
-dp[ 現在の桁 ][ 1の個数 ][ Ｎ未満という事が確定しているか ] := 総数
-N未満であると言い切れない場合には場合分けをしておけばよい。
-位の大きい桁から順に数を下していき、
-1の出現数を末端まで伝播させておけばすべての数を列挙し終えた際に正確に数えることができる。
-10*桁数*(はい/いいえ)　ぐらいの計算量
+LL dp[11][11][2];
+LL f(const string &target, int i = 0, int onecnt = 0, int less = 0) {
+	if (i == -1) {
+		return onecnt;
+	}
+	if (dp[i][onecnt][less] != -1 && less == 1)return dp[i][onecnt][less];
+	LL res = 0;
+	int num = target[i] - '0';
+	int Max = (less ? 9 : num);
+	FOR(nx, 0, Max + 1) {
+		res += f(target, i - 1, onecnt + (nx == 1), less || nx < Max);
+	}
+	if (less == 1)return dp[i][onecnt][less] = res;
+	return res;
+}
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
- ----解説ここまで---- */
+	LL n; cin >> n;
+	string N = to_string(n);
+	FOR(i, 0, 11)FOR(j, 0, 11)FOR(k, 0, 2)dp[i][j][k] = -1;
+	reverse(ALL(N));
+	LL ret1 = f(N, SZ(N) - 1);
+	LL ans = ret1;
+	cout << ans << "\n";
 
-string N;
-ll dp[30][20][2];
-
-ll rec(int x,int cnt,int ok) {
-    if (dp[x][cnt][ok] != -1)return dp[x][cnt][ok];
-    if (x == N.size())return cnt;
-
-    if (ok == 0) {
-        ll tmp = 0;
-        for (int i = 0; i < N[x] - '0'; i++) {
-            tmp += rec(x + 1, cnt + (i == 1), 1);
-        }
-        tmp += rec(x + 1, cnt + (N[x] == '1'), 0);
-        return dp[x][cnt][ok] = tmp;
-    }
-    else {
-        ll tmp = 0;
-        for (int i = 0; i < 10; i++) {
-            tmp += rec(x + 1, cnt + (i == 1), 1);
-        }
-        return dp[x][cnt][ok] = tmp;
-    }
+	return 0;
 }
 
-
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-    
-    memset(dp, -1, sizeof(dp));
-
-    cin >> N;
-
-    cout << rec(0,0,0) << endl;
-
-    return 0;
-}
