238d158a19c7870f1c9decc0035c064e2a8c7f58
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-07 22:59:03 +0900

    solve 028_ABC_C.

--- atcoder/ABC/028_ABC_C.cpp
+++ atcoder/ABC/028_ABC_C.cpp
@@ -1,47 +1,53 @@
-#include<iostream>
-#include<set>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-typedef long long ll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/02/23  Problem: ABC028 C / Link: http://abc028.contest.atcoder.jp/tasks/abc028_c ----- */
-/* ------問題------
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 
-異なる整数が 5 個与えられます。
-この中から 3 つ選んでその和で表すことの出来る整数のうち、3 番目に大きいものを出力してください。
+/* -----  2019/04/07  Problem: ABC 028 C / Link: http://abc028.contest.atcoder.jp/tasks/abc028_c  ----- */
 
------問題ここまで----- */
-/* -----解説等-----
 
-重複要素を選ばないようにしてsetで管理。三番目に大きいものを求めたいのでイテレーターを３つずらせばよい。
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
- ----解説ここまで---- */
+	int N = 5;
+	vector<int> a(N);
+	for (int i = 0; i < N; ++i) {
+		cin >> a[i];
+	}
+	VI b;
+	FOR(i, 0, N) {
+		FOR(j, i + 1, N) {
+			FOR(k, j + 1, N) {
+				b.push_back(a[i] + a[j] + a[k]);
+			}
+		}
+	}
+	RSORT(b);
+	LL ans = b[2];
+	
+	
+	cout << (ans) << "\n";
 
-int a[5];
-set<int >s;
-ll ans = 0LL;
-
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-    
-    FOR(i,0,5)
-    cin >> a[i];
-
-    FOR(i, 0, 5)FOR(j, 0, 5)FOR(k, 0, 5) {
-        if (i != j&&j != k&&k != i) {
-            s.insert(a[i] + a[j] + a[k]);
-        }
-    }
-    auto it = s.end();
-    it--;
-    it--;
-    it--;
-
-    ans = *it;
-    cout << ans << endl;
-
-    return 0;
+	return 0;
 }
