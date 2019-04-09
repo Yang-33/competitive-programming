8a7ae472ce3f58b93911793df51d309e58c4e6f4
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-09 23:28:55 +0900

    solve 024_ABC_C.

--- atcoder/ABC/024_ABC_C.cpp
+++ atcoder/ABC/024_ABC_C.cpp
@@ -1,77 +1,63 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-typedef long long ll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/02/26  Problem: ABC024 C / Link: http://abc024.contest.atcoder.jp/tasks/abc024_c ----- */
-/* ------問題------
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-高橋王国には N 個の街があり、それぞれ 1 〜 N の整数によって番号付けされています。
-高橋王国には K 種類の民族が住んでおり、i 番目の民族は街 Si に住んでいます。
-高橋王国の民族たちには、百年に一回住む街を変える「民族大移動」という文化が有ります。 基本的には全民族が同時期に「民族大移動」を行うのですが、全く同じ日に全民族が移動すると混雑が予想されるため、 以下の様な移動制限を毎日設けて、 D 日かけて行います。
-i 日目は 街の番号が Li 以上 Ri 以下であるよう街の間を自由に行き来できる。それ以外の行き来は禁止される。
-各民族はこの移動制限を守り、いくつかの街を経由しながら目的地の街まで移動します。
-i 番目の民族の目的地は街 Ti です。どの民族もできるだけ早く目的地に到着したいと思っています。
-各民族について、目的地に到着できる最も早い日を求めてください。
-なお、どの民族も D 日以内に目的地に到着できることが保証されています。
+/* -----  2019/04/09  Problem: ABC 024 C / Link: http://abc024.contest.atcoder.jp/tasks/abc024_c  ----- */
 
------問題ここまで----- */
-/* -----解説等-----
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
-区間をできるだけ移動しても問題ないことから、
-町の番号が増加する方向に移動したいならば各日にちごとに最大の町の番号まで移動すればよい。
-一方、町の番号が減少する方向に移動したいならば各日にちごとに最小の町の番号へ進めばよい。
+	LL N, D, K; cin >> N >> D >> K;
+	VL L(D), R(D);
+	FOR(i, 0, D) {
+		cin >> L[i] >> R[i];
+	}
+	FOR(_, 0, K) {
+		int s, t; cin >> s >> t;
+		bool rev = s > t;
+		LL ans = 0LL;
+		FOR(i, 0, D) {
+			if (L[i] <= s && s <= R[i]) {
+				s = R[i];
+				if (rev)s = L[i];
+				if (!rev&&t <= s) {
+					ans = i + 1;
+					break;
+				}
+				if (rev&& s <= t) {
+					ans = i + 1;
+					break;
+				}
+			}
+		}
+		cout << ans << "\n";
+	}
 
-きれいに実装できない。
 
- ----解説ここまで---- */
 
-ll N, D, K;
-pair<int, int>q[10000];
-pair<int, int>x[100];
-ll ans = 0LL;
 
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-
-    cin >> N >> D >> K;
-
-    FOR(i, 0, D) {
-        cin >> q[i].first >> q[i].second;
-    }
-    FOR(i, 0, K) {
-        cin >> x[i].first >> x[i].second;
-    }
-
-    FOR(i, 0, K) {
-        if (x[i].first < x[i].second) {
-            int m1 = x[i].first, m2 = x[i].second;
-            FOR(j, 0, D) {
-                if (q[j].first <= m1&&m1 <= q[j].second) {
-                    m1 = q[j].second;
-                    if (m1 >= m2) {
-                        cout << j + 1 << endl;
-                        break;
-                    }
-                }
-            }
-        }
-        else {
-            int m1 = x[i].first, m2 = x[i].second;
-            FOR(j, 0, D) {
-                if (q[j].first <= m1 && m1 <= q[j].second) {
-                    m1 = q[j].first;
-                    if (m1 <= m2) {
-                        cout << j + 1 << endl;
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    return 0;
+	return 0;
 }
