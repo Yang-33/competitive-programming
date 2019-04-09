bbe604d7f3c97e32cbbbc4c294495ee66cf1f4f7
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-09 23:28:55 +0900

    solve 024_ABC_D.

--- atcoder/ABC/024_ABC_D.cpp
+++ atcoder/ABC/024_ABC_D.cpp
@@ -1,69 +1,47 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-#define MOD 1000000007
-typedef long long ll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/02/26  Problem: ABC024 D / Link: http://abc024.contest.atcoder.jp/tasks/abc024_d ----- */
-/* ------問題------
-高橋君はタテ、ヨコともに 108 マスずつある方眼紙を使って以下の問題を解くことにしました。
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-「一番左下のマスから開始して、右もしくは上に1マス移動するという操作を繰り返して、
-各マスにたどり着く方法の個数を 1,000,000,007 で割った余りを求めよ。」
-しかし書き込み終わったあと、達成感のために舞い上がってしまい、方眼紙の一部を破いてしまいました。
+/* -----  2019/04/09  Problem: ABC 024 D / Link: http://abc024.contest.atcoder.jp/tasks/abc024_d  ----- */
+template <std::uint_least32_t MODULO> class modint {
+public:
+using uint32 = std::uint_least32_t; using uint64 = std::uint_least64_t; using iint64 = std::int_fast64_t; class optimize_tag_t {}; static constexpr optimize_tag_t optimize_tag{}; public:using value_type = uint32; value_type a; static constexpr value_type cst(iint64 x) noexcept { x %= static_cast<iint64>(MODULO); if (x < static_cast<iint64>(0)) { x += static_cast<iint64>(MODULO); }return static_cast<value_type>(x); }constexpr modint(optimize_tag_t, const value_type &x) noexcept : a(x) {}constexpr modint() noexcept : a(static_cast<value_type>(0)) {}constexpr modint(const iint64 &x) noexcept : a(cst(x)) {}constexpr modint operator+(const modint &o) const noexcept { return modint(optimize_tag, a + o.a < MODULO ? a + o.a : a + o.a - MODULO); }constexpr modint operator-(const modint &o) const noexcept { return modint(optimize_tag, a < o.a ? a + MODULO - o.a : a - o.a); }constexpr modint operator*(const modint &o) const noexcept { return modint(optimize_tag, static_cast<value_type>(static_cast<uint64>(a) * static_cast<uint64>(o.a) % static_cast<uint64>(MODULO))); }constexpr modint operator/(const modint &o) const { return modint(optimize_tag, static_cast<value_type>(static_cast<uint64>(a) * static_cast<uint64>((~o).a) % static_cast<uint64>(MODULO))); }modint &operator+=(const modint &o) noexcept { if ((a += o.a) >= MODULO)a -= MODULO; return *this; }modint &operator-=(const modint &o) noexcept { if (a < o.a)a += MODULO; a -= o.a; return *this; }modint &operator*=(const modint &o) noexcept { a = static_cast<value_type>(static_cast<uint64>(a) * static_cast<uint64>(o.a) % static_cast<uint64>(MODULO)); return *this; }modint &operator/=(const modint &o) { a = static_cast<uint64>(a) * (~o).a % MODULO; return *this; }constexpr modint inverse() const noexcept { assert(a != static_cast<value_type>(0) && "0 does not have inverse"); return pow(static_cast<uint64>(MODULO - static_cast<value_type>(2))); }constexpr modint operator~() const noexcept { return inverse(); }constexpr modint operator-() const noexcept { if (a == static_cast<value_type>(0)) { return modint(optimize_tag, static_cast<value_type>(0)); } else { return modint(optimize_tag, MODULO - a); } }modint &operator++() noexcept { if (++a == MODULO) { a = static_cast<value_type>(0); }return *this; }modint &operator--() noexcept { if (a == static_cast<value_type>(0)) { a = MODULO; }--a; return *this; }constexpr bool operator==(const modint &o) const noexcept { return a == o.a; }constexpr bool operator!=(const modint &o) const noexcept { return a != o.a; }constexpr bool operator<(const modint &o) const noexcept { return a < o.a; }constexpr bool operator<=(const modint &o) const noexcept { return a <= o.a; }constexpr bool operator>(const modint &o) const noexcept { return a > o.a; }constexpr bool operator>=(const modint &o) const noexcept { return a >= o.a; }constexpr explicit operator bool() const noexcept { return a; }constexpr explicit operator value_type() const noexcept { return a; }modint pow(iint64 inx) const noexcept { if (inx < 0)assert(a != static_cast<value_type>(0) && "not pow index < 0"); uint64 x = inx; uint64 t = a, u = 1; while (x) { if (x & 1)u = u * t % MODULO; t = (t * t) % MODULO; x >>= 1; }return modint(optimize_tag, static_cast<value_type>(u)); }
+constexpr value_type get() const noexcept { return a; }
+};
 
-高橋君の手元には、あるマスと、その上のマスと右のマスの部分のみが書かれている方眼紙の欠片があります。
-高橋君はこの欠片を元の位置に戻そうと思ったのですが、方眼紙が大きすぎるので、どこに置けばいいのかわかりません。
-欠片の情報から、この欠片が元々の方眼紙の左から何マス、下から何マスの位置にあったのか求めてください。
-つまり、左からxマス、下からyマスのマスのことを (x,y) と書くとして、
-(r,c)、(r,c+1)、(r+1,c)のマスに書かれている整数が与えられるので、 r と c を求めてください。
-なお、一番左下のマスは (0,0) です。
+using mint = modint<MOD>;
 
------問題ここまで----- */
-/* -----解説等-----
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
-探索はサイズ的に不可能。
+	LL a, b, c; cin >> a >> b >> c;
+	mint d = mint(b*c - a * b - a * c);
+	mint r = mint(a*c - b * c) / d;
+	mint x = mint(a*b - b * c) / d;
+	cout << r.get() << " " << x.get() << endl;
 
-A=(r+c)comb(c)
-B=(r+c+1)comb(c+1)
-C=(r+c+1)comb(c)
-であることからr,cの式はABCを使って解くことができる。
-その式はmodの除算を含むので逆元を用いて積に変換する。
-いつものやつだが差の式がある場合には負にならないように気を付けなければならないので
-+MODを各式に入れておけば安心。
-
- ----解説ここまで---- */
-
-
-ll mod_pow(ll x, ll p, ll mod) {
-    ll a = 1;
-    while (p) {
-        if (p & 1) a = a * x % mod;
-        x = x * x % mod;
-        p >>= 1;
-    }
-    return a;
-}
-
-//逆元　1/a (mod m) => a^(m-2) (mod m)
-ll mod_inverse(ll a, ll m) {
-    return mod_pow(a, m - 2, m);
-}
-
-
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-    ll a, b, c;
-
-    cin >> a >> b >> c;
-    ll d = (b*c%MOD - a*b%MOD - a*c%MOD + MOD + MOD) % MOD;
-    ll r = ((a*c%MOD - b*c%MOD+MOD) % MOD)*mod_inverse(d, MOD)%MOD;
-    ll x = ((a*b%MOD - b*c%MOD+MOD) % MOD)*mod_inverse(d, MOD)%MOD;
-
-    cout << r << " " << x << endl;
-
-    return 0;
+	return 0;
 }
