4929d90f2b8da191bd74143cbddb6ce1fca94c50
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-09 23:28:53 +0900

    solve 021_ABC_D.

--- atcoder/ABC/021_ABC_D.cpp
+++ atcoder/ABC/021_ABC_D.cpp
@@ -1,63 +1,84 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-#define MOD 1000000007LL
-typedef long long ll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/02/27  Problem: ABC021 D / Link: http://abc021.contest.atcoder.jp/tasks/abc021_d ----- */
-/* ------問題------
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-いろいろ考えてみたところ、このプログラムの出力する答えは 1≦a1≦a2≦…≦ak≦n であるような整数の組 (a1,a2,…,ak) の個数に等しいということに気づきました。
-しかし、彼はそのようなものの個数を数える方法を思いつきませんでした。
+/* -----  2019/04/09  Problem: ABC 021 D / Link: http://abc021.contest.atcoder.jp/tasks/abc021_d  ----- */
+// new:
+template <std::uint_least32_t MODULO> class modint {
+public:
+using uint32 = std::uint_least32_t; using uint64 = std::uint_least64_t; using iint64 = std::int_fast64_t; class optimize_tag_t {}; static constexpr optimize_tag_t optimize_tag{}; public:using value_type = uint32; value_type a; static constexpr value_type cst(iint64 x) noexcept { x %= static_cast<iint64>(MODULO); if (x < static_cast<iint64>(0)) { x += static_cast<iint64>(MODULO); }return static_cast<value_type>(x); }constexpr modint(optimize_tag_t, const value_type &x) noexcept : a(x) {}constexpr modint() noexcept : a(static_cast<value_type>(0)) {}constexpr modint(const iint64 &x) noexcept : a(cst(x)) {}constexpr modint operator+(const modint &o) const noexcept { return modint(optimize_tag, a + o.a < MODULO ? a + o.a : a + o.a - MODULO); }constexpr modint operator-(const modint &o) const noexcept { return modint(optimize_tag, a < o.a ? a + MODULO - o.a : a - o.a); }constexpr modint operator*(const modint &o) const noexcept { return modint(optimize_tag, static_cast<value_type>(static_cast<uint64>(a) * static_cast<uint64>(o.a) % static_cast<uint64>(MODULO))); }constexpr modint operator/(const modint &o) const { return modint(optimize_tag, static_cast<value_type>(static_cast<uint64>(a) * static_cast<uint64>((~o).a) % static_cast<uint64>(MODULO))); }modint &operator+=(const modint &o) noexcept { if ((a += o.a) >= MODULO)a -= MODULO; return *this; }modint &operator-=(const modint &o) noexcept { if (a < o.a)a += MODULO; a -= o.a; return *this; }modint &operator*=(const modint &o) noexcept { a = static_cast<value_type>(static_cast<uint64>(a) * static_cast<uint64>(o.a) % static_cast<uint64>(MODULO)); return *this; }modint &operator/=(const modint &o) { a = static_cast<uint64>(a) * (~o).a % MODULO; return *this; }constexpr modint inverse() const noexcept { assert(a != static_cast<value_type>(0) && "0 does not have inverse"); return pow(static_cast<uint64>(MODULO - static_cast<value_type>(2))); }constexpr modint operator~() const noexcept { return inverse(); }constexpr modint operator-() const noexcept { if (a == static_cast<value_type>(0)) { return modint(optimize_tag, static_cast<value_type>(0)); } else { return modint(optimize_tag, MODULO - a); } }modint &operator++() noexcept { if (++a == MODULO) { a = static_cast<value_type>(0); }return *this; }modint &operator--() noexcept { if (a == static_cast<value_type>(0)) { a = MODULO; }--a; return *this; }constexpr bool operator==(const modint &o) const noexcept { return a == o.a; }constexpr bool operator!=(const modint &o) const noexcept { return a != o.a; }constexpr bool operator<(const modint &o) const noexcept { return a < o.a; }constexpr bool operator<=(const modint &o) const noexcept { return a <= o.a; }constexpr bool operator>(const modint &o) const noexcept { return a > o.a; }constexpr bool operator>=(const modint &o) const noexcept { return a >= o.a; }constexpr explicit operator bool() const noexcept { return a; }constexpr explicit operator value_type() const noexcept { return a; }modint pow(iint64 inx) const noexcept { if (inx < 0)assert(a != static_cast<value_type>(0) && "not pow index < 0"); uint64 x = inx; uint64 t = a, u = 1; while (x) { if (x & 1)u = u * t % MODULO; t = (t * t) % MODULO; x >>= 1; }return modint(optimize_tag, static_cast<value_type>(u)); }
+constexpr value_type get() const noexcept { return a; }
+};
 
-彼の同僚であるあなたは、彼の代わりにこの課題をこなすプログラムを作ってあげることにしました。
-ただし、答えは非常に大きくなることがあるので、ans の代わりに ans を 1,000,000,007(=10^9+7) で割った余りを出力してください。
 
------問題ここまで----- */
-/* -----解説等-----
+using mint = modint<MOD>;
+const mint mintzero = mint(0); const mint mintone = mint(1);
+mint modinv(mint a) { return mintone / mint(a); }
 
-おなじみ逆元問題。
-最後桁あふれしやすいのでＭＯＤをよくとること。
+vector<mint> fact, inv_fact;
 
- ----解説ここまで---- */
-
-ll N, K;
-
-ll ans = 0LL;
-
-ll mod_pow(ll x, ll p, ll mod) {
-    ll a = 1;
-    while (p) {
-        if (p & 1) a = a * x % mod;
-        x = x * x % mod;
-        p >>= 1;
-    }
-    return a;
+void init_fact(int n) {
+	fact.resize(n);
+	fact[0] = mintone;
+	for (int i = 1; i < n; i++) {
+		fact[i] = fact[i - 1] * mint(i);
+	}
+	inv_fact.resize(n);
+	inv_fact[n - 1] = modinv(fact[n - 1]);
+	for (int i = n - 2; i >= 0; i--) {
+		inv_fact[i] = mint(i + 1) * inv_fact[i + 1];
+	}
 }
-ll mod_inverse(ll a, ll m) {
-    return mod_pow(a, m - 2, m);
+mint nCr(int n, int r) {
+	if (n < r || n < 0 || r < 0) return mintzero;
+	return fact[n] * inv_fact[r] * inv_fact[n - r];
 }
 
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
+mint nPr(int n, int r) {
+	if (n < r || n < 0 || r < 0) return mintzero;
+	return fact[n] * inv_fact[n - r];
+}
 
-    cin >> N >> K;
-    ll x = N - 1 + K;
-    ll y = K;
-    ll fact[220000];
+mint nHr(int n, int r) {
+	if (n < 0 || r < 0) return mintzero;
+	if (n == 0 && r == 0) return mintone;
+	return nCr(n + r - 1, r);
+}
 
-    fact[0] = 1;
 
-    FOR(i, 1, x + 1) {
-        fact[i] = (i*fact[i - 1]) % MOD;
-    }
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
-    ans = ((fact[x] * mod_inverse(fact[y], MOD)) % MOD)*mod_inverse(fact[x - y], MOD) % MOD;
+	init_fact(3e5 + 5);
 
-    cout << ans << endl;
+	LL N, K; cin >> N >> K;
+	LL ans = nHr(N, K).get();
 
-    return 0;
+
+	cout << ans << "\n";
+
+	return 0;
 }
