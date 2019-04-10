634b1d874251f6f76d4cb24121e81bc3cfb84c3f
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-10 22:25:21 +0900

    solve 017_ABC_D.

--- atcoder/ABC/017_ABC_D.cpp
+++ atcoder/ABC/017_ABC_D.cpp
@@ -1,68 +1,67 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-#define MOD 1000000007
-typedef long long ll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/03/01  Problem: ABC017 D / Link: http://abc017.contest.atcoder.jp/tasks/abc017_4 ----- */
-/* ------問題------
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 
-健康志向の高橋君は通販で購入したサプリメントを摂取することにした。
-サプリメントは N 個あり、1 から N まで番号が付けられている。
-また、サプリメントの味は M 種類あり、1 から M まで番号が付けられている。サプリメント i(1≦i≦N) の味は fi(1≦fi≦M) である。
-高橋君はサプリメントを番号順に複数日かけて摂取する予定である。
-高橋君はサボらないように、サプリメントが 1 つ以上残っている場合はその日中に必ず 1 つ以上サプリメントを摂取しなければならないという規則を定めた。
-高橋君は強靭な肉体を持っているため、1 日にどれだけサプリメントを摂取しても大丈夫だが、
-同じ味には飽きてしまうので、同じ日に同じ味のサプリメントを 2 つ以上摂取することはできない。
-高橋君は、サプリメントの摂取方法の是非について吟味するため、このような条件下で全部で何通りの摂取方法があるかを知りたい。
-ここで 2 つの摂取方法についてそれらが違うというのは、ある日について摂取したサプリメントの番号の組み合わせが異なることを定義する。
+/* -----  2019/04/10  Problem: ABC 017 D / Link: http://abc017.contest.atcoder.jp/tasks/abc017_d  ----- */
 
------問題ここまで----- */
-/* -----解説等-----
+template <std::uint_least32_t MODULO> class modint {
+public:
+using uint32 = std::uint_least32_t; using uint64 = std::uint_least64_t; using iint64 = std::int_fast64_t; class optimize_tag_t {}; static constexpr optimize_tag_t optimize_tag{}; public:using value_type = uint32; value_type a; static constexpr value_type cst(iint64 x) noexcept { x %= static_cast<iint64>(MODULO); if (x < static_cast<iint64>(0)) { x += static_cast<iint64>(MODULO); }return static_cast<value_type>(x); }constexpr modint(optimize_tag_t, const value_type &x) noexcept : a(x) {}constexpr modint() noexcept : a(static_cast<value_type>(0)) {}constexpr modint(const iint64 &x) noexcept : a(cst(x)) {}constexpr modint operator+(const modint &o) const noexcept { return modint(optimize_tag, a + o.a < MODULO ? a + o.a : a + o.a - MODULO); }constexpr modint operator-(const modint &o) const noexcept { return modint(optimize_tag, a < o.a ? a + MODULO - o.a : a - o.a); }constexpr modint operator*(const modint &o) const noexcept { return modint(optimize_tag, static_cast<value_type>(static_cast<uint64>(a) * static_cast<uint64>(o.a) % static_cast<uint64>(MODULO))); }constexpr modint operator/(const modint &o) const { return modint(optimize_tag, static_cast<value_type>(static_cast<uint64>(a) * static_cast<uint64>((~o).a) % static_cast<uint64>(MODULO))); }modint &operator+=(const modint &o) noexcept { if ((a += o.a) >= MODULO)a -= MODULO; return *this; }modint &operator-=(const modint &o) noexcept { if (a < o.a)a += MODULO; a -= o.a; return *this; }modint &operator*=(const modint &o) noexcept { a = static_cast<value_type>(static_cast<uint64>(a) * static_cast<uint64>(o.a) % static_cast<uint64>(MODULO)); return *this; }modint &operator/=(const modint &o) { a = static_cast<uint64>(a) * (~o).a % MODULO; return *this; }constexpr modint inverse() const noexcept { assert(a != static_cast<value_type>(0) && "0 does not have inverse"); return pow(static_cast<uint64>(MODULO - static_cast<value_type>(2))); }constexpr modint operator~() const noexcept { return inverse(); }constexpr modint operator-() const noexcept { if (a == static_cast<value_type>(0)) { return modint(optimize_tag, static_cast<value_type>(0)); } else { return modint(optimize_tag, MODULO - a); } }modint &operator++() noexcept { if (++a == MODULO) { a = static_cast<value_type>(0); }return *this; }modint &operator--() noexcept { if (a == static_cast<value_type>(0)) { a = MODULO; }--a; return *this; }constexpr bool operator==(const modint &o) const noexcept { return a == o.a; }constexpr bool operator!=(const modint &o) const noexcept { return a != o.a; }constexpr bool operator<(const modint &o) const noexcept { return a < o.a; }constexpr bool operator<=(const modint &o) const noexcept { return a <= o.a; }constexpr bool operator>(const modint &o) const noexcept { return a > o.a; }constexpr bool operator>=(const modint &o) const noexcept { return a >= o.a; }constexpr explicit operator bool() const noexcept { return a; }constexpr explicit operator value_type() const noexcept { return a; }modint pow(iint64 inx) const noexcept { if (inx < 0)assert(a != static_cast<value_type>(0) && "not pow index < 0"); uint64 x = inx; uint64 t = a, u = 1; while (x) { if (x & 1)u = u * t % MODULO; t = (t * t) % MODULO; x >>= 1; }return modint(optimize_tag, static_cast<value_type>(u)); }
+constexpr value_type get() const noexcept { return a; }
+};
+using mint = modint<MOD>;
 
-日にちでＤＰかと思ったけど実装がよく分からなかった。
-種類でＤＰだった。
-dp[ i日目 ] := 総和
-とある種類のサプリメントが以前に登場していたら、そこまでのものを差し引く。
-そうすることで同じ日に食べるものと食べないものを同時に足すことができる。
-これtdpc の駅に似ている。典型かよ。
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
- ----解説ここまで---- */
+	LL N, M; cin >> N >> M;
+	vector<LL> a(N);
+	for (int i = 0; i < N; ++i) {
+		cin >> a[i];
+		a[i]--;
+	}
+	VI typecnt(M, 0);
+	//区間の要素がuniqueになる分け方
+	vector<mint>dp(N);
+	mint sum = 1;
+	dp[0] = 1;
+	for (int L = 0, R = 0; R < N; R++) {
+		typecnt[a[R]]++;
+		while (typecnt[a[R]] > 1) {
+			sum -= dp[L];
+			typecnt[a[L]]--;
+			L++;
+		}
+		dp[R + 1] = sum;
+		sum += dp[R + 1];
+	}
 
-ll N, M;
-int f[100010];
-ll dp[100010];
-int num[100010];
-ll ans = 0LL;
+	LL ans = dp[N].get();
 
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
 
-    cin >> N >> M;
-    FOR(i, 0, N) {
-        cin >> f[i];
-    }
-    ll sum = 1;
-    dp[0] = 1;
-    int index = 0;
+	cout << (ans) << "\n";
 
-    FOR(i, 0, N) {
-        num[f[i]]++;
-        while (num[f[i]] > 1) {
-            num[f[index]]--;
-            sum = (sum + MOD - dp[index]) % MOD;
-            index++;
-        }
-        dp[i + 1] = sum;
-        sum = (sum + dp[i + 1]) % MOD;
-    }
-
-    ans = dp[N];
-
-    cout << ans << endl;
-
-    return 0;
+	return 0;
 }
