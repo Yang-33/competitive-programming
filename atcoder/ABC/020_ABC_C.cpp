97b970ba3d6ead211ddf20592d646f636b9ad793
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-10 22:25:24 +0900

    solve 020_ABC_C.

--- atcoder/ABC/020_ABC_C.cpp
+++ atcoder/ABC/020_ABC_C.cpp
@@ -1,101 +1,90 @@
-#include<iostream>
-#include<queue>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-const long long int INF = 1e12;
-typedef long long ll;
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/02/27  Problem: ABC020 C / Link: http://abc020.contest.atcoder.jp/tasks/abc020_c ----- */
-/* ------問題------
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 
-正方形のマスが縦 H 行、横 W 列に並んでいます。
-各マスは白または黒で塗られており、白マスのうち 2 つがそれぞれスタート地点とゴール地点として指定されています。
-高橋君はスタート地点から出発して T 秒以内にゴール地点に到着したいです。
-高橋君は、各マスから上下左右に隣接する別のマスに移動することができます。
-このとき、移動先が白マスであれば 1 秒、黒マスであれば x 秒の時間がかかります（移動前のマスの色は移動時間に影響しません）。
-ここで、 x の値は高橋君がスタート地点から出発する前にあなたに選んでもらいます。
-x の値は正整数でなければならず、高橋君の出発後に値を変更することはできません。
-高橋君が T 秒以内にゴール地点に到着することが可能であるような正整数 x の最大値を求めてください。
+/* -----  2019/04/10  Problem: ABC 020 C / Link: http://abc020.contest.atcoder.jp/tasks/abc020_c  ----- */
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+inline bool IN(int y, int x, int H, int W) { return (0 <= y && y < H && 0 <= x && x < W); };
 
------問題ここまで----- */
-/* -----解説等-----
+LL bfs2D(pair<int, int> s, pair<int, int> t, int Height, int Width, vector<string > & masu, LL X) {
+	vector<vector<LL> > d(Height, vector<LL>(Width, LINF));
 
-純粋な探索では計算量がＯ(HWT)となり答えを求められない。
-境界を求めるので二分探索で解けそう。
-ならＯＫ.半開区間[l,r)で探索するので答えはl
-INFの値設定で1WA。気を付けたい
+	d[s.first][s.second] = 0;
+	queue<pair<LL, pair<int, int>>>q;
+	q.push(make_pair(0, s));
 
- ----解説ここまで---- */
+	while (!q.empty()) {
+		pair<LL, pair<int, int>>a = q.front(); q.pop();
+		LL dist = a.first; pair<int, int> pos = a.second;
+		if (d[pos.first][pos.second] < dist)continue;
+		FOR(k, 0, 4) {
+			int ny = pos.first + DY[k]; int nx = pos.second + DX[k];
+			if (IN(ny, nx, Height, Width)) {
+				LL c = (masu[ny][nx] == '#' ? X : 1);
+				if (d[ny][nx] > d[pos.first][pos.second] + c) {
+					d[ny][nx] = d[pos.first][pos.second] + c;
+					q.push(make_pair(d[ny][nx], pair<int, int>(ny, nx)));
+				}
+			}
+		}
+	}
 
-ll H, W, T;
-char m[10][10];
-ll sx, sy, gx, gy;
-ll ans = 0LL;
-
-bool f(ll a) {
-    ll d[10][10];
-    
-    FOR(i, 0, 10)
-        FOR(j, 0, 10)
-        d[i][j] = INF;
-
-    d[sy][sx] = 0;
-
-    queue<pair<int, int>>Q;
-    Q.push(make_pair(sy, sx));
-    while (!Q.empty()) {
-        auto p = Q.front();
-        int y = p.first; int x = p.second;
-        Q.pop();
-        FOR(i, 0, 4) {
-            int yy = y + dy[i]; int xx = x + dx[i];
-            if (!(0 <= xx&&xx < W && 0 <= yy&&yy < H))continue;
-
-            if (m[yy][xx] == '#') {
-                if (d[yy][xx] <= d[y][x] + a) continue;
-                d[yy][xx] = d[y][x] + a;
-            }
-            else {
-                if (d[yy][xx] <= d[y][x] + 1)continue;
-                d[yy][xx] = d[y][x] + 1;
-            }
-            Q.push(make_pair(yy, xx));
-        }
-    }
-    return d[gy][gx] <= T;
+	return d[t.first][t.second]; // (int)
 }
 
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
-    cin >> H >> W >> T;
-    FOR(i, 0, H)
-        FOR(j, 0, W) {
-        cin >> m[i][j];
-        if (m[i][j] == 'S') {
-            sx = j; sy = i;
-        }
-        else if (m[i][j] == 'G') {
-            gx = j; gy = i;
-        }
-    }
+	LL H, W, T; cin >> H >> W >> T;
+	vector<string > s(H);
+	for (int i = 0; i < H; ++i) {
+		cin >> s[i];
+	}
+	PII ss, st;
+	FOR(i, 0, H) {
+		FOR(j, 0, W) {
+			if (s[i][j] == 'S')ss = PII(i, j);
+			if (s[i][j] == 'G')st = PII(i, j);
+		}
+	}
+	LL L = 0, R = 1e12;
+	while (R - L > 1) {
+		LL mid = (R + L) / 2;
+		if (bfs2D(ss, st, H, W, s, mid) <= T) {
+			L = mid;
+		}
+		else {
+			R = mid;
+		}
+	}
+	assert(L > 0);
 
-    ll l = 1, r = 1e9 + 1;
-    ll mid;
-    while (r - l > 1) {
-        mid = (l + r) / 2;
-        if (f(mid))
-            l = mid;
-        else r = mid;
-    }
+	LL ans = L;
 
-    ans = l;
-    cout << ans << endl;
 
-    return 0;
+	cout << (ans) << "\n";
+
+	return 0;
 }
