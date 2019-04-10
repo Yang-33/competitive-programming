19efb7558ca0bf7504fdeefbec7a5148db5edf99
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-10 22:25:24 +0900

    solve 020_ABC_D.

--- atcoder/ABC/020_ABC_D.cpp
+++ atcoder/ABC/020_ABC_D.cpp
@@ -1,81 +1,103 @@
-#include<iostream>
-#include<cstdio>
-#include<algorithm>
-#include<cmath>
-#include<string>
-#include<cstring>
-#include<vector>
-#include<map>
-#include<list>
-#include<stack>
-#include<queue>
-#include<climits> //INT_MIN/MAX
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-#define FORR(i,s,e) for(ll (i)=(s);(i)>(e);(i)--)
-#define MOD 1000000007
-#define debug(x) cout<<#x<<": "<<x<<endl
-const int INF = 1e9;
-typedef long long ll;
-int dx[8] = { 1,1,1,0,0,-1,-1,-1 };
-int dy[8] = { 1,0,-1,1,-1,1,0,-1 };
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/02/27  Problem: ABC020 D / Link: http://abc020.contest.atcoder.jp/tasks/abc020_d ----- */
-/* ------問題------
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 
+/* -----  2019/04/10  Problem: ABC 020 D / Link: http://abc020.contest.atcoder.jp/tasks/abc020_d  ----- */
 
------問題ここまで----- */
-/* -----解説等-----
-
-100点解法
-lcm(a,b)=a*b/gcd(a,b)
-gcd(a,b)=gcd(a%b,b)を利用する
-するとΣ(n->N) lcm(n,K) はKの余剰で考えることができる
-K種の値についてlcm(x%K,K)=gc,(x%K,K)*x*K,
-about N/Kぐらいの項をgcm(x%K,K)でまとめることができるので
-公差Kの等差数列の和として考えることができる -> О(1)
-О(K log K)で計算可能
-
-101点解法
-gcd(i,k)はk種類しか存在しない。
-ｋの約数を求め、各約数についてのlcmを求めればよい。その際重複して数え上げないように注意する。
-http://abc020.contest.atcoder.jp/submissions/663276
-
-----解説ここまで---- */
-
-ll N, K;
-ll ans = 0LL;
-
-ll gcd(ll a, ll b) {
-    if (a < b) swap(a, b);
-    ll r = a%b;
-    while (r > 0) {
-        a = b;
-        b = r;
-        r = a%b;
-    }
-    return b;
+map<LL, LL> make_factor(long long  n) {
+	map<LL, LL>ans;
+	for (long long i = 2; i * i <= n; i++) {
+		while (n%i == 0) {
+			ans[i]++;
+			n /= i;
+		}
+	}
+	if (n != 1)ans[n]++;
+	return ans;
 }
 
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
+vector<LL> getdivisor(LL n) {
+	vector<LL> res;
+	for (LL i = 1; i*i <= n; i++) {
+		if (n%i == 0) {
+			res.push_back(i);
+			if (n / i != i)res.push_back(n / i);
+		}
+	}
+	sort(res.begin(), res.end());
+	return res;
+}
 
-    cin >> N >> K;
-    if (K > 100)return 0;
 
-    FOR(i, 1, K + 1) {
-        if (i > N)
-            break;
-        ll n = N / K;
-        if (N%K >= i)
-            n++;
-        ans += n*(2 * i + (n - 1)*K) / 2 / gcd(i, K)*K;
-        ans %= MOD;
-    }
-    cout << ans << endl;
 
-    return 0;
+template <std::uint_least32_t MODULO> class modint {
+public:
+using uint32 = std::uint_least32_t; using uint64 = std::uint_least64_t; using iint64 = std::int_fast64_t; class optimize_tag_t {}; static constexpr optimize_tag_t optimize_tag{}; public:using value_type = uint32; value_type a; static constexpr value_type cst(iint64 x) noexcept { x %= static_cast<iint64>(MODULO); if (x < static_cast<iint64>(0)) { x += static_cast<iint64>(MODULO); }return static_cast<value_type>(x); }constexpr modint(optimize_tag_t, const value_type &x) noexcept : a(x) {}constexpr modint() noexcept : a(static_cast<value_type>(0)) {}constexpr modint(const iint64 &x) noexcept : a(cst(x)) {}constexpr modint operator+(const modint &o) const noexcept { return modint(optimize_tag, a + o.a < MODULO ? a + o.a : a + o.a - MODULO); }constexpr modint operator-(const modint &o) const noexcept { return modint(optimize_tag, a < o.a ? a + MODULO - o.a : a - o.a); }constexpr modint operator*(const modint &o) const noexcept { return modint(optimize_tag, static_cast<value_type>(static_cast<uint64>(a) * static_cast<uint64>(o.a) % static_cast<uint64>(MODULO))); }constexpr modint operator/(const modint &o) const { return modint(optimize_tag, static_cast<value_type>(static_cast<uint64>(a) * static_cast<uint64>((~o).a) % static_cast<uint64>(MODULO))); }modint &operator+=(const modint &o) noexcept { if ((a += o.a) >= MODULO)a -= MODULO; return *this; }modint &operator-=(const modint &o) noexcept { if (a < o.a)a += MODULO; a -= o.a; return *this; }modint &operator*=(const modint &o) noexcept { a = static_cast<value_type>(static_cast<uint64>(a) * static_cast<uint64>(o.a) % static_cast<uint64>(MODULO)); return *this; }modint &operator/=(const modint &o) { a = static_cast<uint64>(a) * (~o).a % MODULO; return *this; }constexpr modint inverse() const noexcept { assert(a != static_cast<value_type>(0) && "0 does not have inverse"); return pow(static_cast<uint64>(MODULO - static_cast<value_type>(2))); }constexpr modint operator~() const noexcept { return inverse(); }constexpr modint operator-() const noexcept { if (a == static_cast<value_type>(0)) { return modint(optimize_tag, static_cast<value_type>(0)); } else { return modint(optimize_tag, MODULO - a); } }modint &operator++() noexcept { if (++a == MODULO) { a = static_cast<value_type>(0); }return *this; }modint &operator--() noexcept { if (a == static_cast<value_type>(0)) { a = MODULO; }--a; return *this; }constexpr bool operator==(const modint &o) const noexcept { return a == o.a; }constexpr bool operator!=(const modint &o) const noexcept { return a != o.a; }constexpr bool operator<(const modint &o) const noexcept { return a < o.a; }constexpr bool operator<=(const modint &o) const noexcept { return a <= o.a; }constexpr bool operator>(const modint &o) const noexcept { return a > o.a; }constexpr bool operator>=(const modint &o) const noexcept { return a >= o.a; }constexpr explicit operator bool() const noexcept { return a; }constexpr explicit operator value_type() const noexcept { return a; }modint pow(iint64 inx) const noexcept { if (inx < 0)assert(a != static_cast<value_type>(0) && "not pow index < 0"); uint64 x = inx; uint64 t = a, u = 1; while (x) { if (x & 1)u = u * t % MODULO; t = (t * t) % MODULO; x >>= 1; }return modint(optimize_tag, static_cast<value_type>(u)); }
+constexpr value_type get() const noexcept { return a; }
+};
+using mint = modint<MOD>;
+
+// \sum ^N_{i,gcd(i,K)=1} i
+mint GCDoneSUM(LL N, LL K) {
+	VI factorsvi; {
+		auto factors = make_factor(K);
+		for (auto it : factors) {
+			factorsvi.push_back(it.first);
+		}
+	}
+	mint ret = 0;
+	FOR(state, 0, 1 << SZ(factorsvi)) {
+		LL prod = 1;
+		FOR(i, 0, SZ(factorsvi)) {
+			if (state & 1 << i) {
+				prod *= factorsvi[i];
+			}
+		}
+		mint cnt = N / prod;
+		mint subsum = cnt * (cnt + 1) / mint(2)*prod;
+		mint exin = (__builtin_parity(state) ? -1 : +1);
+		ret += exin * subsum;
+	}
+
+	return ret;
+}
+
+
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL N, K; cin >> N >> K;
+
+	auto divsK = getdivisor(K);
+	mint ans = 0;
+	for (auto divK : divsK) {
+		ans += GCDoneSUM(N / divK, K / divK);
+	}
+	ans *= K;
+	cout << (ans).get() << "\n";
+
+	return 0;
 }
