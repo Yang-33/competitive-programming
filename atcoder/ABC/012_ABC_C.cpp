3775a084f380d40f084a10d51bad67d13340abea
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-26 22:25:18 +0900

    solve 012_ABC_C.

--- atcoder/ABC/012_ABC_C.cpp
+++ atcoder/ABC/012_ABC_C.cpp
@@ -1,43 +1,59 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-typedef long long ll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/03/02  Problem: ABC012 C / Link: https://abc012.contest.atcoder.jp/tasks/abc012_3 ----- */
-/* ------問題------
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-高橋君は、先日、九九の表を全て暗記することに成功しました。
-九九で物足りなくなった高橋君は、 1 × 1 から 9 × 9 までの値を、全て足してみることにしました。
-高橋君は、九九のうちの一つの答えを忘れてしまったので、その計算を後回しにして、残りの和を求めたところ、N になりました。
-しかし、高橋君は、後回しにした問題がどれだか解らなくなってしまいました。
-高橋君が後回しにしてしまった問題として、あり得るものを辞書順で全て出力しなさい。
+/* -----  2019/04/11  Problem: ABC 012 C / Link: http://abc012.contest.atcoder.jp/tasks/abc012_c  ----- */
 
------問題ここまで----- */
-/* -----解説等-----
+vector<LL> getdivisor(LL n) {
+	vector<LL> res;
+	for (LL i = 1; i*i <= n; i++) {
+		if (n%i == 0) {
+			res.push_back(i);
+			if (n / i != i)res.push_back(n / i);
+		}
+	}
+	sort(res.begin(), res.end());
+	return res;
+}
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
-整数問題を制約の中で全探索すれば通る。
-Ｎの制約から簡単に解けそうだったので書いたら通った。
-可換なものかつ制約の整数内なものは制約上の整数をひとつづつ進めればよい。
+	LL sum = 0;
+	FOR(i, 1, 10) {
+		FOR(j, 1, 10) {
+			sum += i * j;
+		}
+	}
+	LL N; cin >> N;
+	sum -= N;
+	for (LL i = 1; i <= min(sum, 9LL); i++) {
+		if (sum / i <= 9 && sum%i == 0) {
+			cout << i << " x " << sum / i << endl;
+		}
+	}
 
- ----解説ここまで---- */
-
-ll N;
-ll ans = 0LL;
-
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-
-    cin >> N;
-    int left = 2025 - N;
-
-    FOR(i, 1, 10) {
-        int a = left / i;
-        if (!(a*i == left && 1 <= a&&a <= 9))continue;
-        cout << i << " x " << a << endl;
-    }
-
-    return 0;
+	return 0;
 }
