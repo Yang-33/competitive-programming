acee13c35cfb8b92f8041f3b0f58691cc0194351
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-26 22:25:19 +0900

    solve 013_ABC_C.

--- atcoder/ABC/013_ABC_C.cpp
+++ atcoder/ABC/013_ABC_C.cpp
@@ -1,65 +1,65 @@
-#include<iostream>
-#include<algorithm>
-#include<climits> //INT_MIN/MAX
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-typedef long long ll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/03/01  Problem: ABC013 C / Link: http://abc013.contest.atcoder.jp/tasks/abc013_3 ----- */
-/* ------問題------
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-セキュリティ意識が高く、最新鋭の錠を購入した高橋君ですが、財布の管理は甘かったらしくお金がピンチな状況です。
-高橋君の収入は安定せず、次の収入があるのは今から N 日後です。高橋君は N 日間、できるだけ食費を抑えて節約生活を送ることにしました。
-はじめ、高橋君の満腹度は H です。N 日間のそれぞれの日について、その日にとる食事を次の 3 種類の中から 1 つ選びます。
-普通の食事: A 円の出費をして、満腹度が B 増える。
-質素な食事: C 円の出費をして、満腹度が D 増える。（ただし、C<A かつ D<B）
-食事抜き: 出費なしで、満腹度が E 減る。
-厳しく節約すれば出費を抑えることができますが、あまりに節約しすぎて体調を崩してしまってはいけないため、
-N 日間で一度も満腹度が 0 以下にならないようにしなければなりません。
-高橋君は最低何円の食費で N 日間を乗り切ることができるでしょうか？
-ただし、高橋君は超人級の胃袋を持っており、その満腹度には上限がないとする。すなわち、いくら食事をしても高橋君の満腹度が頭打ちになることはない。
+/* -----  2019/04/11  Problem: ABC 013 C / Link: http://abc013.contest.atcoder.jp/tasks/abc013_c  ----- */
 
------問題ここまで----- */
-/* -----解説等-----
 
-最小の費用を決定する際に知りたいのは普通の食事と質素な食事をどれだけ食べるかであるが、
-片側を決めてしまえばもう片側も選択されうる。
-※( H + Bx + Dy - ( N - x - y  )*E ) > 0 となる x に対して最小の y は決定されうるため
-片側を全探索していけばよい。
-線形で解け！！！！！！！！！！！！！！！！！！！！！
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
- ----解説ここまで---- */
+	// 普通の食事の回数でやる
 
-ll N, H, a, b, c, d, e;
+	LL N, H; cin >> N >> H;
+	LL A, B, C, D, E; cin >> A >> B >> C >> D >> E;
+	LL ans = LINF;
+	FOR(hutu, 0, N + 1) {
+		LL cost = A * hutu;
+		LL new_H = H + B * hutu;
+		auto f = [&](int p, LL leftH) {
+			// p日質素をする
+			leftH += p * D;
+			int sis_day = N - hutu - p;
+			leftH -= sis_day * E;
+			return leftH > 0;
+		};
+		LL L = -1, R = N;
+		while (R - L > 1) {
+			LL mid = (L + R) / 2;
+			if (f(mid, new_H)) {
+				R = mid;
+			}
+			else L = mid;
+		}
+		// R is ok
+		cost += R * C;
+		ans = min(ans, cost);
+	}
 
-ll ans = 0LL;
+	cout << ans << "\n";
 
-bool check(ll x, ll y) {
-    return H + b*x + d*y - (N - x - y)*e > 0;
-}
-
-ll fy(ll x) {
-    ll rec = (double)((N - x)*e - H - b*x) / (double)(d + e);
-    rec++;
-    if ((N - x)*e - H - b*x < 0) rec = 0;
-    return rec;
-}
-
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-
-    cin >> N >> H >> a >> b >> c >> d >> e;
-
-    ans = LLONG_MAX / 10;
-    FOR(i, 0, N + 1) {
-        if(check(i,fy(i)))
-        ans = min(ans, a*i + c*fy(i));
-    }
-
-    cout << ans << endl;
-
-    return 0;
+	return 0;
 }
