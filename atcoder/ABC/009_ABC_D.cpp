d390a690c60d13e7c74ee40cfbd39d0884af7246
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-26 22:25:15 +0900

    solve 009_ABC_D.

--- atcoder/ABC/009_ABC_D.cpp
+++ atcoder/ABC/009_ABC_D.cpp
@@ -1,119 +1,55 @@
-#include<iostream>
-#include<vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-typedef long long ll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/03/02  Problem: ABC009 D / Link: https://abc009.contest.atcoder.jp/tasks/abc009_4 ----- */
-/* ------問題------
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 
-数列 A はすべての要素が 32 ビットの符号なし整数で表現でき、その値は次のようにして決まる。
-はじめの K 項 A1,?A2,?…,?AK は入力で与えられる。
-A とは別に K 項の数列 C1,?C2,?…,?CK （こちらもすべての要素が 32 ビットの符号なし整数におさまる）が入力で与えられ、
-K+1 項目以降の A の値はこの C を用いて次のように計算される。
-N≧1 に対し AN+K=(C1?AND?AN+K?1)?XOR?(C2?AND?AN+K?2)?XOR?…? XOR?(CK?AND?AN)
-ただし AND はビットごとの論理積、 XOR はビットごとの排他的論理和を表す。
-この数列 A の M 番目の値 AM を求めるプログラムを作成せよ。
+/* -----  __MAKE_TIME__  Problem: __PROBLEM__ / Link: __CONTEST_URL__  ----- */
 
------問題ここまで----- */
-/* -----解説等-----
+template<typename SemiRing>struct Kitamasa { using T = typename SemiRing::t1; using VT = vector<T>; VT c; vector<VT> rs; int m;	Kitamasa(const VT &C) :c(C), rs(1), m(c.size()) { rs[0].assign(2 * m + 1, SemiRing::id1()); rs[0][1] = SemiRing::id2(); }VT merge(const VT &x, const VT &y) { VT z(2 * m + 1, SemiRing::id1()); for (int i = 1; i <= m; i++)for (int j = 1; j <= m; j++)z[i + j] = SemiRing::add(z[i + j], SemiRing::mul(x[i], y[j])); for (int i = 2 * m; i > m; z[i--] = SemiRing::id1())for (int j = 1; j <= m; j++)z[i - j] = SemiRing::add(z[i - j], SemiRing::mul(c[m - j], z[i])); return z; }T calc(const VT &A, long long n) { assert((int)A.size() == m); VT res(rs[0]); for (int i = 0; n; i++, n >>= 1) { if (i >= (int)rs.size())rs.emplace_back(merge(rs[i - 1], rs[i - 1])); if (~n & 1) continue; res = merge(res, rs[i]); }T ans = SemiRing::id1(); for (int i = 1; i <= m; i++) ans = SemiRing::add(ans, SemiRing::mul(res[i], A[i - 1])); return ans; } };
 
-線形漸化式は行列の累乗を用いて解ける(プログラミングコンテストチャレンジブック pp.180-185)。
-ただし今回は×がandに、+がxorになっている。
-しかしこれは、行列累乗の際の計算で、×, +をand, xorにそれぞれ置き換えて計算すれば良い。
-実際には、2つの二項演算(和と積)を備えた集合が、半環であればよい。(半環の性質は解説p.97に載っている)
-http://www.slideshare.net/chokudai/abc009/97
+int main() {
+	struct A {
+		using t1 = unsigned int;
+		static const t1 id1() { return 0; }
+		static const t1 id2() { return UINT32_MAX; }
+		static t1 add(const t1& l, const t1& r) {
+			return l ^ r;
+		}
+		static t1 mul(const t1& l, const t1& r) {
+			return l & r;
+		}
+	};
+	int K, M; cin >> K >> M;
+	vector<A::t1>a(K), c(K);
+	FOR(i, 0, K) {
+		cin >> a[i];
+	}
+	FOR(i, 0, K) {
+		cin >> c[i];
+	}
+	reverse(ALL(c));
+	Kitamasa<A> mats(c);
+	cout << mats.calc(a, --M) << endl;
 
-この性質を実際に確かめる(1bitごとに全ての場合を計算すればよい)と、
-xor: 結合法則、交換法則、単位元(0)を持つ
-and: 結合法則、単位元(全てのbitが1、ここでは(1<<32)-1)を持つ
-andとxorで分配法則が成立
-xorの単位元(0)はandの零元
-が全て成立するので、and, xorは非負整数の集合で半環をなす。
-
-後は計算するだけ。
-
-まだきびしい。むずかしいわ。
-
- ----解説ここまで---- */
-
-ll N;
-
-ll ans = 0LL;
-typedef vector<vector<ll>> mat;
-
-mat mul_bit(mat &A, mat &B) {
-    mat C(A.size(), vector<ll>(B[0].size()));
-    FOR(i,0, A.size())FOR(k,0, B.size())FOR(j,0, B[0].size()) {
-        C[i][j] = (C[i][j] ^ (A[i][k] & B[k][j]));
-    }
-    return C;
-}
-
-mat pow(mat A, ll n) {
-    mat B(A.size(), vector<ll>(A.size()));
-    FOR(i,0, A.size()) {
-        B[i][i] = -1;
-    }
-    while (n > 0) {
-        if (n & 1) B = mul_bit(B, A);
-        A = mul_bit(A, A);
-        n >>= 1;
-    }
-    return B;
-}
-
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-
-    ll a[101], c[101];
-    ll k, m;
-    cin >> k >> m;
-
-    mat A;
-    FOR(i,0, k) {
-        cin >> a[i];
-    }
-    if (k >= m) {
-        cout << a[m - 1] << endl;
-        return 0;
-    }
-    reverse(a, a + k);
-    FOR(i,0, k) {
-        A.push_back(vector<ll>(1, a[i]));
-    }
-    /*
-    a0
-    a1
-    a2
-    ...
-
-    */
-    mat C;
-    FOR(i,0, k) {
-        cin >> c[i];
-    }
-    C.push_back(vector<ll>(c, c + k));
-    FOR(i,0, k - 1) {
-        vector<ll> t(k, 0);
-        t[i] = -1;
-        C.push_back(t);
-    }
-    /*
-    c0 c1 c2 ... cn-1 cn
-    1  0  0 ...  0    0
-    0  1  0 ...  0    0
-    0  0  1 ...  1    0
-
-    */
-
-    auto mC = pow(C, m - k);
-    auto ans = mul_bit(mC, A);
-    cout << ans[0][0] << endl;
-
-
-    return 0;
+	return 0;
 }
