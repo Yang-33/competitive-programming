ce7ef2583baccb34a243bac7d664c1e46d961803
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-26 22:25:19 +0900

    solve 013_ABC_D.

--- atcoder/ABC/013_ABC_D.cpp
+++ atcoder/ABC/013_ABC_D.cpp
@@ -1,75 +1,69 @@
-#include<iostream>
-#include<algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-#define FORR(i,s,e) for(ll (i)=(s);(i)>(e);(i)--)
-typedef long long ll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/03/01  Problem: ABC013 D / Link: http://abc013.contest.atcoder.jp/tasks/abc013_4 ----- */
-/* ------問題------
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-N本の縦線とM本の横線で構成されたあみだくじがある。
-(M本の横線の位置の情報が与えられる。)
-このくじをD個縦につなげたとき、
-左からi番目(1<=i<=Nを満たすi全て)の縦線を選ぶと
-最終的にどこにたどり着くかそれぞれ答えなさい。(長いので要約)
-
------問題ここまで----- */
-/* -----解説等-----
-
-クエリの高速処理なら014とおんなじやん!?とはなったけど
-ダブリングをうまく使いこなせておらず正解ならず。
-しかしダブリングであった。遷移するタイプはダブリングで前処理しておけば便利な場合が多いのかも。
-2^kのテーブルを作れば遷移がlogで抑えられる。実質増加二分探索。
-
-移動先をもとめるときに、横棒は逆順から適用していかないとダメ
-普通の順番で適用すると、「下側のi番目に上側の何番目が入るか」を求めてしまう
-求めるべきは「上側のi番目が下側の何番目に入るか」である (1WA)
-
- ----解説ここまで---- */
-
-ll N, M, D;
-ll ans = 0LL;
-int c[200010];
-ll doub[32][100010];
-
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-
-    cin >> N >> M >> D;
-    FOR(i, 0, N) {
-        doub[0][i] = i;
-    }
-
-    FOR(i, 0, M) {
-        cin >> c[i];
-    }
-
-    FORR(i, M - 1, -1) {
-        swap(doub[0][c[i]], doub[0][c[i] - 1]);
-    }//あみだ
-
-    FOR(k, 0, 30) { //2^kテーブル作成
-        FOR(v, 0, N) {
-            doub[k + 1][v] = doub[k][doub[k][v]];
-        }
-    }
-
-    FOR(i, 0, N) {
-        int d = D;
-        int pos = i;
-        int k = 0;
-        do {
-            if (d & 1)pos = doub[k][pos];
-            d >>= 1;
-            k++;
-        } while (d != 0);
-        cout << pos + 1 << endl;
-    }
+/* -----  2019/04/11  Problem: ABC 013 D / Link: http://abc013.contest.atcoder.jp/tasks/abc013_d  ----- */
 
 
-    return 0;
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL N, M, D; cin >> N >> M >> D;
+	vector<LL> a(M);
+	for (int i = 0; i < M; ++i) {
+		cin >> a[i];
+		a[i]--;
+	}
+	reverse(ALL(a));
+	// doubling !!
+	VI next_p(N);
+	iota(ALL(next_p), 0);
+	FOR(i, 0, M) {
+		int p = a[i];
+		swap(next_p[p], next_p[p + 1]);
+	}
+	VVI doubling(32, VI(N, -1));
+	FOR(i, 0, N) {
+		doubling[0][i] = next_p[i];
+	}
+	FOR(i, 0, 31) {
+		FOR(j, 0, N) {
+			doubling[i + 1][j] = doubling[i][doubling[i][j]];
+		}
+	}
+	FOR(i, 0, N) {
+		// D回移動する
+		// 上から二分探索っぽく
+		int p = i;
+		FORR(t, 30, 0 - 1) {
+			if (D & 1 << t)
+				p = doubling[t][p];
+		}
+		cout << p + 1 << endl;
+	}
+
+	return 0;
 }
