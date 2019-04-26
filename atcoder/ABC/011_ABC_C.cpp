ad60f0d50d0bc4eb808b9c6a5e714435a5eb24c5
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-26 22:25:17 +0900

    solve 011_ABC_C.

--- atcoder/ABC/011_ABC_C.cpp
+++ atcoder/ABC/011_ABC_C.cpp
@@ -1,66 +1,69 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-typedef long long ll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/03/02  Problem: ABC011 C / Link: https://abc011.contest.atcoder.jp/tasks/abc011_3 ----- */
-/* ------問題------
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+/* -----  2019/04/11  Problem: ABC 011 C / Link: http://abc011.contest.atcoder.jp/tasks/abc011_c  ----- */
 
 
-あなたは、友人から、一人用のゲームを紹介されました。
-最初に、数字 N が与えられます。 1 , 2 , 3 の中から好きな数字を選び、 与えられた数字に対し、引き算を行う、という処理を行うことできます。
-この処理は 100 回まで行うことが可能であり、最終的に数字を 0 にすることが目標のゲームです。
-しかし、計算途中でなってはいけないNG数字が 3 つ与えられており、 この数字に一時的にでもなってしまった瞬間、このゲームは失敗となります。
-NG数字が N と同じ場合も失敗となります。
-NG数字が N と同じ場合について、正しい判定が行われていませんでした。リジャッジを行いました。
-あなたは、このゲームが、目標達成可能なゲームとなっているか調べたいです。
-目標達成可能な場合はYES、そうでない場合はNOと出力してください。
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
------問題ここまで----- */
-/* -----解説等-----
+	LL N; cin >> N;
+	VI ng(301, 0);
+	FOR(i, 0, 3) {
+		int a; cin >> a;
+		ng[a] = 1;
+	}
+	auto chmax = [](LL& a, const LL b) {
+		a = max(a, b);
+	};
 
-ＮＧ数字がNと同じ点だけに注意。
-動的計画法を使わなくても、3マスまでしか進めないことから貪欲に遠くに行けばよい。
-
-
- ----解説ここまで---- */
-
-ll N, a, b, c;
-bool m[301];
-ll ans = 0LL;
-
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-
-    cin >> N >> a >> b >> c;
-    if (N == a || N == b || N == c) {
-        cout << "NO" << endl;
-        return 0;
-    }
-    FOR(i, 0, 301)m[i] = true;
-    m[a] = false;
-    m[b] = false;
-    m[c] = false;
-
-    int index = N;
-    FOR(i, 0, 100) {
-        if (m[index - 3] == true && index - 3 >= 0)index -= 3;
-        else if (m[index - 2] == true && index - 2 >= 0)index -= 2;
-        else if (m[index - 1] == true && index - 1 >= 0)index -= 1;
-        else {
-            cout << "NO" << endl;
-            return 0;
-        }
-        if (index == 0) {
-            cout << "YES" << endl;
-            return 0;
-        }
-    }
-
-    cout << "NO" << endl;
-
-    return 0;
+	VL dp(N + 1, -1);
+	dp[N] = (ng[N] ? 0 : 100);
+	FORR(n, N, 0) {
+		FOR(sub, 1, 3 + 1) {
+			int nx = n - sub;
+			if (nx >= 0 && !ng[nx]) {
+				chmax(dp[nx], dp[n] - 1);
+			}
+		}
+	}
+	auto print_yesno = [](LL yes, int mode = 0) {
+		if (mode == 0) {
+			cout << (yes ? "yes" : "no") << endl;
+		}
+		else if (mode == 1) {
+			cout << (yes ? "Yes" : "No") << endl;
+		}
+		else if (mode == 2) {
+			cout << (yes ? "YES" : "NO") << endl;
+		}
+		else assert(0);
+	};
+	DD(de(dp[0]))
+		print_yesno(dp[0] >= 0, 2);
+	return 0;
 }
