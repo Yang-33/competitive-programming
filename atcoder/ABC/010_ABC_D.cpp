c11b6f7b1b309162a6eeaf7020c0e8d0af1f99d7
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-26 22:25:16 +0900

    solve 010_ABC_D.

--- atcoder/ABC/010_ABC_D.cpp
+++ atcoder/ABC/010_ABC_D.cpp
@@ -1,111 +1,136 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,s,e) for(int (i)=(s);(i)<(e);(i)++)
-const int INF = 1e9;
-typedef long long ll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/03/02  Problem: ABC010 D / Link: https://abc010.contest.atcoder.jp/tasks/abc010_4 ----- */
-/* ------問題------
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-高橋君の秘書のなぎさちゃんは、高橋君が大好きです。今日も、高橋君に悪い虫が取り憑かないように、高橋君を監視しなければなりません。
-高橋君は、女の子と仲良くなるために、自前のSNSを使います。SNSで友人関係にある人を辿って行き、見つけた女の子にメッセージを送ります。 
-なぎさちゃんは、高橋君のメッセージを女の子が見ることがないように、このSNSに対して、工作を行うことにしました。
-行える工作活動は、以下の 2つです。
-特定の二人の友人関係を解消する
-特定の一人のパスワードを変え、ログイン出来なくする　高橋君のパスワードは変更できません。(21:11追記)
-友人関係が解消されると、高橋君は、その二人の間を辿ることが出来なくなります。しかし、他の友人を経由して、辿ることが可能な場合は、その限りではありません。
-パスワードを変更すると、その人は、メッセージを見ることが不可能になります。友人関係に変化はないので、パスワードを変更された人を辿って、別の友人を探すのは可能です。
-なぎさちゃんは、出来るだけ工作の回数を少なくして、予めマークした女の子達が、高橋君のメッセージを閲覧できないようにしたいです。なぎさちゃんが工作を行う必要のある回数を求めてください。
+/* -----  2019/04/12  Problem: ABC 010 D / Link: http://abc010.contest.atcoder.jp/tasks/abc010_d  ----- */
+/* DINIC */
 
------問題ここまで----- */
-/* -----解説等-----
+typedef long long LL;
+typedef LL CapType;
+const CapType DINIC_eps = 0;
+const CapType DINIC_INF = 1e18;
+struct DINIC {
+#define  MAX_V 1200
+	struct edge {
+		int to, rev; CapType cap;
+		edge() {}
+		edge(int to, CapType cap, int rev) :to(to), cap(cap), rev(rev) {}
+	};
 
-グラフをうまく切断したいみたい。
-操作２が厄介である。
-各友人関係を容量1の枝で双方向に繋ぎ、高橋くんをsource、女の子から容量1で片方向につながれた頂点をsinkとするネットワークと考える。
-ここで、各操作1が頂点間の枝を切断する操作であり、また、女の子以外に対して操作2を行う必要がないことを考えると、
-操作2は女の子とsinkを結ぶ枝を切断する操作であることがわかる。
-この操作を最小にしたいということは、求める答えは、このネットワークの最小カットである。
-よって最大フロー=最小カット定理により、このネットワークの最大フローを求めれば良い。
+	vector<edge> G[MAX_V];
+	LL level[MAX_V], iter[MAX_V];
 
-冷静になぎさちゃんが怖すぎる。
+	//void add_edge(int from, int to, CapType cap) {
+	//	G[from].push_back(edge(to, cap, G[to].size()));
+	//	G[to].push_back(edge(from, 0, G[from].size() - 1));
+	//}
+	void add_directed_edge(int from, int to, CapType cap) {
+		G[from].push_back(edge(to, cap, G[to].size()));
+		G[to].push_back(edge(from, 0, G[from].size() - 1));
+	}
+	void add_undirected_edge(int from, int to, CapType cap) {
+		G[from].push_back(edge(to, cap, G[to].size()));
+		G[to].push_back(edge(from, cap, G[from].size() - 1));
+	}
+	void bfs(int s) {
+		memset(level, -1, sizeof(level));
+		queue<int> q;
+		level[s] = 0;
+		q.push(s);
+		while (!q.empty()) {
+			int v = q.front(); q.pop();
+			FOR(i, 0, (int)G[v].size()) {
+				edge &e = G[v][i];
+				if (e.cap > DINIC_eps && level[e.to] < 0) {
+					level[e.to] = level[v] + 1;
+					q.push(e.to);
+				}
+			}
+		}
+	}
 
-https://gist.github.com/knuu/64ba401cfd58bd803e527cbd588ba721
-つよそう
+	CapType dfs(int v, int t, CapType f) {
+		if (v == t) return f;
+		for (LL &i = iter[v]; i < (int)G[v].size(); ++i) {
+			edge &e = G[v][i];
+			if (e.cap > DINIC_eps && level[v] < level[e.to]) {
+				CapType d = dfs(e.to, t, min(f, e.cap));
+				if (d > DINIC_eps) {
+					e.cap -= d;
+					G[e.to][e.rev].cap += d;
+					return d;
+				}
+			}
+		}
+		return 0;
+	}
+
+	CapType max_flow(int s, int t) {
+		CapType flow = 0;
+		while (true) {
+			bfs(s);
+			if (level[t] < 0) return flow;
+			memset(iter, 0, sizeof(iter));
+			CapType f;
+			while ((f = dfs(s, t, DINIC_INF)) > 0)
+				flow += f;
+		}
+	}
+	bool used[MAX_V];
+	int countdfs(int v) {
+		int ret = 0;
+		used[v] = 1;
+		ret++;
+		FOR(i, 0, (int)G[v].size()) {
+			int u = G[v][i].to;
+			if (G[v][i].cap > DINIC_eps && !used[u])ret += countdfs(u);
+		}
+		return ret;
+	}
 
 
- ----解説ここまで---- */
-
-int N, g, E;
-
-int ans = 0LL;
-
-#define MAX_V 130
-struct edge {
-    int to; int cap; int rev;
 };
-vector<edge> G[MAX_V];
 
-bool used[MAX_V];
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
-void add_edge(int from, int to, int cap) {
-    G[from].push_back((edge) { to, cap, (int)G[to].size() });
-    G[to].push_back((edge) { from, 0, (int)G[from].size() - 1 });
-}
+	int N, G, E; cin >> N >> G >> E;
+	int S = 0, T = N;
+	DINIC F;
+	FOR(i, 0, G) {
+		int a; cin >> a;
+		F.add_directed_edge(a, T, 1);
+	}
+	FOR(i, 0, E) {
+		int a, b; cin >> a >> b;
+		F.add_undirected_edge(a, b, 1);
+	}
+	LL ans = F.max_flow(S, T);
 
-int dfs(int v, int t, int f) {
-    if (v == t)return f;
-    used[v] = true;
+	cout << ans << "\n";
 
-    FOR(i, 0, (int)G[v].size()) {
-        edge &e = G[v][i];
-        if (!used[e.to] && e.cap > 0) {
-            int d = dfs(e.to, t, min(f, e.cap));
-            if (d > 0) {
-                e.cap -= d;
-                G[e.to][e.rev].cap += d;
-                return d;
-            }
-        }
-    }
-    return 0;
-}
-
-int max_flow(int s, int t) {
-    int flow = 0;
-    for (;;) {
-        memset(used, 0, sizeof(used));
-        int f = dfs(s, t, INF);
-        if (f == 0)return flow;
-        flow += f;
-    }
-    return -1;
-}
-
-
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-
-    cin >> N >> g >> E;
-    FOR(i, 0, g) {
-        int a; cin >> a;
-        add_edge(a, N, 1);
-    }
-
-    FOR(i, 0, E) {
-        int a, b;
-        cin >> a >> b;
-        add_edge(a, b, 1);
-        add_edge(b, a, 1);
-    }
-    ans = max_flow(0, N);
-    cout << ans << endl;
-
-    return 0;
+	return 0;
 }
