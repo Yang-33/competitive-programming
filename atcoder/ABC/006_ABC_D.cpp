320c6d51a7543dff8d826af348df2a37a8dac37b
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-27 22:30:22 +0900

    solve 006_ABC_D.

--- atcoder/ABC/006_ABC_D.cpp
+++ atcoder/ABC/006_ABC_D.cpp
@@ -1,48 +1,52 @@
-#include<iostream>
-#include<algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-const int INF = 1e9;
 
-/* -----  2017/03/06  Problem: ABC006 D / Link: http://abc006.contest.atcoder.jp/tasks/abc006_4 ----- */
-/* ------問題------
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-数字が書かれたカードが N 枚あります。このカードの束（山札）に対して以下の操作が可能です。
-山札からカードを 1 枚抜き取り、任意の場所に挿入する。
-山札の上から下に向けて、カードを昇順に並べ替えるために必要な、最小の操作回数を求めてください。
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
------問題ここまで----- */
-/* -----解説等-----
+/* -----  2019/04/19  Problem: ABC 006 D / Link: http://abc006.contest.atcoder.jp/tasks/abc006_d  ----- */
 
-最初のカードの並びに対して、カードを抜いた後の並びがなるべく一致しているものを求めたい。
-最長増加部分列。
+int lis(vector<int> a) {
+	int n = a.size();
+	const int inf = 1e9;
+	vector<int> dp(n, inf);
+	for (int i = 0; i < n; i++) *lower_bound(dp.begin(), dp.end(), a[i]) = a[i];
+	return lower_bound(dp.begin(), dp.end(), inf) - dp.begin();
+}
 
-----解説ここまで---- */
 
-ll N;
-int a[30000];
-int dp[32000];
-ll ans = 0LL;
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
+	int N; cin >> N;
+	vector<int> a(N);
+	for (int i = 0; i < N; ++i) {
+		cin >> a[i];
+	}
+	LL ans = N - lis(a);
 
-    cin >> N;
-    FOR(i, 0, N) {
-        cin >> a[i];
-    }
-    FOR(i, 0, 32000)dp[i] = INF;
+	cout << ans << "\n";
 
-    int res = -1;
-    FOR(i, 0, N) {
-        int id = lower_bound(dp, dp + i, a[i]) - dp;
-        dp[id] = a[i];
-        res = max(res, id);
-    }
-    cout << N-res-1 << endl;
-
-    return 0;
+	return 0;
 }
