86c6e12006c63540c2c97ccaf541ca3a2b6616ea
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-27 22:30:20 +0900

    solve 004_ABC_C.

--- atcoder/ABC/004_ABC_C.cpp
+++ atcoder/ABC/004_ABC_C.cpp
@@ -1,47 +1,49 @@
-#include<iostream>
-#include<algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/03/07  Problem:  / Link: http://abc004.contest.atcoder.jp/tasks/abc004_3 ----- */
-/* ------問題------
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-ここにカードが 6 枚あり、それぞれ 1 から 6 までの整数が書かれています。
-最初、左から右へ 1 のカードから 6 のカードまでがソートされた状態で並んでいます。
-高橋君には整数 N が与えられます。高橋君は以下の操作を i=0,1,2, … ,N?1 に対して i=0 から 順番に行います。
-左から (i mod 5)+1 番目にあるカードと、左から (i mod 5)+2 番目にあるカードの位置を入れ替える。
-最終状態の盤面は。
+/* -----  2019/04/19  Problem: ABC 004 C / Link: http://abc004.contest.atcoder.jp/tasks/abc004_c  ----- */
 
------問題ここまで----- */
-/* -----解説等-----
 
-３０回動かすと元に戻るという性質から３０の剰余だけかんがえればよい。
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
- ----解説ここまで---- */
+	LL N; cin >> N;
+	N %= 30;
+	VI a(6);
+	iota(ALL(a), 1);
+	FOR(i, 0, N) {
+		int p1 = i % 5;
+		int p2 = i % 5 + 1;
+		swap(a[p1], a[p2]);
+	}
 
-ll N;
-int num[7];
-ll ans = 0LL;
+	FOR(i, 0, 6) {
+		cout << a[i];
+	}cout << endl;
 
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-
-    FOR(i, 0, 7)num[i] = i;
-
-    cin >> N;
-    int s = N % 30;
-    FOR(i, 0, s) {
-        int x = i % 5;
-        swap(num[x + 1], num[x + 2]);
-    }
-
-    FOR(i, 1, 7)
-        cout << num[i];
-    cout << endl;
-
-    return 0;
+	return 0;
 }
