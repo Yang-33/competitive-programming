b8f8b4f4e43ec009eec1e6c71a1956e6eed92d7f
Yang-33 <kasai.yuta0810@gmail.com>
2019-04-27 22:30:20 +0900

    solve 004_ABC_D.

--- atcoder/ABC/004_ABC_D.cpp
+++ atcoder/ABC/004_ABC_D.cpp
@@ -1,59 +1,61 @@
-#include<iostream>
-#include<algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
-#define FOR(i,s,e) for(ll (i)=(s);(i)<(e);(i)++)
-const ll LINF = 1e16;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-/* -----  2017/03/08  Problem: ABC004 D / Link: http://abc004.contest.atcoder.jp/tasks/abc004_4 ----- */
-/* ------問題------
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-一直線上に無限個の箱が並んでいます。それぞれの箱に、左から右にかけて …,?2,?1,0,1,2,… の番号が付いています。
-いま、番号が ?100 の箱に赤いマーブルが R 個入っています。同様に、番号が 0 の箱に緑のマーブルが G 個、番号が 100 の箱に青いマーブルが B 個入っています。これら以外の箱には何も入っていません。
-以下の操作を繰り返して、全ての箱に入っているマーブルの個数が 1 個以下になるようにして下さい。
-マーブルを 1 つ選び、それを左右どちらかの隣接する箱に移動させる。
-ただしこのとき、1 つの箱に複数の異なる色のマーブルを入れてはならない。
-必要となる最小の操作回数を求めてください
+/* -----  2019/04/19  Problem: ABC 004 D / Link: http://abc004.contest.atcoder.jp/tasks/abc004_d  ----- */
 
------問題ここまで----- */
-/* -----解説等-----
 
-緑のマーブルの左端の位置を全探索し、それに対応した各マーブルの左端の位置を決定しそこから移動にかかるコストを計算していけばよい。
-なんで昔解けなかったんだろう。
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 
- ----解説ここまで---- */
+	LL R, G, B; cin >> R >> G >> B;
+	// Gsについてみる
+	// [[ ]] [[ ]] [[ ]]
+	LL ans = LINF;
+	FOR(Gs, -1000, 1000 + 1) {
+		LL Rs = min(-100 - R / 2, Gs - R);
+		LL Bs = max(100 - B / 2, Gs + G);
 
-ll N,R,G,B;
+		LL subcost = 0;
 
-ll ans = 0LL;
+		FOR(i, Rs, Rs + R) {
+			subcost += abs(-100 - i);
+		}
+		FOR(i, Gs, Gs + G) {
+			subcost += abs(0 - i);
+		}
+		FOR(i, Bs, Bs + B) {
+			subcost += abs(100 - i);
+		}
 
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
+		ans = min(ans, subcost);
+	}
 
-    cin >> R>>G>>B;
-    ans = LINF;
 
-    FOR(i, -600, 601) {
-        int Rl = min(i - R, -100 - R / 2);
-        int Bl = max(i + G, 100 - B / 2);
-        ll c = 0;
-        FOR(s, Rl, Rl + R) {
-            c += abs(-100 - s);
-        }
-        FOR(s, i, i + G) {
-            c += abs(s);
-        }
-        FOR(s, Bl, Bl + B) {
-            c += abs(100 - s);
-        }
-        ans = min(ans, c);
-    }
-    
+	cout << ans << "\n";
 
-    cout << ans << endl;
-    
-    return 0;
+	return 0;
 }
