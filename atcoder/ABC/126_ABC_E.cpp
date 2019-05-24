c8be18aee2865862b703e0edf1ebb43f112d72a1
Yang-33 <kasai.yuta0810@gmail.com>
2019-05-24 16:47:51 +0900

    solve 126_ABC_E.

---
+++ atcoder/ABC/126_ABC_E.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+
+/* -----  2019/05/19  Problem: ABC 126 E / Link: http://abc126.contest.atcoder.jp/tasks/abc126_e  ----- */
+
+struct UnionFind {
+	vector<int> data;
+	UnionFind(int n) { data.assign(n, -1); }
+	bool unionSet(int x, int y) {
+		x = root(x); y = root(y);
+		if (x != y) {
+			if (data[y] < data[x]) swap(x, y);
+			data[x] += data[y]; data[y] = x;
+		}
+		return x != y;
+	}
+	bool same(int x, int y) { return root(x) == root(y); }
+	int root(int x) { return data[x] < 0 ? x : data[x] = root(data[x]); }
+	int size(int x) { return -data[root(x)]; }
+};
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL N, M; cin >> N >> M;
+	UnionFind uf(N);
+	FOR(i, 0, M) {
+		int a, b, c; cin >> a >> b >> c;
+		a--, b--;
+		uf.unionSet(a, b);
+	}
+	LL ans = 0LL;
+	FOR(i, 0, N) {
+		if (uf.root(i) == i)ans++;
+	}
+
+	cout << (ans) << "\n";
+
+	return 0;
+}
