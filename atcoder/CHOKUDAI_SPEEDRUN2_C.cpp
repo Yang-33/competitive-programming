822dc8bf7bb4e086b3d39f010775f4128ed499ed
Yang-33 <kasai.yuta0810@gmail.com>
2019-05-25 16:58:27 +0900

    solve CHOKUDAI_SPEEDRUN2_C.

---
+++ atcoder/CHOKUDAI_SPEEDRUN2_C.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+
+/* -----  2019/05/25  Problem: chokudai_speedrun2 C / Link: __CONTEST_URL__  ----- */
+
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL N; 
+	cin >> N;
+	LL ans = 0LL;
+	VL a(N), b(N);
+	FOR(i, 0, N) {
+		cin >> a[i] >> b[i];
+		ans = max(ans, a[i] + b[i]);
+	}
+
+	
+	
+	cout << (ans) << "\n";
+
+	return 0;
+}
