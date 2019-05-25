94a06193686fc152ae7447b640a0c030c4507daa
Yang-33 <kasai.yuta0810@gmail.com>
2019-05-26 02:25:28 +0900

    solve 127_ABC_B.

---
+++ atcoder/ABC/127_ABC_B.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+
+/* -----  2019/05/25  Problem: ABC 127 B / Link: http://abc127.contest.atcoder.jp/tasks/abc127_b  ----- */
+
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL r, D, x; cin >> r >> D >> x;
+	FOR(i, 0, 10) {
+		LL nx = r * x - D;
+		cout << nx << endl;
+		x = nx;
+	}
+
+	return 0;
+}
