e3a6db93be46cd284a0e2f13f6eed6f7bf4847a7
Yang-33 <kasai.yuta0810@gmail.com>
2019-05-27 01:32:11 +0900

    solve AOJ1138.

--- AOJ/AOJ1138.cpp
+++ AOJ/AOJ1138.cpp
@@ -14,75 +14,63 @@ using VL = vector<LL>;        using VVL = vector<VL>;
 #define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
 #define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
 #define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
-#define debug(x) cerr << #x << ": " << x << endl
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
 const int INF = 1e9;                          const LL LINF = 1e16;
 const LL MOD = 1000000007;                    const double PI = acos(-1.0);
-int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2018/06/12  Problem: AOJ 1138 / Link: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1138  ----- */
-/* ------問題------
-
-昔，昔のことである． 一人の旅人がいた． 彼は，馬車（駅馬車）を使っての旅行を計画している． 出発地と目的地は決まっているのだが，ルートが決まらなくて困っている． 彼のためにルートを決定するプログラムを書くことがあなたの仕事である．
-この国には複数の都市があり，それらの間を結ぶ道路網が整備されている． 道路で直結されている二つの都市の間は，馬車で移動することができる． 馬車に乗るには，馬車券が必要である． それぞれの馬車券には，馬の頭数が指定されている． 当然ながら，頭数の多い馬車ほど速い．
-旅人は，出発時に何枚かの馬車券を持っている． 道路網と手持ちの馬車券に関する情報から， どのようなルートを選べば最も早く目的地に着けるかを求めたい． この際，馬車券をどう使えばよいかも考える必要がある．
-次のような条件を仮定する．
-1台の馬車で移動できるのは，1区間（道路で直結された都市間）だけである． すなわち，都市に到着するたびに馬車を乗り換えなければならない．
-1区間に使える馬車券は1枚だけである．
-1枚の馬車券は一度しか使えない．
-都市間の所要時間は，距離を頭数で割った値である．
-乗り換えに要する時間は無視する．
-
------問題ここまで----- */
-/* -----解説等-----
-
-DAGなのでDPできる。
-最短路でもできる。
-
-----解説ここまで---- */
+/* -----  2019/05/26  Problem: AOJ 1138 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/1138  ----- */
 
 
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
-	int B, N, E, S, T;
-	while (cin >> B >> N >> E >> S >> T, N) {
-		vector<double> speed(B);
-		FOR(i, 0, B) {
-			cin >> speed[i];
+	LL N, M, P, A, B; ;
+	while (cin >> N >> M >> P >> A >> B, N) {
+
+		vector<double>t(N);
+		FOR(i, 0, N) {
+			cin >> t[i];
 		}
-		VVI G(N, VI(N, -1));
-		FOR(i, 0, E) {
-			int a, b, c;
-			cin >> a >> b >> c;
+		VVI edge(M, VI(M, -1));
+		FOR(i, 0, P) {
+			int a, b, c; cin >> a >> b >> c;
 			a--, b--;
-			G[a][b] = G[b][a] = c;
+			edge[a][b] = edge[b][a] = c;
 		}
-		S--, T--;
-		vector<vector<double>>dp(1 << B, vector<double>(N,INF));
-		dp[0][S] = 0;
-		FOR(state, 0, 1 << B) {
-			FOR(v, 0, N) {
-				FOR(b, 0, B) {
-					if (state & 1 << b)continue;
-					FOR(nv, 0, N) {
-						if (G[v][nv]>=0) {
-							dp[state|1<<b][nv] = min(dp[state|1<<b][nv], dp[state][v] + G[v][nv]/speed[b]);
-						}
+		vector<vector<double>>dp(1 << N, vector<double>(M, LINF));
+		A--, B--;
+		dp[0][A] = 0;
+		FOR(state, 0, 1 << N) {
+			FOR(from, 0, M) {
+				FOR(to, 0, M) {
+					if (edge[from][to] == -1)continue;
+					FOR(i, 0, N) {
+						if (state & 1 << i)continue;
+						double cost = edge[from][to] / t[i];
+						int nxstate = state | (1 << i);
+						dp[nxstate][to] = min(dp[nxstate][to], dp[state][from] + cost);
 					}
 				}
 			}
 		}
-		double ans = INF;
-		FOR(state, 0, 1 << B) {
-			ans = min(ans, dp[state][T]);
+
+		double ans = LINF;
+		FOR(i, 0, 1 << N) {
+			ans = min(ans, dp[i][B]);
 		}
-		if (ans >= INF) {
+		if (ans >= LINF - 1) {
 			cout << "Impossible" << endl;
 		}
 		else {
-			cout << fixed << setprecision(10) << ans << "\n";
+			cout << fixed << setprecision(10) << ans << endl;
 		}
 	}
+
 	return 0;
 }
