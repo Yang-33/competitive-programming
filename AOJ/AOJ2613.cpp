9de7fd7dbac6934b8e097d51385b43f4b76bea3d
Yang-33 <kasai.yuta0810@gmail.com>
2019-06-01 18:14:45 +0900

    solve AOJ2613.

--- AOJ/AOJ2613.cpp
+++ AOJ/AOJ2613.cpp
@@ -1,140 +1,124 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
-#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
-#define debug(x) cout << #x << ": " << x << endl
-#define mp make_pair
-#define pb push_back
-const ll MOD = 1000000007;
-const int INF = 1e9;
-const ll LINF = 1e16;
-const double PI = acos(-1.0);
-int dx[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-int dy[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 
-/* -----  2017/06/16  Problem: JAG summer 2013  / Link: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2613  ----- */
-/* ------問題------
+/* -----  2019/05/30  Problem: AOJ 2613 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/2613  ----- */
 
-AOJ2613
-Unordered Operators
 
------問題ここまで----- */
-/* -----解説等-----
+LL expr1(const string&s, int &id, vector<set<char>>&op);
+LL expr2(const string&s, int &id, vector<set<char>>&op);
+LL expr3(const string&s, int &id, vector<set<char>>&op);
+LL expr4(const string&s, int &id, vector<set<char>>&op);
 
-構文解析の問題であるが、演算順序が最適なものを探す必要がある。
-演算子は３つしかないのでこれを全探索すればよい。演算順位は３つ存在するので３段階の構文解析をすればよい。
-(v[1].find(s[i]) != string::npos
-とかができるようになった。=> string ::nposは-1で定義されているらしい。
-
-----解説ここまで---- */
-
-ll N;
-string S;
-char c[3] = { '+','-','*' };
-ll ans = LLONG_MIN;
-
-ll expr0(int &i, string &s, vector<string>&v);
-ll expr1(int &i, string &s, vector<string>&v);
-ll expr2(int &i, string &s, vector<string>&v);
-ll expr3(int &i, string &s, vector<string>&v);
-ll getnum(int &i, string &s, vector<string>&v) {
-	ll num = 0;
-	while ('0' <= s[i] && s[i] <= '9') {
-		num *= 10;
-		num += s[i] - '0';
-		i++;
+// 優先度低い
+LL expr1(const string&s, int &id, vector<set<char>>&op) {
+	LL ret = expr2(s, id, op);
+	while (id < SZ(s) && op[0].find(s[id]) != op[0].end()) {
+		char c = s[id];
+		id++;
+		if (c == '+') {
+			ret += expr2(s, id, op);
+		}
+		if (c == '-') {
+			ret -= expr2(s, id, op);
+		}
+		if (c == '*') {
+			ret *= expr2(s, id, op);
+		}
 	}
-	return num;
+	return ret;
 }
-
-ll expr0(int &i, string &s, vector<string>&v) {
-	ll num = expr1(i, s, v);
-
-	while (i < N && (v[0].find(s[i]) != string::npos)) {
-		char op = s[i];
-		i++;
-		if (op == '+') {
-			num += expr1(i, s, v);
+LL expr2(const string&s, int &id, vector<set<char>>&op) {
+	LL ret = expr3(s, id, op);
+	while (id < SZ(s) && op[1].find(s[id]) != op[1].end()) {
+		char c = s[id];
+		id++;
+		if (c == '+') {
+			ret += expr3(s, id, op);
 		}
-		else if (op == '-') {
-			num -= expr1(i, s, v);
+		if (c == '-') {
+			ret -= expr3(s, id, op);
 		}
-		else if (op == '*') {
-			num *= expr1(i, s, v);
+		if (c == '*') {
+			ret *= expr3(s, id, op);
 		}
 	}
-	return num;
+	return ret;
 }
-
-ll expr1(int &i, string &s, vector<string>&v) {
-	ll num = expr2(i, s, v);
-
-	while (i < N && (v[1].find(s[i]) != string::npos)) {
-		char op = s[i];
-		i++;
-		if (op == '+') {
-			num += expr2(i, s, v);
+LL expr3(const string&s, int &id, vector<set<char>>&op) {
+	LL ret = expr4(s, id, op);
+	while (id < SZ(s) && op[2].find(s[id]) != op[2].end()) {
+		char c = s[id];
+		id++;
+		if (c == '+') {
+			ret += expr4(s, id, op);
 		}
-		else if (op == '-') {
-			num -= expr2(i, s, v);
+		if (c == '-') {
+			ret -= expr4(s, id, op);
 		}
-		else if (op == '*') {
-			num *= expr2(i, s, v);
+		if (c == '*') {
+			ret *= expr4(s, id, op);
 		}
 	}
-	return num;
+	return ret;
 }
-
-ll expr2(int &i, string &s, vector<string>&v) {
-	ll num = expr3(i, s, v);
-
-	while (i < N && (v[2].find(s[i]) != string::npos)) {
-		char op = s[i];
-		i++;
-		if (op == '+') {
-			num += expr3(i, s, v);
-		}
-		else if (op == '-') {
-			num -= expr3(i, s, v);
-		}
-		else if (op == '*') {
-			num *= expr3(i, s, v);
-		}
+LL expr4(const string&s, int &id, vector<set<char>>&op) {
+	if (s[id] == '(') {
+		id++;
+		LL ret = expr1(s, id, op);
+		id++;
+		return ret;
 	}
-	return num;
-}
-
-
-ll expr3(int &i, string &s, vector<string>&v) {
-	ll num = 0;
-	if (s[i] == '(') {
-		i++;
-		num = expr0(i, s, v);
-		i++;
+	LL ret = 0;
+	while (id < SZ(s) && isdigit(s[id])) {
+		ret *= 10;
+		ret += s[id] - '0';
+		id++;
 	}
-	else num = getnum(i, s, v);
-	return num;
+
+	return ret;
 }
 
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
-	cin >> S;
-	FOR(i, 0, 3)FOR(j, 0, 3)FOR(k, 0, 3) {
-		vector<string>v(3);
-		v[i].pb(c[0]);
-		v[j].pb(c[1]);
-		v[k].pb(c[2]);
-		N = S.size();
-		int x = 0;
-		ans = max(ans, expr0(x, S, v));
+	LL ans = LLONG_MIN;
+	string s; cin >> s;
+	FOR(i, 0, 3) {
+		FOR(j, 0, 3) {
+			FOR(k, 0, 3) {
+				vector<set<char>>op(3);
+				op[i].insert('+');
+				op[j].insert('-');
+				op[k].insert('*');
+				int id = 0;
+				ans = max(ans, expr1(s, id, op));
+			}
+		}
 	}
+
 	cout << ans << endl;
 
 	return 0;
