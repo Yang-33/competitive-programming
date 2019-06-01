458b1ce344c0c76f1467bf3fab5ebdbd8d2cc778
Yang-33 <kasai.yuta0810@gmail.com>
2019-06-01 18:14:44 +0900

    solve AOJ2182.

--- AOJ/AOJ2182.cpp
+++ AOJ/AOJ2182.cpp
@@ -8,36 +8,23 @@ using VL = vector<LL>;        using VVL = vector<VL>;
 
 #define ALL(a)  begin((a)),end((a))
 #define RALL(a) (a).rbegin(), (a).rend()
-#define PB push_back
-#define EB emplace_back
-#define MP make_pair
 #define SZ(a) int((a).size())
 #define SORT(c) sort(ALL((c)))
 #define RSORT(c) sort(RALL((c)))
 #define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
 #define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
 #define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
-#define debug(x) cerr << #x << ": " << x << endl
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
 const int INF = 1e9;                          const LL LINF = 1e16;
 const LL MOD = 1000000007;                    const double PI = acos(-1.0);
-int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2018/04/27  Problem: AOJ 2182 / Link: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2182  ----- */
-/* ------問題------
+/* -----  2019/06/01  Problem: AOJ 2182 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/2182  ----- */
 
-
-
------問題ここまで----- */
-/* -----解説等-----
-
-dp[i][eleven]:=i番目までの数を使ったときに、あまりがeleven%11となるような数字の組数
-なんかmapで累積和dpもできる気がする(区間LR選択みたいに)
-
-----解説ここまで---- */
-
-LL N;
-
-LL ans = 0LL;
 
 int main() {
 	cin.tie(0);
@@ -45,25 +32,21 @@ int main() {
 
 	string s;
 	while (cin >> s, s != "0") {
-		ans = 0;
-		N = SZ(s);
+		int N = SZ(s);
 		VVL dp(N + 1, VL(11, 0));
 		FOR(i, 0, N) {
-			int num = s[i] - '0';
-
-			if (num)dp[i + 1][num]++;
-
-			FOR(j, 0, 11)
-				dp[i + 1][(10 * j + num) % 11] += dp[i][j];
+			int a = s[i] - '0';
+			FOR(j, 0, 11) {
+				dp[i + 1][(j * 10 + a) % 11] += dp[i][j];
+			}
+			if (a)dp[i + 1][a]++;
 		}
-		FOR(i, 1, N + 1) {
+		LL ans = 0LL;
+		FOR(i, 0, N + 1) {
 			ans += dp[i][0];
 		}
-
-
-		cout << ans << "\n";
+		cout << (ans) << "\n";
 	}
-
 
 	return 0;
 }
