4fb839262730d967e9e7d239d96dd329c6541aae
Yang-33 <kasai.yuta0810@gmail.com>
2019-06-29 18:18:55 +0900

    solve AOJ2741.

--- AOJ/AOJ2741.cpp
+++ AOJ/AOJ2741.cpp
@@ -1,97 +1,102 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
-#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
-#define debug(x) cout << #x << ": " << x << endl
-#define mp make_pair
-#define pb push_back
-const ll MOD = 1000000007;
-const int INF = 1e9;
-const ll LINF = 1e16;
-const double PI = acos(-1.0);
-int dx[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-int dy[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 
-/* -----  2017/07/11  Problem: AOJ 2711  / Link: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2711&lang=jp ----- */
-/* ------問題------
+/* -----  2019/06/03  Problem: AOJ 2741 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/2741  ----- */
 
 
------問題ここまで----- */
-/* -----解説等-----
 
-ゲーム。
-各状態のデッキの状況を記録、かつ得点を引数にわたす。
-
-----解説ここまで---- */
-
-ll N, M;
-int a[55];
-int b[55];
-ll ans = 0LL;
-int dp[55][55][55][55][2][3];
-
-int dfs(int as, int at, int bs, int bt, int turn, int pass, int asum, int bsum) {
-	int& ret = dp[as][at][bs][bt][turn][pass];
-	if (ret != INF&&ret != -INF)return ret;
-	if (!turn) {
-		if (at < N) {
-			if (a[at] > 0) {//1
-				ret = max(ret, dfs(as, at + 1, bs, bt, turn ^ 1, 0, asum + a[at], bsum));
-			}
-			else {//-1
-				ret = max(ret, dfs(as, at + 1, bs, bt, turn ^ 1, 0, asum, 0));
-			}
-		}
-		//つまないpass)
-		if (as == at&&bs == bt) {
-			if (pass == 0)ret = max(ret, dfs(as, at, bs, bt, turn ^ 1, 1, 0, 0));
-			else if (pass == 1)ret = max(ret, 0);
-		}
-		else {
-			ret = max(ret, dfs(at, at, bt, bt, turn ^ 1, 0, 0, 0) + asum - bsum);
-		}
-	}
-	else {
-		if (bt < M) {
-			if (b[bt] > 0) {//1
-				ret = min(ret, dfs(as, at, bs, bt + 1, turn ^ 1, 0, asum, bsum + b[bt]));
-			}
-			else {//-1
-				ret = min(ret, dfs(as, at, bs, bt + 1, turn ^ 1, 0, 0, bsum));
-			}
-		}
-		//つまない(pass)
-		if (as == at&&bs == bt) {
-			if (pass == 0)ret = min(ret, dfs(as, at, bs, bt, turn ^ 1, 1, 0, 0));
-			else if (pass == 1)ret = min(ret, 0);
-		}
-		else {
-			ret = min(ret, dfs(at, at, bt, bt, turn ^ 1, 0, 0, 0) + asum - bsum);
-		}
-	}
-
-	return ret;
-}
-
+LL dp[51][51][51][51][2][2];
 int main() {
-	cin.tie(0);
-	ios_base::sync_with_stdio(false);
-
-	cin >> N >> M;
-	FOR(i, 0, N)cin >> a[i];
-	FOR(i, 0, M)cin >> b[i];
-
-	FOR(i, 0, 55)FOR(j, 0, 55)FOR(k, 0, 55)FOR(l, 0, 55)FOR(m, 0, 2)FOR(n, 0, 3) {
-		if (m == 0)dp[i][j][k][l][m][n] = -INF;
-		else dp[i][j][k][l][m][n] = INF;
+	FOR(i, 0, 51)FOR(j, 0, 51)FOR(k, 0, 51)FOR(l, 0, 51)FOR(a, 0, 2) {
+		dp[i][j][k][l][0][a] = -LINF;
+		dp[i][j][k][l][1][a] = LINF;
 	}
 
-	ans = dfs(0, 0, 0, 0, 0, 0, 0, 0);
+	int N, M; cin >> N >> M;
+	vector<int> a(N);
+	for (int i = 0; i < N; ++i) {
+		cin >> a[i];
+	}
+	vector<int> b(M);
+	for (int i = 0; i < M; ++i) {
+		cin >> b[i];
+	}
+
+	function<LL(int, int, int, int, bool, bool, LL, LL)>
+		f = [&](
+			int As, int At, int Bs, int Bt,
+			bool player, bool passcnt, LL Asum, LL Bsum) {
+		LL& ret = dp[As][At][Bs][Bt][player][passcnt];
+		if (player == 0 && ret != -LINF)return ret;
+		if (player == 1 && ret != LINF)return ret;
+		if (player == 0) { // to max
+			if (At < N) {
+				if (a[At] > 0) {
+					ret = max(ret, f(As, At + 1, Bs, Bt, !player, 0, Asum + a[At], Bsum));
+				}
+				else { // -1
+					ret = max(ret, f(As, At + 1, Bs, Bt, !player, 0, Asum, 0));
+				}
+			}
+			if (As == At && Bs == Bt) {
+				if (passcnt == 0) {
+					ret = max(ret, f(As, At, Bs, Bt, !player, 1, 0, 0));
+				}
+				else {
+					ret = max(ret, 0LL);
+				}
+			}
+			else {
+				ret = max(ret, f(At, At, Bt, Bt, !player, 0, 0, 0) + Asum - Bsum);
+			}
+
+		}
+		else { // to min
+			if (Bt < M) {
+				if (b[Bt] > 0) {
+					ret = min(ret, f(As, At, Bs, Bt+1, !player, 0, Asum, Bsum + b[Bt]));
+				}
+				else { // -1
+					ret = min(ret, f(As, At, Bs, Bt+1, !player, 0, 0, Bsum));
+				}
+			}
+			if (As == At && Bs == Bt) {
+				if (passcnt == 0) {
+					ret = min(ret, f(As, At, Bs, Bt, !player, 1, 0, 0));
+				}
+				else {
+					ret = min(ret, 0LL);
+				}
+			}
+			else {
+				ret = min(ret, f(At, At, Bt, Bt, !player, 0, 0, 0) + Asum - Bsum);
+			}
+		}
+		return ret;
+	};
+	LL ans = f(0, 0, 0, 0, 0, 0, 0, 0);
 	cout << ans << endl;
 
 	return 0;
