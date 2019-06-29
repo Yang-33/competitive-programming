fb51d60358bf2202fb9ad08bef7037eb71283c6f
Yang-33 <kasai.yuta0810@gmail.com>
2019-06-29 18:14:29 +0900

    solve M-SOLUTIONS2019_B.

---
+++ atcoder/M-SOLUTIONS2019_B.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+
+/* -----  2019/06/01  Problem: M-Solutions2019 B / Link: https://atcoder.jp/contests/m-solutions2019  ----- */
+
+
+int main() {
+	string s;
+	cin >> s;
+	int ans = 0;
+	for (auto cc : s) {
+		if (cc == 'x')ans++;
+	}
+	auto print_yesno = [](LL yes, int mode = 0) {
+		if (mode == 0) {
+			cout << (yes ? "yes" : "no") << endl;
+		}
+		else if (mode == 1) {
+			cout << (yes ? "Yes" : "No") << endl;
+		}
+		else if (mode == 2) {
+			cout << (yes ? "YES" : "NO") << endl;
+		}
+		else assert(0);
+	};
+	print_yesno(ans > 7, 2);
+
+	return 0;
+}
