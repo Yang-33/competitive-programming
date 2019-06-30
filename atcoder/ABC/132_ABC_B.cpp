008d29bd234745793bbf75d7d06d42bedd971096
Yang-33 <kasai.yuta0810@gmail.com>
2019-07-01 00:20:11 +0900

    solve 132_ABC_B.

---
+++ atcoder/ABC/132_ABC_B.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+
+/* -----  2019/06/29  Problem: ABC 132 B / Link: http://abc132.contest.atcoder.jp/tasks/abc132_b  ----- */
+
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	LL N; cin >> N;
+	vector<LL> a(N);
+	for (int i = 0; i < N; ++i) {
+		cin >> a[i];
+	}
+	
+	LL ans = 0LL;
+	FOR(i, 1, N - 1) {
+		VL b({ a[i - 1],a[i],a[i + 1] });
+		auto it = minmax_element(ALL(b));
+		if (a[i] != *it.first&&a[i] != *it.second)ans++;
+	}
+	
+	
+	cout << (ans) << "\n";
+
+	return 0;
+}
