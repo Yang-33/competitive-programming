7d44e38d2bdc0b7a041934344bda43dc0b92543f
Yang-33 <kasai.yuta0810@gmail.com>
2019-07-13 23:00:50 +0900

    solve AOJ0031.

---
+++ AOJ/AOJ0031.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+
+/* -----  2019/07/13  Problem: AOJ 0031 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/0031  ----- */
+
+
+int main() {
+	int N;
+	while (cin >> N) {
+		vector<int>a;
+		FOR(i, 0, 10) {
+			if (N & 1 << i) {
+				a.emplace_back(1 << i);
+			}
+		}
+		FOR(i, 0, SZ(a)) {
+			cout << a[i] << " \n"[i + 1 == SZ(a)];
+		}
+	}
+
+	return 0;
+}
