5095ccd7a5ee8647b20ff3159ec7d928fea84c85
Yang-33 <kasai.yuta0810@gmail.com>
2019-07-14 00:13:12 +0900

    solve AOJ0047.

---
+++ AOJ/AOJ0047.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+
+/* -----  2019/07/14  Problem: AOJ 0047 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/0047  ----- */
+
+stringstream deletecommma(string line) {
+	istringstream ss(line);
+	string s;
+	stringstream input;
+	while (getline(ss, s, ',')) {
+		input << s; input << " ";
+	}
+	return input;
+}
+int main() {
+	string in;
+	VI a(3); a[0] = 1;
+	while (cin >> in) {
+		auto input = deletecommma(in);
+		char x, y;
+		input >> x >> y;
+		x -= 'A', y -= 'A';
+		swap(a[x],a[y]);
+	}
+	FOR(i, 0, 3) {
+		if (a[i])cout << char(i + 'A') << "\n";
+	}
+	return 0;
+}
