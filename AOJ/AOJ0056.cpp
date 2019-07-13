dbdc531cf0469c9ce2cf8a5b2757ebb98638756c
Yang-33 <kasai.yuta0810@gmail.com>
2019-07-14 01:41:31 +0900

    solve AOJ0056.

---
+++ AOJ/AOJ0056.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+
+/* -----  2019/07/14  Problem: AOJ 0056 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/0056  ----- */
+bool prime[50001];
+vector<long long > make_prime(long long  N) {
+	vector<long long >a;
+	FOR(i, 0, N + 1) {
+		prime[i] = 1;
+	}prime[0] = prime[1] = 0;
+
+	for (LL i = 2; i <= N; i++) {
+		if (prime[i]) {
+			a.push_back((long long)i);
+			for (LL j = i * 2; j <= N; j += i)
+				prime[j] = 0;
+		}
+	}
+	return a;
+}
+int main() {
+	auto primes = make_prime(50000);
+	int N;
+	while (cin >> N, N) {
+		LL ans = 0;
+		FOR(i, 0, SZ(primes)) {
+			LL p1 = primes[i];
+			if (p1 > N)break;
+			LL p2 = N - p1;
+			if (p1 >= p2 && p2 > 0 && prime[p2])ans++;
+
+		}
+		cout << ans << "\n";
+	}
+}
