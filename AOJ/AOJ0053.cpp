e9105acec28a7d40124b54bf970e3680ad8e69ce
Yang-33 <kasai.yuta0810@gmail.com>
2019-07-14 00:41:27 +0900

    solve AOJ0053.

---
+++ AOJ/AOJ0053.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+
+/* -----  2019/07/14  Problem: AOJ 0053 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/0053  ----- */
+#define primeN 800000
+vector<long long > make_prime(long long  N) {
+	vector<long long >a;
+	vector<long long >prime(primeN, 0);
+	FOR(i, 0, primeN + 1) {
+		prime[i] = 1;
+	}prime[0] = prime[1] = 0;
+
+	for (int i = 2; i <= primeN; i++) {
+		if (prime[i]) {
+			a.push_back((long long)i);
+			for (int j = i * 2; j <= primeN; j += i)
+				prime[j] = 0;
+		}
+	}
+	return a;
+}
+#undef primeN 
+struct Cumusum {
+	vector<LL>csum;
+	Cumusum(vector<LL>&a) {
+		csum.assign((int)a.size() + 1, 0);
+		for (int i = 0; i < (int)a.size(); i++) {
+			csum[i + 1] = csum[i] + a[i];
+		}
+	}
+	LL query(int l, int r) {
+		return csum[r] - csum[l];
+	}
+};
+
+
+int main() {
+	auto res = make_prime(0);
+	Cumusum csum(res);
+	LL N;
+	while (cin >> N, N) {
+		cout << csum.query(0, N) << "\n";
+
+	}
+	return 0;
+}
