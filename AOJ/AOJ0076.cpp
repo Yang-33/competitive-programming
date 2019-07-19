e472543fcfec7857e8be0f4085bd3357af9f1938
Yang-33 <kasai.yuta0810@gmail.com>
2019-07-19 18:38:34 +0900

    solve AOJ0076.

---
+++ AOJ/AOJ0076.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+
+/* -----  2019/07/19  Problem: AOJ 0076 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/0076  ----- */
+
+
+using Double = long double;
+const double EPS = 1e-9;
+
+typedef struct Point {
+	Double x, y;
+	Point(Double x_, Double y_) : x(x_), y(y_) {}
+
+	bool operator < (const Point& a) const {
+		return fabs(x - a.x) < EPS ? y + EPS < a.y : x + EPS < a.x;
+	}
+
+	bool operator > (const Point& a) const {
+		return a < *this;
+	}
+
+	bool operator == (const Point& a) const {
+		return !(a < *this) && !(a > *this);
+	}
+
+	Point operator + (const Point& a) const {
+		return Point(x + a.x, y + a.y);
+	}
+
+	Point operator - (const Point& a) const {
+		return Point(x - a.x, y - a.y);
+	}
+
+	Point operator * (const Point& a) const {
+		return Point(x * a.x, y * a.y);
+	}
+
+	Point operator / (const Point& a) const {
+		return Point(x / a.x, y / a.y);
+	}
+
+	Point operator * (const Double& d) const {
+		return Point(x * d, y * d);
+	}
+
+	Point operator / (const Double& d) const {
+		return Point(x / d, y / d);
+	}
+} Vector;
+
+Double norm(const Point& a) {
+	return a.x * a.x + a.y * a.y;
+}
+Double abs(const Point& a) {
+	return sqrt(norm(a));
+}
+Vector unit_vector(const Vector& a) {
+	return a / abs(a);
+}
+Point rotate(const Vector& v, Double rad) {
+	return Vector(v.x * cos(rad) - v.y * sin(rad),
+		v.x * sin(rad) + v.y * cos(rad));
+}
+
+int main() {
+	Point Ido(0, 0);
+	int N;
+	while (cin >> N, N != -1) {
+		Point now(1, 0);
+		const Double rad = PI / 2;
+		FOR(_, 0, N - 1) {
+			Vector to = now + unit_vector(rotate(now, rad));
+			now = to;
+		}
+		cout << fixed << setprecision(10) << now.x << "\n" << now.y << "\n";
+	}
+
+	return 0;
+}
