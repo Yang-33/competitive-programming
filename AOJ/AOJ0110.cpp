b429c3e745174d49a40fd7a833a295b8ad8195cc
Yang-33 <kasai.yuta0810@gmail.com>
2019-07-20 16:48:34 +0900

    solve AOJ0110.

--- AOJ/AOJ0110.cpp
+++ AOJ/AOJ0110.cpp
@@ -1,40 +1,35 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
-#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
-#define debug(x) cout << #x << ": " << x << endl
-#define mp make_pair
-#define pb push_back
-const ll MOD = 1000000007;
-const int INF = 1e9;
-const ll LINF = 1e16;
-const double PI = acos(-1.0);
-int dx[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-int dy[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 
-/* -----  2017/05/12  Problem:  / Link:   ----- */
-/* ------問題------
+/* -----  2019/07/20  Problem: AOJ 0110 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/0110  ----- */
 
-
-
------問題ここまで----- */
-/* -----解説等-----
-
-
-
-----解説ここまで---- */
-
-
-ll getnum(int &i, string &s, int x) {
-	ll num = 0;
+LL getnum(int &i, string &s, int x) {
+	LL num = 0;
 	int ii = i;
-	while (isdigit(s[i])||s[i]=='X') {
-		if (ii-i == 0 && s[i] == 'X'&&x==0)return LINF;
+	while (isdigit(s[i]) || s[i] == 'X') {
+		if (ii - i == 0 && s[i] == 'X'&&x == 0)return LINF;
 		char c = s[i];
 		if (s[i] == 'X')c = '0' + x;
 		num *= 10;
@@ -45,36 +40,29 @@ ll getnum(int &i, string &s, int x) {
 	return num;
 }
 
-
-string f(string &s) {
-	string ans = "";
-	int p = 0;
-	FOR(i, 0, 10) {
-		p = 0;
-		ll x1 = getnum(p, s, i);
-		p++;
-		ll x2 = getnum(p, s, i);
-		p++;
-		ll x3 = getnum(p, s, i);
-		if (x1 + x2 == x3) {
-			ans = to_string(i);
-			break;
-		}
-	}
-
-	if (ans == "")ans = "NA";
-
-	return ans;
-}
-
 int main() {
-	cin.tie(0);
-	ios_base::sync_with_stdio(false);
-
 	string s;
-
-	while (cin >> s) {
-		cout << f(s) << endl;
+	auto f = [](string&s) {
+		string ans = "";
+		FOR(i, 0, 10) {
+			int p = 0;
+			LL x1 = getnum(p, s, i);
+			p++;
+			LL x2 = getnum(p, s, i);
+			p++;
+			LL x3 = getnum(p, s, i);
+			if (x1 + x2 == x3) {
+				ans = to_string(i);
+				break;
+			}
+		}
+		if (ans == "")ans = "NA";
+		return ans;
+	};
+	while (getline(cin, s)) {
+		string ans = f(s);
+		cout << (ans) << "\n";
 	}
+
 	return 0;
 }
