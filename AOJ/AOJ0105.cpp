a8424f41f28b7289ec70d798538dfef3b86b0538
Yang-33 <kasai.yuta0810@gmail.com>
2019-07-20 16:48:33 +0900

    solve AOJ0105.

--- AOJ/AOJ0105.cpp
+++ AOJ/AOJ0105.cpp
@@ -1,66 +1,47 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
 
-#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
-#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
-#define debug(x) cout << #x << ": " << x << endl
-#define mp make_pair
-#define pb push_back
-const ll MOD = 1000000007;
-const int INF = 1e9;
-const ll LINF = 1e16;
-const double PI = acos(-1.0);
-int dx[8] = {0, 0, 1, -1, 1, 1, -1, -1};
-int dy[8] = {1, -1, 0, 0, 1, -1, 1, -1};
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
 
-/* -----  2017/05/12  Problem:  / Link:   ----- */
-/* ------問題------
+/* -----  2019/07/20  Problem: AOJ 0105 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/0105  ----- */
 
-
-
------問題ここまで----- */
-/* -----解説等-----
-
-
-
-----解説ここまで---- */
-
-ll N;
-
-ll ans = 0LL;
 
 int main() {
-  cin.tie(0);
-  ios_base::sync_with_stdio(false);
+	string in;
+	map<string, VI>m;
+	while (getline(cin, in)) {
+		stringstream ss(in);
+		string s; int id;
+		ss >> s >> id;
+		m[s].push_back(id);
+	}
+	for (auto it : m) {
+		cout << it.first << endl;
+		SORT(it.second);
+		FOR(i, 0, SZ(it.second)) {
+			cout << it.second[i] << " \n"[i + 1 == SZ(it.second)];
+		}
+	}
 
-  string s;
-  int n;
-  vector<pair<string, int>>v;
-  while (cin >> s >> n) {
-	  v.push_back(mp(s, n));
-  }
-
-  sort(v.begin(), v.end());
-  cout << v[0].first << endl;
-  cout << v[0].second;
-
-
-  FOR(i, 1, v.size()) {
-	  if (v[i].first == v[i - 1].first) {
-		  cout << " " << v[i].second;
-	  }
-	  else {
-		  cout << endl;
-		  cout << v[i].first << endl;
-		  cout << v[i].second;
-	  }
-
-  }
-  cout << endl;
-
-  return 0;
+	return 0;
 }
