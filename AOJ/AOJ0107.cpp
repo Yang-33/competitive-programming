2e22755730d7035ad3cface69a48fa87cc4d0ecf
Yang-33 <kasai.yuta0810@gmail.com>
2019-07-20 16:48:34 +0900

    solve AOJ0107.

---
+++ AOJ/AOJ0107.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+
+/* -----  2019/07/20  Problem: AOJ 0107 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/0107  ----- */
+
+
+int main() {
+	LL D, W, H;
+	while (cin >> D >> W >> H, D || W || H) {
+		int N; cin >> N;
+		VL a({ D,W,H });
+		SORT(a);
+		FOR(i, 0, N) {
+			LL r; cin >> r;
+			bool ok = 0;
+
+			do {
+				double h = a[0] / 2.0, w = a[1] / 2.0;
+				ok |= r > sqrt(h*h + w * w);
+
+			} while (next_permutation(ALL(a)));
+			cout << (ok ? "OK" : "NA") << "\n";
+		}
+	}
+
+	return 0;
+}
