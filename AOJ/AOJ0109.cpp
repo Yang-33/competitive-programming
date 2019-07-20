2e555e53726c25ce330f2763b72fa95753bfaa48
Yang-33 <kasai.yuta0810@gmail.com>
2019-07-20 16:48:34 +0900

    solve AOJ0109.

--- AOJ/AOJ0109.cpp
+++ AOJ/AOJ0109.cpp
@@ -14,83 +14,82 @@ using VL = vector<LL>;        using VVL = vector<VL>;
 #define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
 #define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
 #define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
-#define debug(x) cerr << #x << ": " << x << endl
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
 const int INF = 1e9;                          const LL LINF = 1e16;
 const LL MOD = 1000000007;                    const double PI = acos(-1.0);
-int DX[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };    int DY[8] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-/* -----  2018/06/30  Problem: AOJ 0109 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/0109  ----- */
-/* ------問題------
+/* -----  2019/07/20  Problem: AOJ 0109 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/0109  ----- */
 
-四則演算の構文解析
+struct Parsing {
 
------問題ここまで----- */
-/* -----解説等-----
-
-BNFをみて練習
-演算の読み込みに注意して、読み込んでi++的なことをする。or 保存
-
-----解説ここまで---- */
-
-
-LL expr1(int&, string&);
-LL expr2(int&, string&);
-LL expr3(int&, string&);
-LL getnum(int&, string&);
-
-// + - 
-LL expr1(int & i, string &s) {
-	LL ret = expr2(i, s);
-	while (s[i] == '+' || s[i] == '-') {
-		if (s[i] == '+')ret += expr2(++i, s);
-		if (s[i] == '-')ret -= expr2(++i, s);
-	}
-	return ret;
-}
-// * / 
-LL expr2(int & i, string &s) {
-	LL ret = expr3(i, s);
-	while (s[i] == '*' || s[i] == '/') {
-		if (s[i] == '*')ret *= expr3(++i, s);
-		if (s[i] == '/')ret /= expr3(++i, s);
-	}
-	return ret;
-
-}
-// (re expr) or num
-LL expr3(int & i, string &s) {
-	if (s[i] == '(') {
-		i++;
-		LL ret = expr1(i, s);
-		i++;// ')'
+	Parsing() {}
+	// expr1が優先度min
+	LL expr1(int &i, string &s) {
+		LL ret = expr2(i, s);
+		while (i < SZ(s) && (s[i] == '+' || s[i] == '-')) {
+			if (s[i] == '+') {
+				i++;
+				ret += expr2(i, s);
+			}
+			else if (s[i] == '-') {
+				i++;
+				ret -= expr2(i, s);
+			}
+		}
 		return ret;
 	}
-	else {
-		return getnum(i, s);
+
+	LL expr2(int &i, string &s) {
+		LL ret = expr3(i, s);
+		while (i < SZ(s) && (s[i] == '*' || s[i] == '/')) {
+			if (s[i] == '*') {
+				i++;
+				ret *= expr3(i, s);
+			}
+			else if (s[i] == '/') {
+				i++;
+				ret /= expr3(i, s);
+			}
+		}
+		return ret;
 	}
-}
-LL getnum(int& i, string &s) {
-	LL ret = 0;
-	while (isdigit(s[i])){
-		ret *= 10; 
-		ret += s[i] - '0';
-		i++;
+	LL expr3(int &i, string &s) {
+		LL ret;
+		if (s[i] == '(') {
+			i++; // )
+			ret = expr1(i, s); // この区間でアレ
+			i++; // )
+		}
+		else ret = parsenum(i, s);
+
+		return ret;
 	}
-	return ret;
-}
 
-
-
+	LL parsenum(int &i, const string &s) {
+		LL res = 0;
+		while (i < SZ(s) && isdigit(s[i])) {
+			res *= 10;
+			res += s[i] - '0';
+			i++;
+		}
+		return res;
+	}
+};
 int main() {
-	cin.tie(0);
-	ios_base::sync_with_stdio(false);
-
-	int N; cin >> N;
-	FOR(shuri, 0, N) {
+	LL N; cin >> N;
+	Parsing sol;
+	FOR(_, 0, N) {
 		string s; cin >> s;
-		int i = 0;
-		cout << expr1(i, s) << endl;
+		int id = 0;
+		LL ans = sol.expr1(id,s);
+		cout << (ans) << "\n";
 	}
+
 
 	return 0;
 }
