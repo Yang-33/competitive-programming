16ab96f681e76a0b47330d5fd570052c3ac2fb93
Yang-33 <kasai.yuta0810@gmail.com>
2019-07-23 00:39:33 +0900

    solve AOJ0148.

---
+++ AOJ/AOJ0148.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+
+/* -----  2019/07/23  Problem: AOJ 0148 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/0148  ----- */
+
+
+int main() {
+	LL a;
+	while (cin >> a) {
+		int b = (a - (a / 39) * 39);
+		if (!b)b = 39;
+		cout << "3C" << setw(2) << setfill('0') << b << "\n";
+
+	}
+
+}
