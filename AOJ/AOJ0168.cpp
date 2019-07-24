7747bd1c377c9456660d110aea87c6c37d97d2a4
Yang-33 <kasai.yuta0810@gmail.com>
2019-07-24 17:46:38 +0900

    solve AOJ0168.

---
+++ AOJ/AOJ0168.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+
+/* -----  2019/07/23  Problem: AOJ 0168 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/0168  ----- */
+
+
+int main() {
+	const int N = 30;
+	VL fib(N + 1, 0);
+	fib[0] = 1;
+	fib[1] = 1;
+	fib[2] = 2;
+	FOR(i, 3, N + 1) {
+		fib[i] = fib[i - 1] + fib[i - 2] + fib[i - 3];
+	}
+	int in;
+	while (cin >> in, in) {
+		cout << (fib[in] + 3650 - 1) / 3650 << "\n";
+	}
+
+	return 0;
+}
