b754c604790631a0d13f32505c23d19f42ddb242
Yang-33 <kasai.yuta0810@gmail.com>
2019-08-16 00:28:20 +0900

    solve AOJ0228.

---
+++ AOJ/AOJ0228.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using VS = vector<string>;    using LL = long long;
+using VI = vector<int>;       using VVI = vector<VI>;
+using PII = pair<int, int>;   using PLL = pair<LL, LL>;
+using VL = vector<LL>;        using VVL = vector<VL>;
+
+#define ALL(a)  begin((a)),end((a))
+#define RALL(a) (a).rbegin(), (a).rend()
+#define SZ(a) int((a).size())
+#define SORT(c) sort(ALL((c)))
+#define RSORT(c) sort(RALL((c)))
+#define UNIQ(c) (c).erase(unique(ALL((c))), end((c)))
+#define FOR(i, s, e) for (int(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (int(i) = (s); (i) > (e); (i)--)
+//#pragma GCC optimize ("-O3") 
+#ifdef YANG33
+#include "mydebug.hpp"
+#else
+#define DD(x) 
+#endif
+const int INF = 1e9;                          const LL LINF = 1e16;
+const LL MOD = 1000000007;                    const double PI = acos(-1.0);
+
+/* -----  2019/08/15  Problem: AOJ 0228 / Link: https://onlinejudge.u-aizu.ac.jp/challenges/search/volumes/0228  ----- */
+
+
+int main() {
+
+	int N;
+	VVI p = {
+		//g,f,e,d,c,b,a
+		 {0,1,1,1,1,1,1},
+		 {0,0,0,0,1,1,0},
+		 {1,0,1,1,0,1,1},
+		 {1,0,0,1,1,1,1},
+		 {1,1,0,0,1,1,0},
+		 {1,1,0,1,1,0,1},
+		 {1,1,1,1,1,0,1},
+		 {0,1,0,0,1,1,1},
+		 {1,1,1,1,1,1,1},
+		 {1,1,0,1,1,1,1},
+	};
+	while (cin >> N, N != -1) {
+		VI pre(7, 0);
+		FOR(_, 0, N) {
+			int a; cin >> a;
+			FOR(i, 0, 7) {
+				cout << (p[a][i] ^ pre[i]);
+				pre[i] = p[a][i];
+			}
+			puts("");
+		}
+	}
+
+	return 0;
+}
